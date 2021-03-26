015a358a7861a6e935a04d9d33107caa389f897a
realXCV <realXCV@yahoo.ca>
2021-03-26 10:21:19 -0400

    Update PrimeCPP.cpp
    Faster, uses less memory, and fixes a few bugs.

--- PrimeCPP/PrimeCPP.cpp
+++ PrimeCPP/PrimeCPP.cpp
@@ -3,148 +3,172 @@
 // ---------------------------------------------------------------------------
 
 #include <chrono>
-#include <ctime>
-#include <iostream>
-#include <bitset>
+#include <cstdlib>
 #include <map>
+
 
 class prime_sieve
 {
-  private:
+private:
 
-      int sieveSize = 0;
-      unsigned char * rawbits = nullptr;
-      const std::map<const int, const int> myDict = 
-      {
-            { 10 , 1 },                 // Historical data for validating our results - the number of primes
-            { 100 , 25 },               // to be found under some limit, such as 168 primes under 1000
-            { 1000 , 168 },
-            { 10000 , 1229 },
-            { 100000 , 9592 },
-            { 1000000 , 78498 },
-            { 10000000 , 664579 },
-            { 100000000 , 5761455 }
-      };
+	int sieveSize = 0;
+	unsigned char* rawbits = nullptr;
 
-      bool validateResults()
-      {
-          if (myDict.end() == myDict.find(sieveSize))
-              return false;
-          return myDict.find(sieveSize)->second == countPrimes();
-      }
+	const std::map<const int, const int> myDict =
+	{
+		{ 10 , 4 },                 // Historical data for validating our results - the number of primes
+		{ 100 , 25 },               // to be found under some limit, such as 168 primes under 1000
+		{ 1000 , 168 },
+		{ 10000 , 1229 },
+		{ 100000 , 9592 },
+		{ 1000000 , 78498 },
+		{ 10000000 , 664579 },
+		{ 100000000 , 5761455 }
+	};
 
-      bool GetBit(unsigned int index)
-      {
-          if (index % 2 == 0)
-              return false;
-          index = index / 2;
-          return ((rawbits[index / 8]) & (1 << (index % 8))) != 0;
-      }
+	// Number of bits on for each byte
+	const unsigned char lut[256] = {
+		0,1,1,2,1,2,2,3,1,2,2,3,2,3,3,4,
+		1,2,2,3,2,3,3,4,2,3,3,4,3,4,4,5,
+		1,2,2,3,2,3,3,4,2,3,3,4,3,4,4,5,
+		2,3,3,4,3,4,4,5,3,4,4,5,4,5,5,6,
+		1,2,2,3,2,3,3,4,2,3,3,4,3,4,4,5,
+		2,3,3,4,3,4,4,5,3,4,4,5,4,5,5,6,
+		2,3,3,4,3,4,4,5,3,4,4,5,4,5,5,6,
+		3,4,4,5,4,5,5,6,4,5,5,6,5,6,6,7,
+		1,2,2,3,2,3,3,4,2,3,3,4,3,4,4,5,
+		2,3,3,4,3,4,4,5,3,4,4,5,4,5,5,6,
+		2,3,3,4,3,4,4,5,3,4,4,5,4,5,5,6,
+		3,4,4,5,4,5,5,6,4,5,5,6,5,6,6,7,
+		2,3,3,4,3,4,4,5,3,4,4,5,4,5,5,6,
+		3,4,4,5,4,5,5,6,4,5,5,6,5,6,6,7,
+		3,4,4,5,4,5,5,6,4,5,5,6,5,6,6,7,
+		4,5,5,6,5,6,6,7,5,6,6,7,6,7,7,8
+	};
 
-      void ClearBit(unsigned int index)
-      {
-          if (index % 2 == 0)
-          {
-              printf("You're setting even bits, which is sub-optimal.\n");
-              return;
-          }
-          index = index / 2;
-          rawbits[index / 8] &= ~(1 << (index % 8));
-      }
+	bool validateResults()
+	{
+		if (myDict.end() == myDict.find(sieveSize))
+			return false;
+		return myDict.find(sieveSize)->second == countPrimes();
+	}
 
-  public:
+	bool GetBit(unsigned int index)
+	{
+		return rawbits[index / 8] & 1 << index % 8;
+	}
 
-      prime_sieve(int n)
-      {
-          sieveSize = n;
-          rawbits = (unsigned char *) malloc(n / 8 + 1);
-          if (rawbits)
-            memset(rawbits, 0xff, n / 8 + 1);
-      }
+	void ClearBit(unsigned int index)
+	{
+		rawbits[index / 8] &= ~(1 << index % 8);
+	}
 
-      ~prime_sieve()
-      {
-          free(rawbits);
-      }
+public:
 
-      void runSieve()
-      {
-          int factor = 3;
-          int q = sqrt(sieveSize);
+	prime_sieve(int n)
+	{
+		sieveSize = n;
+		rawbits = (unsigned char*)malloc(n / 16 + 1);
+	}
 
-          while (factor < q)
-          {
-              for (int num = factor; num < sieveSize; num++)
-              {
-                  if (GetBit(num))
-                  {
-                      factor = num;
-                      break;
-                  }
-              }
-              for (int num = factor * 3; num < sieveSize; num += factor * 2)
-                  ClearBit(num);
-               
-              factor += 2;
-          }
-      }
+	~prime_sieve()
+	{
+		free(rawbits);
+	}
 
-      void printResults(bool showResults, double duration, int passes)
-      {
-          if (showResults)
-              printf("2, ");
+	void reset()
+	{
+		if (rawbits)
+			memset(rawbits, 0xff, sieveSize / 16 + 1);
+	}
 
-          int count = 1;
-          for (int num = 3; num <= sieveSize; num++)
-          {
-              if (GetBit(num))
-              {
-                  if (showResults)
-                      printf("%d, ", num);
-                  count++;
-              }
-          }
+	void runSieve()
+	{
+		reset();
 
-          if (showResults)
-              printf("\n");
-          
-          printf("Passes: %d, Time: %lf, Avg: %lf, Limit: %d, Count: %d, Valid: %d\n", 
-                 passes, 
-                 duration, 
-                 duration / passes, 
-                 sieveSize, 
-                 count, 
-                 validateResults());
-      }
+		int halfsize = sieveSize / 2;
+		int factor = 1;
 
-      int countPrimes()
-      {
-          int count = 0;
-          for (int i = 0; i < sieveSize; i++)
-              if (GetBit(i))
-                  count++;
-          return count;
-      }
+		int step;
+
+		int q = (int)sqrt(sieveSize) / 2;
+
+		while (factor <= q)
+		{
+			for (int num = factor; num <= halfsize; ++num)
+			{
+				if (GetBit(num))
+				{
+					factor = num;
+					step = factor * 2 + 1;
+					break;
+				}
+			}
+			for (int num = factor + step; num <= halfsize; num += step)
+				ClearBit(num);
+
+			++factor;
+		}
+	}
+
+	void printResults(bool showResults, double duration, int passes)
+	{
+		if (showResults)
+			printf("2, ");
+
+		int count = 1;
+		for (int num = 1; num < (sieveSize + 1) / 2; ++num)
+		{
+			if (GetBit(num))
+			{
+				if (showResults)
+					printf("%d, ", num * 2 + 1);
+				count++;
+			}
+		}
+
+		if (showResults)
+			printf("\n");
+
+		printf("Passes: %d, Time: %lf, Avg: %lf, Limit: %d, Count: %d, Valid: %d\n",
+			passes,
+			duration,
+			duration / passes,
+			sieveSize,
+			count,
+			validateResults());
+	}
+
+	int countPrimes()
+	{
+		int count = 0;
+		int halfsize = (sieveSize + 1) / 2;
+		int limit = halfsize / 8;
+		for (unsigned char* i = rawbits; i < rawbits + limit; ++i)
+			count += lut[*i];
+
+		count += lut[rawbits[limit] & 0xFF >> 8 - halfsize % 8];
+
+		return count;
+	}
 };
 
 int main()
 {
-    auto passes = 0;
-    prime_sieve* sieve = nullptr;
+	auto passes = 0;
+	prime_sieve* sieve = new prime_sieve(1000000);
 
-    auto tStart = std::chrono::steady_clock::now();
-    while (std::chrono::duration_cast<std::chrono::seconds>(std::chrono::steady_clock::now() - tStart).count() < 10)
-    {
-        delete sieve;
-        sieve = new prime_sieve(1000000);
-        sieve->runSieve();
-        passes++;
-    }
-    auto tD = std::chrono::steady_clock::now() - tStart;
-    
-    if (sieve)
-    {
-        sieve->printResults(false, std::chrono::duration_cast<std::chrono::microseconds>(tD).count() / 1000000, passes);
-        delete sieve;
-    }
+	auto tStart = std::chrono::steady_clock::now();
+	while (std::chrono::duration_cast<std::chrono::seconds>(std::chrono::steady_clock::now() - tStart).count() < 5)
+	{
+		sieve->runSieve();
+		passes++;
+	}
+	auto tD = std::chrono::steady_clock::now() - tStart;
+
+	if (sieve)
+	{
+		sieve->printResults(true, std::chrono::duration_cast<std::chrono::microseconds>(tD).count() / 1000000, passes);
+		delete sieve;
+	}
 }
