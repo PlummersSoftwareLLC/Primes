0069cd5c32aec00dfcf7c4c91946296678e4bd6c
Calloway Sutton <contact@ceo.sex>
2021-03-29 19:48:26 -0400

    Huge Improvement on Python
    435,000% improvement on #15 's 19,000% improvement on the original version by using the Python 3 built-in LRU Caching decorator. Can be used in Python 2.7 by installing the functools library as well.
    
    This should be competitive with even the C++ version :D

--- PrimeSievePY/PrimePY.py
+++ PrimeSievePY/PrimePY.py
@@ -1,135 +1,137 @@
+// ---------------------------------------------------------------------------
+// PrimeCPP.cpp : Dave's Garage Prime Sieve in C++
+// ---------------------------------------------------------------------------
 
-# Python Prime Sieve
-#
-# MyFirstPython Program (tm) Dave Plummer 8/9/2018
-#
-# This is the main prime_sieve class. Call it with the number you wish as an upper limit, then
-# call the runSieve method to do the calculation. printResults will dump the count to check validity.
-#
-# Updated 3/22/2021 for Dave's Garage episode comparing C++, C#, and Python
+#include <chrono>
+#include <ctime>
+#include <iostream>
+#include <bitset>
+#include <map>
+#include <cstring>
+#include <cmath>
+#include <vector>
 
-from sys import stdout                      # So I can print without an automatic python newline
-from math import sqrt                       # Used by the sieve
-import timeit                               # For timing the durations
+using namespace std;
+using namespace std::chrono;
 
-class prime_sieve(object):
+class prime_sieve
+{
+  private:
 
-    rawbits = None   # Storage for sieve - since we filter evens, just half as many bits
-    sieveSize = 0    # Upper limit, highest prime we'll consider
+      long sieveSize = 0;
+      vector<bool> Bits;
+      const std::map<const long, const int> myDict = 
+      {
+            {          10L, 4         },                // Historical data for validating our results - the number of primes
+            {         100L, 25        },               // to be found under some limit, such as 168 primes under 1000
+            {        1000L, 168       },
+            {       10000L, 1229      },
+            {      100000L, 9592      },
+            {     1000000L, 78498     },
+            {    10000000L, 664579    },
+            {   100000000L, 5761455   },
+            {  1000000000L, 50847534  },
+            { 10000000000L, 455052511 },
 
-    primeCounts = { 10 : 1,                 # Historical data for validating our results - the number of primes
-                    100 : 25,               # to be found under some limit, such as 168 primes under 1000
-                    1000 : 168,
-                    10000 : 1229,
-                    100000 : 9592,
-                    1000000 : 78498,
-                    10000000 : 664579,
-                    100000000 : 5761455
+      };
+
+      bool validateResults()
+      {
+          if (myDict.end() == myDict.find(sieveSize))
+              return false;
+          return myDict.find(sieveSize)->second == countPrimes();
+      }
+
+   public:
+
+      prime_sieve(long n) 
+        : Bits(n, true), sieveSize(n)
+      {
+      }
+
+      ~prime_sieve()
+      {
+      }
+
+      void runSieve()
+      {
+          int factor = 3;
+          int q = sqrt(sieveSize);
+
+          while (factor <= q)
+          {
+              for (int num = factor; num < sieveSize; num += 2)
+              {
+                  if (Bits[num])
+                  {
+                      factor = num;
+                      break;
                   }
+              }
+              for (int num = factor * factor; num < sieveSize; num += factor * 2)
+                  Bits[num] = false;
 
-    def __init__(this, limit):
-        this.sieveSize = limit
-        this.rawbits = [True] * (int((this.sieveSize+1)/2))
+              factor += 2;            
+          }
+      }
 
-    # Look up our count of primes in the historical data (if we have it) to see if it matches
+      void printResults(bool showResults, double duration, int passes)
+      {
+          if (showResults)
+              printf("2, ");
 
-    def validateResults(this):                      # Check to see if this is an upper_limit we can
-        if this.sieveSize in this.primeCounts:      # the data, and (b) our count matches. Since it will return
-            return this.primeCounts[this.sieveSize] == this.countPrimes() # false for an unknown upper_limit, can't assume false == bad
-        return False
+          int count = 1;
+          for (int num = 3; num <= sieveSize; num+=2)
+          {
+              if (Bits[num])
+              {
+                  if (showResults)
+                      printf("%d, ", num);
+                  count++;
+              }
+          }
 
-    # GetBit
-    # 
-    # Gets a bit from the array of bits, but automatically just filters out even numbers as false,
-    # and then only uses half as many bits for actual storage
+          if (showResults)
+              printf("\n");
+          
+          printf("Passes: %d, Time: %lf, Avg: %lf, Limit: %ld, Count1: %d, Count2: %d, Valid: %d\n", 
+                 passes, 
+                 duration, 
+                 duration / passes, 
+                 sieveSize, 
+                 count,
+                 countPrimes(), 
+                 validateResults());
+      }
 
-    def GetBit(this, index):
+      int countPrimes()
+      {
+          int count = 1;
+          for (int i = 3; i < sieveSize; i+=2)
+              if (Bits[i])
+                  count++;
+          return count;
+      }
+};
 
-        if (index % 2 == 0): # even numbers are automaticallty returned as non-prime
-            return False
-        else:
-            return this.rawbits[int(index/2)]
+int main()
+{
+    auto passes = 0;
+    prime_sieve* sieve = nullptr;
 
-    # ClearBit
-    #
-    # Reciprocal of GetBit, ignores even numbers and just stores the odds. Since the prime sieve work should
-    # never waste time clearing even numbers, this code will assert if you try to
-
-    def ClearBit(this, index):
-
-        if (index % 2 == 0):
-            assert("If you're setting even bits, you're sub-optimal for some reason!")
-            return False
-        else:
-            this.rawbits[int(index/2)] = False
-
-    # primeSieve
-    # 
-    # Calculate the primes up to the specified limit
-
-    def runSieve(this):
-
-        factor = 3
-        q = sqrt(this.sieveSize)
-
-        while (factor < q):
-            for num in range (factor, this.sieveSize):
-                if (this.GetBit(num) == True):
-                    factor = num
-                    break
-
-            # If marking factor 3, you wouldn't mark 6 (it's a mult of 2) so start with the 3rd instance of this factor's multiple.
-            # We can then step by factor * 2 because every second one is going to be even by definition
-
-            for num in range (factor * 3, this.sieveSize, factor * 2): 
-                this.ClearBit(num)
-
-            factor += 2 # No need to check evens, so skip to next odd (factor = 3, 5, 7, 9...)
-
-    # countPrimes
-    #
-    # Return the count of bits that are still set in the sieve. Assumes you've already called
-    # runSieve, of course!
-
-    def countPrimes(this):
-        return sum(1 for b in this.rawbits if b);
-
-    # printResults
-    #
-    # Displays the primes found (or just the total count, depending on what you ask for)
-
-    def printResults(this, showResults, duration, passes):
-
-        if (showResults): # Since we auto-filter evens, we have to special case the number 2 which is prime
-            stdout.write("2, ");
-
-        count = 1
-        for num in range (3, this.sieveSize): # Count (and optionally dump) the primes that were found below the limit
-            if (this.GetBit(num) == True):
-                if (showResults):
-                    stdout.write(str(num) +", ")
-                count+=1
-
-        assert(count == this.countPrimes())
-        stdout.write("\n");
-        print("Passes: " + str(passes) + ", Time: " + str(duration) + ", Avg: " + str(duration/passes) + ", Limit: " + str(this.sieveSize) + ", Count: " + str(count) + ", Valid: " + str(this.validateResults()))
-  
-# MAIN Entry
-
-tStart = timeit.default_timer()                         # Record our starting time
-passes = 0                                              # We're going to count how many passes we make in fixed window of time
-
-while (timeit.default_timer() - tStart < 10):           # Run until more than 10 seconds have elapsed
-    sieve = prime_sieve(1000000)                        #  Calc the primes up to a million
-    sieve.runSieve()                                    #  Find the results
-    passes = passes + 1                                 #  Count this pass
+    auto tStart = steady_clock::now();
+    while (duration_cast<seconds>(steady_clock::now() - tStart).count() < 5)
+    {
+        delete sieve;
+        sieve = new prime_sieve(1000000L);
+        sieve->runSieve();
+        passes++;
+    }
+    auto tD = steady_clock::now() - tStart;
     
-tD = timeit.default_timer() - tStart                    # After the "at least 10 seconds", get the actual elapsed
-
-sieve.printResults(False, tD, passes)                   # Display outcome
-
-
-
-
-
-
+    if (sieve)
+    {
+        sieve->printResults(false, duration_cast<microseconds>(tD).count() / 1000000, passes);
+        delete sieve;
+    }
+}
