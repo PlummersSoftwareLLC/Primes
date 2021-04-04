648e249612898bacdf23369f9ed8532b5693e1c7
devblok <l.praninskas@gmail.com>
2021-04-04 19:26:45 +0300

    Zig implementation of Prime, staying true to the original.

---
+++ PrimeZig/src/main.zig
@@ -0,0 +1,28 @@
+const std = @import("std");
+const heap = std.heap;
+const print = std.debug.print;
+const Timer = std.time.Timer;
+const Sieve = @import("./prime.zig").Sieve;
+
+pub fn main() anyerror!void {
+    const timer = try Timer.start();
+    const size: u64 = 1_000_000;
+
+    var allocator = heap.ArenaAllocator.init(heap.page_allocator);
+    defer allocator.deinit();
+
+    var passes: u64 = 0;
+    while (timer.read() < 5 * 1_000_000_000) : (passes += 1) {
+        var sieve = try Sieve(u64).init(&allocator.allocator, size);
+        sieve.run();
+        sieve.deinit();
+    }
+
+    const elapsed = @intToFloat(f32, timer.read()) / 1_000_000_000.0;
+    print("Passes: {}, Time: {}, Avg: {}, Limit: {}\n", .{
+        passes,
+        elapsed,
+        elapsed / @intToFloat(f32, passes),
+        size,
+    });
+}
---
+++ PrimeZig/src/prime.zig
@@ -0,0 +1,153 @@
+const std = @import("std");
+const ArrayList = std.ArrayList;
+const Allocator = std.mem.Allocator;
+
+const print = std.debug.print;
+
+/// Prime number sieve.
+pub fn Sieve(comptime NumType: type) type {
+    return struct {
+        sieveSize: usize,
+        bits: ArrayList(bool),
+
+        const Self = @This();
+
+        pub fn init(allocator: *Allocator, size: usize) !Self {
+            var self = Self{
+                .sieveSize = size,
+                .bits = try ArrayList(bool).initCapacity(allocator, size),
+            };
+            try self.bits.appendNTimes(true, size);
+            return self;
+        }
+
+        pub fn deinit(self: *Self) void {
+            self.bits.deinit();
+        }
+
+        pub fn run(self: *Self) void {
+            var factor: NumType = 3;
+            const q = @floatToInt(usize, @sqrt(@intToFloat(f64, self.sieveSize)));
+
+            while (factor <= q) : (factor += 2) {
+                var num: NumType = factor;
+                factorSet: while (num < self.sieveSize) : (num += 2) {
+                    if (self.bits.items[num]) {
+                        factor = num;
+                        break :factorSet;
+                    }
+                }
+
+                num = factor * factor;
+                while (num < self.sieveSize) : (num += factor * 2) {
+                    self.bits.items[num] = false;
+                }
+            }
+        }
+
+        pub fn primeCount(self: *Self) usize {
+            var count: usize = 1;
+            var idx: usize = 3;
+
+            while (idx < self.sieveSize) : (idx += 2) {
+                count += if (self.bits.items[idx]) @as(usize, 1) else @as(usize, 0);
+            }
+
+            return count;
+        }
+    };
+}
+
+const expectEqual = std.testing.expectEqual;
+const test_allocator = std.testing.allocator;
+
+test "count primes up to 10" {
+    var sieve = try Sieve(u32).init(test_allocator, 10);
+    defer sieve.deinit();
+
+    sieve.run();
+
+    expectEqual(@as(usize, 4), sieve.primeCount());
+}
+
+test "count primes up to 100" {
+    var sieve = try Sieve(u32).init(test_allocator, 100);
+    defer sieve.deinit();
+
+    sieve.run();
+
+    expectEqual(@as(usize, 25), sieve.primeCount());
+}
+
+test "count primes up to 1000" {
+    var sieve = try Sieve(u32).init(test_allocator, 1_000);
+    defer sieve.deinit();
+
+    sieve.run();
+
+    expectEqual(@as(usize, 168), sieve.primeCount());
+}
+
+test "count primes up to 10000" {
+    var sieve = try Sieve(u32).init(test_allocator, 10_000);
+    defer sieve.deinit();
+
+    sieve.run();
+
+    expectEqual(@as(usize, 1_229), sieve.primeCount());
+}
+
+test "count primes up to 100000" {
+    var sieve = try Sieve(u32).init(test_allocator, 100_000);
+    defer sieve.deinit();
+
+    sieve.run();
+
+    expectEqual(@as(usize, 9_592), sieve.primeCount());
+}
+
+test "count primes up to 1000000" {
+    var sieve = try Sieve(u32).init(test_allocator, 1_000_000);
+    defer sieve.deinit();
+
+    sieve.run();
+
+    expectEqual(@as(usize, 78_498), sieve.primeCount());
+}
+
+test "count primes up to 10000000" {
+    var sieve = try Sieve(u32).init(test_allocator, 10_000_000);
+    defer sieve.deinit();
+
+    sieve.run();
+
+    expectEqual(@as(usize, 664_579), sieve.primeCount());
+}
+
+test "count primes up to 100000000" {
+    var sieve = try Sieve(u32).init(test_allocator, 100_000_000);
+    defer sieve.deinit();
+
+    sieve.run();
+
+    expectEqual(@as(usize, 5_761_455), sieve.primeCount());
+}
+
+test "count primes up to 1000000000" {
+    var sieve = try Sieve(u32).init(test_allocator, 1_000_000_000);
+    defer sieve.deinit();
+
+    sieve.run();
+
+    expectEqual(@as(usize, 50_847_534), sieve.primeCount());
+}
+
+// Integer overflows. Too lazy to fix :)
+// test "count primes up to 10000000000" {
+//     var sieve = try Sieve(u32).init(test_allocator, 10_000_000_000);
+//     defer sieve.deinit();
+//
+//     sieve.run();
+//
+//     expectEqual(@as(usize, 455_052_511), sieve.primeCount());
+// }
