6c7759a5b67de8137891fcca826f1ed84afaac08
ssovest <ssergienko56@gmail.com>
2021-04-05 18:00:31 +0300

    more optimization & pythonization

--- PrimeSievePY/PrimePY.py
+++ PrimeSievePY/PrimePY.py
@@ -1,17 +1,18 @@
+"""
+Python Prime Sieve
 
-# Python Prime Sieve
-#
-# MyFirstPython Program (tm) Dave Plummer 8/9/2018
-#
-# This is the main prime_sieve class. Call it with the number you wish as an upper limit, then
-# call the runSieve method to do the calculation. printResults will dump the count to check validity.
-#
-# Updated 3/22/2021 for Dave's Garage episode comparing C++, C#, and Python
+MyFirstPython Program (tm) Dave Plummer 8/9/2018
+Updated 3/22/2021 for Dave's Garage episode comparing C++, C#, and Python
+"""
 
-class prime_sieve(object):
+class PrimeSieve:
 
-    primeCounts = { 10 : 1,                 # Historical data for validating our results - the number of primes
-                    100 : 25,               # to be found under some limit, such as 168 primes under 1000
+    """This is the main PrimeSieve class. Call it with the number you wish as
+    an upper limit, then call the run_sieve method to do the calculation.
+    print_results will dump the count to check validity."""
+
+    prime_counts = { 10 : 4,                 # Historical data for validating our results - the number of primes
+                    100 : 25,                # to be found under some limit, such as 168 primes under 1000
                     1000 : 168,
                     10000 : 1229,
                     100000 : 9592,
@@ -21,76 +22,88 @@ class prime_sieve(object):
                   }
 
     def __init__(self, limit):
-        self.size = limit
-        self.bits = bytearray(b"\x00\x01" * ((self.size + 1) // 2))
+        self._size = limit
+        self._bits = bytearray(b"\x01" * ((self._size + 1) // 2))
 
-    # Look up our count of primes in the historical data (if we have it) to see if it matches
+    def validate_results(self):                      # Check to see if this is an upper_limit we can
 
-    def validateResults(self):                      # Check to see if this is an upper_limit we can
-        if self.size in self.primeCounts:           # the data, and (b) our count matches. Since it will return
-            return self.primeCounts[self.size] == self.countPrimes() # false for an unknown upper_limit, can't assume false == bad
+        """Look up our count of primes in the historical data (if we have it)
+        to see if it matches"""
+
+        if self._size in self.prime_counts:                              # the data, and (b) our count matches. Since it will return
+            return self.prime_counts[self._size] == self.count_primes()  # false for an unknown upper_limit, can't assume false == bad
         return False
 
-    # primeSieve
-    # 
-    # Calculate the primes up to the specified limit
+    def run_sieve(self):
 
-    def runSieve(self):
+        """Calculate the primes up to the specified limit"""
 
-        factor = 3
-        q = self.size**0.5
+        factor = 1
+        q = self._size**0.5 / 2
+        bitslen = len(self._bits)
 
-        while (factor < q):
-            factor = self.bits.index(b"\x01", factor)
+        while (factor <= q):
+            factor = self._bits.index(b"\x01", factor)
 
             # If marking factor 3, you wouldn't mark 6 (it's a mult of 2) so start with the 3rd instance of this factor's multiple.
             # We can then step by factor * 2 because every second one is going to be even by definition
+            start = factor * 3 + 1
+            step  = factor * 2 + 1
+            size  = bitslen - start
+            self._bits[start :: step] = b"\x00" * (size // step + bool(size % step))  # bool is (a subclass of) int in python
 
-            self.bits[factor * 3 :: factor * 2] = b"\x00" * ((self.size - factor * 3) // (factor * 2) + 1)
+            factor += 1
 
-            factor += 2 # No need to check evens, so skip to next odd (factor = 3, 5, 7, 9...)
+    def count_primes(self):
 
-    # countPrimes
-    #
-    # Return the count of bits that are still set in the sieve. Assumes you've already called
-    # runSieve, of course!
+        """Return the count of bits that are still set in the sieve.
+        Assumes you've already called run_sieve, of course!"""
 
-    def countPrimes(self):
-        return sum(self.bits);
+        return sum(self._bits)
 
-    # printResults
-    #
-    # Displays the primes found (or just the total count, depending on what you ask for)
+    def print_results(self, show_results, duration, passes):
 
-    def printResults(self, showResults, duration, passes):
+        """Displays the primes found (or just the total count,
+        depending on what you ask for)"""
 
-        if showResults: # Since we auto-filter evens, we have to special case the number 2 which is prime
-            print("2, ", end="");
+        if show_results:  # Since we auto-filter evens, we have to special case the number 2 which is prime
+            print("2, ", end="")
 
         count = 1
-        for num in range (3, self.size, 2): # Count (and optionally dump) the primes that were found below the limit
-            if self.bits[num]:
-                if showResults:
-                    print(str(num) + ", ", end="")
+        for num, is_prime in enumerate(self._bits):  # Count (and optionally dump) the primes that were found below the limit
+            if num and is_prime:
+                if show_results:
+                    print(str(num * 2 + 1) + ", ", end="")
                 count += 1
 
-        assert(count == self.countPrimes())
-        print();
-        print("Passes: %s, Time: %s, Avg: %s, Limit: %s, Count: %s, Valid: %s" % (passes, duration, duration/passes, self.size, count, self.validateResults()))
-  
+        assert(count == self.count_primes())
+        if show_results:
+            print()
+        print("Passes: %s, Time: %s, Avg: %s, Limit: %s, Count: %s, Valid: %s" % (passes, duration, duration/passes, self._size, count, self.validate_results()))
+
 # MAIN Entry
 if __name__ == "__main__":
+    from argparse import ArgumentParser
+    from timeit import default_timer  # For timing the durations
 
-    from timeit import default_timer                        # For timing the durations
+    parser = ArgumentParser(description="Python Prime Sieve")
+    parser.add_argument("--limit", "-l", help="Upper limit for calculating prime numbers", type=int, default=1_000_000)
+    parser.add_argument("--time", "-t", help="Time limit", type=float, default=10)
+    parser.add_argument("--show", "-s", help="Print found prime numbers", action="store_true")
 
-    tStart = default_timer()                                # Record our starting time
-    passes = 0                                              # We're going to count how many passes we make in fixed window of time
+    args = parser.parse_args()
+    limit = args.limit
+    timeout = args.time
+    show_results = args.show
 
-    while (default_timer() - tStart < 10):                  # Run until more than 10 seconds have elapsed
-        sieve = prime_sieve(1_000_000)                      #  Calc the primes up to a million
-        sieve.runSieve()                                    #  Find the results
-        passes = passes + 1                                 #  Count this pass
-        
-    tD = default_timer() - tStart                           # After the "at least 10 seconds", get the actual elapsed
+    time_start = default_timer()                           # Record our starting time
+    passes = 0                                             # We're going to count how many passes we make in fixed window of time
 
-    sieve.printResults(False, tD, passes)                   # Display outcome
+    while (default_timer() - time_start < timeout):        # Run until more than 10 seconds have elapsed
+        sieve = PrimeSieve(limit)                          # Calc the primes up to a million
+        sieve.run_sieve()                                  # Find the results
+        passes = passes + 1                                # Count this pass
+
+    time_delta = default_timer() - time_start              # After the "at least 10 seconds", get the actual elapsed
+
+    sieve.print_results(show_results, time_delta, passes)  # Display outcome
