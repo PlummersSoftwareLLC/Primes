54a2128eda1b5bac6f061b53417ea5e0c3690c29
Matthew Peters <jmp306@gmail.com>
2021-04-08 08:52:18 -0500

    Update prime_sieve_rust.rs
    removed a bunch of print statements
    fixed print results so that it works correctly
    fixed naming to remove warnings
    commented out clear bit to remove warning
    fixed an unused mut in prime_sieve

--- PrimeRust/prime_sieve_rust.rs
+++ PrimeRust/prime_sieve_rust.rs
@@ -3,7 +3,7 @@ use std::vec::Vec;
 use std::time::{Instant};
 use std::collections::HashMap;
 
-fn validateResults(sievesize:i64, primecount:i64)->bool{
+fn validate_results(sievesize:i64, primecount:i64)->bool{
     // to be found under some limit, such as 168 primes under 1000
     // Historical data for validating our results - the number of primes
     let results_map: HashMap<i64, i64> = 
@@ -30,12 +30,11 @@ fn validateResults(sievesize:i64, primecount:i64)->bool{
 
 fn prime_seive(size: usize) -> Vec<bool>{
     let seivesize = size;
-    let mut primevec:Vec<bool> = Vec::new();
-    primevec = vec![true; (seivesize+1)/2];
+    let primevec = vec![true; (seivesize+1)/2];
     return primevec
 }
 
-fn GetBit(index: i64, primevec: &Vec<bool>) -> bool{
+fn get_bit(index: i64, primevec: &Vec<bool>) -> bool{
     if index % 2 == 0 {
         return false;
     }
@@ -44,23 +43,23 @@ fn GetBit(index: i64, primevec: &Vec<bool>) -> bool{
     }
 }
 
-fn ClearBit(index:i64, mut primevec: Vec<bool>){
-        if index % 2 == 0 {
-            println!("You are setting even bits, which is sub-optimal");
-            return;
-        }
-        primevec[(index / 2) as usize ] = false;      
+//fn clear_bit(index:i64, mut primevec: Vec<bool>){
+//       if index % 2 == 0 {
+//            println!("You are setting even bits, which is sub-optimal");
+//            return;
+//        }
+//        primevec[(index / 2) as usize ] = false;      
+//
+//}
 
-}
-
-fn runSieve(sievesize: i64) -> Vec<bool>{
+fn run_sieve(sievesize: i64) -> Vec<bool>{
     let mut factor:i64 = 3;
     let q = (sievesize as f64).sqrt() as i64;
     let mut primevec = prime_seive(sievesize as usize);
     while factor < q {
-        for num in (factor .. sievesize) {
+        for num in factor .. sievesize {
 
-            if GetBit(num, &primevec) == true{
+            if get_bit(num, &primevec) == true{
                 
                 factor = num;
                 
@@ -84,28 +83,25 @@ fn runSieve(sievesize: i64) -> Vec<bool>{
 
 }
 
-fn printResults(sievesize:i64, primevec: &Vec<bool>, showResults:bool, passes:i64, duration:u64){
-    if showResults{
-        //println!("2, ");
+fn print_results(sievesize:i64, primevec: &Vec<bool>, show_results:bool, passes:i64, duration:u64){
+    if show_results{
+        print!("2, ");
         let mut count:i64 = 1;
         for num in 3..sievesize{
-            if GetBit(num, &primevec) {
-                    if showResults {
-                        //println!("{}, ", num);
+            if get_bit(num, &primevec) {
+                    if show_results {
+                        print!("{}, ", num);
                         count = count + 1;
                     }
             }
             
-            if showResults{
-                
-                
-                //print!("{}:{}|", num, GetBit(num, &primevec));
-                
-            
-                    //println!("");
-            }
+
         }
-        println!("Passes: {}, Time: {}, Avg: {}, Limit: {}, Count: {}, Valid: {}",passes, duration, (duration as i64 / passes), sievesize, count, validateResults(sievesize, count));
+        if show_results{
+            println!("");
+        }
+        println!("Passes: {}, Time: {}, Avg: {}, Limit: {}, Count: {}, Valid: {}",passes, duration,
+         ((duration as f64) / (passes as f64)), sievesize, count, validate_results(sievesize, count));
     }
 }
 
@@ -121,11 +117,11 @@ fn main() {
     
     while start.elapsed().as_secs() < 10 {
         //println!("{}", start.elapsed().as_secs());
-        primevec= runSieve(sievesize);
+        primevec= run_sieve(sievesize);
         passes = passes + 1;
     }
 
     let duration = start.elapsed();
-    printResults(sievesize, &primevec, true, passes, duration.as_secs());
+    print_results(sievesize, &primevec, true, passes, duration.as_secs());
 
 }
