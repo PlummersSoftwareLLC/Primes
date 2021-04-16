b9703acf5408780c9be0f5bcacb68d5ff4eb172a
Michael Hardeman <michael.hardeman@protonmail.com>
2021-04-15 20:52:10 -0400

    Allocating Bit_Array on the heap instead of stack
    For Test_Power values higher than 7 the program would crash because it
    exhausted stack space. Using the heap seems to have no effect on
    performance but we have much more room available to use. The program
    will now run correctly for all supported powers of 10 up to 10^10!
    
    Moved Q's calculation out of the Run_Sieve function, but it had no
    effect on performance. I assume the optimizer was already doing that.
    
    Changed the Print_Result output to more closely match the output from
    other implementations. If people create docker containers to run these
    tests and compare this will help them to include the Ada results since
    the output is now similar.
    
    I also changed the measuring of the Finish time to be accurate instead
    of always reporting 5 seconds. 1 iteration of 10^10 was taking 82
    seconds on my machine, the new results are more accurate.

--- PrimeSieveAda/src/main.adb
+++ PrimeSieveAda/src/main.adb
@@ -1,11 +1,16 @@
+with Ada.Unchecked_Deallocation;
 with Ada.Text_IO;   use Ada.Text_IO;
 with Ada.Real_Time; use Ada.Real_Time;
 with Ada.Numerics.Long_Long_Elementary_Functions;
+use Ada.Numerics.Long_Long_Elementary_Functions;
 
 procedure Main is
 
    type Int_64 is new Long_Long_Integer;
    type Bit_Array is array (Int_64 range <>) of Boolean;
+   type Bit_Array_Access is access all Bit_Array;
+
+   procedure Free is new Ada.Unchecked_Deallocation (Bit_Array, Bit_Array_Access);
 
    subtype Power_Of_Ten is Natural range 1 .. 10;
    type Results_Array is array (Power_Of_Ten) of Int_64;
@@ -16,69 +21,7 @@ procedure Main is
    Test_Power    : constant Power_Of_Ten := 6;
    Show_Primes   : constant Boolean      := False; 
 
-   procedure Run_Sieve (Bits : in out Bit_Array; Sieve_Size : in Int_64) is
-      use Ada.Numerics.Long_Long_Elementary_Functions;
-      Factor : Int_64 := 3;
-      Q      : Int_64 := Int_64 (Sqrt (Long_Long_Float (Sieve_Size)));
-      Num    : Int_64;
-   begin
-      while Factor <= Q loop
-         Num := Factor;
-         Inner : while Num < Sieve_Size loop
-            if Bits (Num) then
-               Factor := Num;
-               exit Inner;
-            end if;
-            Num := Num + 2;
-         end loop Inner;
-         Num := Factor * Factor;
-         while Num < Sieve_Size loop
-            Bits (Num) := False;
-            Num        := Num + Factor * 2;
-         end loop;
-         Factor := Factor + 2;
-      end loop;
-   end Run_Sieve;
-
-   function Count_Primes (Bits : in Bit_Array) return Int_64 is
-      Count : Int_64 := 1;
-   begin
-      for I in Bits'Range loop
-         if 1 = (I mod 2) and Bits (I) then
-            Count := Count + 1;
-         end if;
-      end loop;
-      return Count;
-   end Count_Primes;
-
-   procedure Print_Result (
-      Limit    : in Int_64;
-      Elapsed  : in Time_Span;
-      Passes   : in Int_64;
-      Expected : in Int_64;
-      Actual   : in Int_64) 
-   is
-      Elapsed_Duration : Duration := To_Duration (Elapsed);
-      Average          : Duration := Elapsed_Duration / Integer (Passes);
-   begin
-      Put_Line ("Limit   :" & Limit'Image);
-      Put_Line ("Time    :" & Elapsed_Duration'Image);
-      Put_Line ("Passes  :" & Passes'Image);
-      Put_Line ("Average :" & Average'Image);
-      Put_Line ("Expected:" & Expected'Image);
-      Put_Line ("Actual  :" & Actual'Image);
-   end Print_Result;
-
-   procedure Print_Primes (Bits : in Bit_Array) is begin
-      Put ("[ 2");
-      for I in Bits'Range loop
-         if 1 = (I mod 2) and Bits (I) then
-            Put ("," & I'Image);
-         end if;
-      end loop;
-      Put (" ]");
-      New_Line;
-   end Print_Primes;
+   -- Constants
 
    Expected_Array : constant Results_Array := (
        1 => 4,            -- 10 **  1 =>             10
@@ -93,19 +36,88 @@ procedure Main is
       10 => 455_052_511); -- 10 ** 10 => 10_000_000_000
    Sieve_Size : constant Int_64 := 10 ** Test_Power;
    Expected   : constant Int_64 := Expected_Array (Test_Power);
+   Q          : constant Int_64 := Int_64 (Sqrt (Long_Long_Float (Sieve_Size)));
 
-   Passes   : Int_64 := 0;
-   Bits     : Bit_Array (3 .. Sieve_Size);
-   Start    : Time := Clock;
-   Finish   : Time := Start + Test_Duration;
+   procedure Run_Sieve (Bits : in Bit_Array_Access; Sieve_Size : in Int_64) is
+      use Ada.Numerics.Long_Long_Elementary_Functions;
+      Factor : Int_64 := 3;
+      Num    : Int_64;
+   begin
+      while Factor <= Q loop
+         Num := Factor;
+         Inner : while Num < Sieve_Size loop
+            if Bits.all (Num) then
+               Factor := Num;
+               exit Inner;
+            end if;
+            Num := Num + 2;
+         end loop Inner;
+         Num := Factor * Factor;
+         while Num < Sieve_Size loop
+            Bits.all (Num) := False;
+            Num        := Num + Factor * 2;
+         end loop;
+         Factor := Factor + 2;
+      end loop;
+   end Run_Sieve;
+
+   function Count_Primes (Bits : in Bit_Array_Access) return Int_64 is
+      Count : Int_64 := 1;
+   begin
+      for I in Bits.all'Range loop
+         if 1 = (I mod 2) and Bits.all (I) then
+            Count := Count + 1;
+         end if;
+      end loop;
+      return Count;
+   end Count_Primes;
+
+   procedure Print_Result (
+      Passes   : in Int_64;
+      Elapsed  : in Time_Span;
+      Limit    : in Int_64;
+      Expected : in Int_64;
+      Actual   : in Int_64) 
+   is
+      Elapsed_Duration : Duration := To_Duration (Elapsed);
+      Average          : Duration := Elapsed_Duration / Integer (Passes);
+   begin
+      Put ("Passes:" & Passes'Image & ", ");
+      Put ("Time:" & Elapsed_Duration'Image & ", ");
+      Put ("Average:" & Average'Image & ", ");
+      Put ("Limit:" & Limit'Image & ", ");
+      Put ("Expected:" & Expected'Image & ", ");
+      Put ("Actual:" & Actual'Image & ", ");
+      Put ("Valid: " & Boolean'Image (Expected = Actual));
+      New_Line;
+   end Print_Result;
+
+   procedure Print_Primes (Bits : in Bit_Array_Access) is begin
+      Put ("[ 2");
+      for I in Bits.all'Range loop
+         if 1 = (I mod 2) and Bits.all (I) then
+            Put ("," & I'Image);
+         end if;
+      end loop;
+      Put (" ]");
+      New_Line;
+   end Print_Primes;
+
+   Passes : Int_64 := 0;
+   Bits   : Bit_Array_Access := new Bit_Array(3 .. Sieve_Size);
+   Start  : Time := Clock;
+   Target : Time := Start + Test_Duration;
+   Finish : Time;
 begin
 
-   while Clock < Finish loop
-      Bits := (others => True);
+   while Clock < Target loop
+      Bits.all := (others => True);
       Run_Sieve (Bits, Sieve_Size);
       Passes := Passes + 1;
    end loop;
+   Finish := Clock;
    
-   Print_Result (Sieve_Size, Test_Duration, Passes, Expected, Count_Primes (Bits));
+   Print_Result (Passes, Finish - Start, Sieve_Size, Expected, Count_Primes (Bits));
    if Show_Primes then Print_Primes (Bits); end if;
+   Free (Bits);
 end Main;
