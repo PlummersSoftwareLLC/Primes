b5d4660988b6b91679ab7b73d0cd8c06de3fb6a5
Rutger van Bergen <rbergen@xs4all.nl>
2021-04-21 19:19:13 +0200

    Completed wrapper script and associated Docker file

--- PrimeMIXAL/solution_1/runprime.sh
+++ PrimeMIXAL/solution_1/runprime.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-`which time` -f '%e' mixvm -r prime | tr '[:upper:]' '[:lower:]' | sed -n "s/<time>/`cat \/tmp\/primetime.txt`/p"
+`which time` -f '%e' mixvm -r prime 2> /tmp/primetime.txt | tr '[:upper:]' '[:lower:]' > /tmp/primeoutput.txt
+sed -n "s/<time>/`cat \/tmp\/primetime.txt`/p" /tmp/primeoutput.txt
