ca53ce7ad8be1b128a1f6d94b0976097670cca17
Tudor Marghidanu <tudor@bit.fish>
2021-04-29 01:54:56 +0300

    Additional parsing rules

--- _tools/bin/report.py
+++ _tools/bin/report.py
@@ -1,18 +1,32 @@
 #!/usr/bin/env python3
 
 import argparse
-import os
 import glob
+import logging
+import os
 import re
+import sys
 import time
 
 import pandas as pd
 import pandas_profiling as pp
 
+# Setup Python logging to print messages to stdout
+log = logging.getLogger()
+log.setLevel(logging.INFO)
+handler = logging.StreamHandler(sys.stdout)
+formatter = logging.Formatter("%(levelname)s: %(message)s")
+handler.setFormatter(formatter)
+log.addHandler(handler)
+
+# Line parsing rules
+RULES = [
+    r"^(?P<label>.+)\s*;\s*(?P<passes>\d+)\s*;\s*(?P<duration>\d+([.]\d+)?)\s*;\s*(?P<threads>\d)$",
+    r"^Passes:\s+(?P<passes>\d+),?\s+Time:\s+(?P<duration>\d+[.]\d+),?\s+Avg:\s+(\d+[.]\d+),?\s+Limit:\s+(\d+),?\s+Count1:\s+(\d+),?\s+Count2:\s+(\d+),?\s+Valid:\s+(true|false|1|0|Yes|No)$",
+]
+
 
 def main(directory):
-    directory = os.path.abspath(directory)
-
     df = pd.DataFrame(
         columns=[
             "implementation",
@@ -25,34 +39,44 @@ def main(directory):
         ]
     )
 
-    for file in glob.glob(f"{directory}/**/*.out"):
+    directory = os.path.abspath(directory)
+    files = glob.glob(f"{directory}/**/*.out")
+    if not len(files):
+        raise Exception("No output files found!")
+
+    for file in files:
+        if os.stat(file).st_size == 0:
+            log.warning(f"File '{file}' appears to be empty. Skipping...")
+            continue
+
+        # Extracting additional information from the file name.
         name = os.path.splitext(os.path.basename(file))[0]
         metadata = name.split("-", 2)
 
+        log.info(f"Processing {name} ...")
         with open(file, "r") as handle:
             for line in handle.readlines():
-                match = re.match(
-                    r"^\s*(?P<label>.+)\s*;\s*(?P<passes>\d+)\s*;\s*(?P<duration>\d+([.]\d+)?)\s*;\s*(?P<threads>\d)\s*$",
-                    line,
-                )
+                for rule in RULES:
+                    match = re.match(rule, line.strip())
 
-                if not match:
-                    print("No matches found!")
-                    continue
+                    if match:
+                        data = match.groupdict()
+                        df = df.append(
+                            {
+                                "implementation": metadata[0],
+                                "solution": metadata[1],
+                                "os": metadata[2],
+                                "label": data.get("label", metadata[0]),
+                                "passes": data.get("passes"),
+                                "duration": data.get("duration"),
+                                "threads": data.get("threads", 1),
+                            },
+                            ignore_index=True,
+                        )
+                        break
 
-                df = df.append(
-                    {
-                        "implementation": metadata[0],
-                        "solution": metadata[1],
-                        "os": metadata[2],
-                        "label": match.group("label"),
-                        "passes": match.group("passes"),
-                        "duration": match.group("duration"),
-                        "threads": match.group("threads"),
-                    },
-                    ignore_index=True,
-                )
-
+    # Making sure the dataframe has the correct column types.
+    # Unfortunately these cannot be specified during creation.
     df = df.astype(
         {
             "implementation": "string",
@@ -64,11 +88,10 @@ def main(directory):
             "threads": int,
         }
     )
-    print(df.dtypes)
-
     df.sort_values(by=["passes", "duration"], ascending=False, inplace=True)
     print(df.to_string())
 
+    # Generate report
     profile = pp.ProfileReport(df, title="Software Drag Race", explorative=True)
     profile.to_file(f"dr-{time.time()}.html")
 
@@ -83,6 +106,10 @@ if __name__ == "__main__":
         help="Root directory",
         required=False,
     )
-
     args = parser.parse_args()
-    main(**args.__dict__)
+
+    try:
+        main(**args.__dict__)
+    except Exception as e:
+        log.error(e)
+        exit(1)
