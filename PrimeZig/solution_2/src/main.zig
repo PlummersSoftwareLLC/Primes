71282f85e2c580d4aa278abbbbf4f0265bfc718c
Jan Halsema <janhlsm@gmail.com>
2021-05-25 11:27:03 +0200

    add Ityonemo sieve to benchmark

--- PrimeZig/solution_2/src/main.zig
+++ PrimeZig/solution_2/src/main.zig
@@ -5,17 +5,107 @@ const time = std.time;
 const prime = @import("./prime.zig");
 
 pub fn main() anyerror!void {
-    const timer = try time.Timer.start();
-
     const size = 1_000_000;
     const run_for = 5; // Seconds
 
+    comptime const configs = .{
+        .{ bool, false, true },
+        .{ u1, 0, 1 },
+        .{ u8, 0, 1 },
+        .{ usize, 0, 1 },
+        .{ bool, true, false },
+        .{ u1, 1, 0 },
+        .{ u8, 1, 0 },
+        .{ usize, 1, 0 },
+        .{ u16, 0, 1 },
+        .{ u32, 0, 1 },
+        .{ u64, 0, 1 },
+    };
+
+    try runSieveBitTest(size, run_for);
+    try runItyonemoTest(size, run_for);
+    inline for (configs) |run| {
+        try runSieveTest(run[0], run[1], run[2], size, run_for);
+    }
+    inline for (configs) |run| {
+        try runUnrolledSieveTest(run[0], run[1], run[2], size, run_for);
+    }
+}
+
+fn runItyonemoTest(
+    comptime size: comptime_int,
+    run_for: comptime_int,
+) anyerror!void {
+    var allocator = std.heap.page_allocator;
+
+    const timer = try time.Timer.start();
+    var passes: u64 = 0;
+
+    var slice = try allocator.alloc(bool, size >> 1);
+    defer allocator.free(slice);
+
+    while (timer.read() < run_for * time.ns_per_s) : (passes += 1) {
+        var sieve = prime.ItyonemoSieve.init(slice, size);
+        sieve.run();
+    }
+    const elapsed = timer.read();
+
+    printResults("Ityonemo Sieve: ", passes, elapsed, size);
+}
+
+
+fn runSieveTest(
+    comptime Type: type,
+    comptime trueVal: Type,
+    comptime falseVal: Type,
+    size: comptime_int,
+    run_for: comptime_int,
+) anyerror!void {
+    const timer = try time.Timer.start();
     var passes: u64 = 0;
     while (timer.read() < run_for * time.ns_per_s) : (passes += 1) {
-        const field = [_]u8{0} ** size;
-        prime.Sieve(u8, 0, 1, size).init(field).run();
+        const field = [_]Type{trueVal} ** size;
+        prime.Sieve(Type, trueVal, falseVal, size).init(field).run();
     }
+    const elapsed = timer.read();
 
-    const elapsed = @intToFloat(f32, timer.read()) / @intToFloat(f32, time.ns_per_s);
-    try std.io.getStdOut().writer().print("ManDeJan;{};{d:.5};1\n", .{ passes, elapsed });
+    printResults("Regular Sieve Type: " ++ @typeName(Type), passes, elapsed, size);
+}
+
+
+fn runUnrolledSieveTest(
+    comptime Type: type,
+    comptime trueVal: Type,
+    comptime falseVal: Type,
+    size: comptime_int,
+    run_for: comptime_int,
+) anyerror!void {
+    const timer = try time.Timer.start();
+    var passes: u64 = 0;
+    while (timer.read() < run_for * time.ns_per_s) : (passes += 1) {
+        const field = [_]Type{trueVal} ** size;
+        prime.UnrolledSieve(Type, trueVal, falseVal, size).init(field).run();
+    }
+    const elapsed = timer.read();
+
+    printResults("Unrolled Sieve Type: " ++ @typeName(Type), passes, elapsed, size);
+}
+
+
+fn runSieveBitTest(comptime size: comptime_int, run_for: comptime_int) anyerror!void {
+    // @compileLog(@typeInfo(std));
+    const timer = try time.Timer.start();
+    var passes: u64 = 0;
+    while (timer.read() < run_for * time.ns_per_s) : (passes += 1) {
+        prime.BitSieve(size).init().run();
+    }
+    const elapsed = timer.read();
+    printResults("Bit set", passes, elapsed, size);
+}
+
+fn printResults(backing: []const u8, passes: usize, elapsed_ns: u64, limit: usize) void {
+    const elapsed = @intToFloat(f32, elapsed_ns) / @intToFloat(f32, time.ns_per_s);
+    print("{s:<26}, Passes: {:5}, Time: {d:.5}, Avg: {d:.5}, Limit: {}\n", .{
+        backing, passes, elapsed, elapsed / @intToFloat(f32, passes), limit,
+    });
 }
--- PrimeZig/solution_2/src/prime.zig
+++ PrimeZig/solution_2/src/prime.zig
@@ -173,6 +173,67 @@ pub fn UnrolledSieve(
     };
 }
 
+/// Prime number sieve.
+pub const ItyonemoSieve = struct {
+    limit: usize,
+    slice: []bool,
+
+    const Self = @This();
+
+    /// Initializes the sieve struct.  Please pass me a slice!
+    /// The length of the slice should be half the limit.
+    pub fn init(slice: []bool, limit: usize) Self {
+        std.debug.assert(slice.len == limit >> 1);
+        // set all odds to be prime, except 1, which is not prime
+        slice[0] = false;
+        std.mem.set(bool, slice[1..], true);
+        return Self{
+            .limit = limit,
+            .slice = slice
+        };
+    }
+
+    pub fn run(self: Self) void {
+        var factor : u64 = 3;  // start factor at "3"
+        // calculate the stopping point
+        const q = @floatToInt(u64, @sqrt(@intToFloat(f64, self.limit)));
+        const size = self.limit >> 1;
+
+        while (factor <= q) : (factor += 2) {
+            // search for the next factor to try, we can ignore composite numbers.
+            var num: u64 = factor;
+            factorSearch: while (num < size) : (num += 2) {
+                if (self.slice[num >> 1]) {
+                    factor = num;
+                    break :factorSearch;
+                }
+            }
+
+            // start filling out the sieve at factor * factor.
+            var start:u64 = (factor * factor) >> 1;
+            while (start < size) : (start += factor) {
+                self.slice[start] = false;
+            }
+        }
+    }
+
+    pub fn primeCount(self: Self) u64 {
+        var count: u64 = 1;  // start with 2, which we aren't counting using this fn
+        var idx: u64 = 0;
+
+        while (idx < (self.limit >> 1)) : (idx += 1) {
+            count += @boolToInt(self.slice[idx]);
+        }
+
+        return count;
+    }
+
+    pub fn isPrime(self: Self, result: []bool) void {
+        for (result) | *val, idx | {
+            result[idx] = if (idx % 2 == 0) idx == 2 else self.slice[idx >> 1];
+        }
+    }
+};
 
 test "Test byte sieve" {
     const expected_results = .{
