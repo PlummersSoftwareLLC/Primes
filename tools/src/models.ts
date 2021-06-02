df298babd7002dac7c474a08dfe7801e2659933c
Tudor <tudor@marghidanu.com>
2021-06-02 18:57:31 +0300

    Removed sensitive information

--- tools/src/models.ts
+++ tools/src/models.ts
@@ -1,5 +1,3 @@
-import * as si from "systeminformation";
-
 export interface Result {
   implementation: string;
   solution: string;
@@ -8,6 +6,78 @@ export interface Result {
   duration: number;
   threads: number;
   tags: { [key: string]: string };
+}
+
+export interface Report {
+  version: string;
+  metadata: { [key: string]: string | number };
+  machine: {
+    cpu: CpuInfo;
+    os: OsInfo;
+    system: SystemInfo;
+    docker: DockerInfo;
+  };
+  results: Result[];
+}
+
+export interface CpuInfo {
+  manufacturer: string;
+  brand: string;
+  vendor: string;
+  family: string;
+  model: string;
+  stepping: string;
+  revision: string;
+  voltage: string;
+  speed: number;
+  speedMin: number;
+  speedMax: number;
+  governor: string;
+  cores: number;
+  physicalCores: number;
+  efficiencyCores?: number;
+  performanceCores?: number;
+  processors: number;
+  socket: string;
+  flags: string;
+  virtualization: boolean;
+  cache: {
+    l1d: number;
+    l1i: number;
+    l2: number;
+    l3: number;
+  };
+}
+
+export interface OsInfo {
+  platform: string;
+  distro: string;
+  release: string;
+  codename: string;
+  kernel: string;
+  arch: string;
+  codepage: string;
+  logofile: string;
+  build: string;
+  servicepack: string;
+  uefi: boolean;
+  hypervizor?: boolean;
+  remoteSession?: boolean;
+}
+
+export interface SystemInfo {
+  manufacturer: string;
+  model: string;
+  version: string;
+  sku: string;
+  virtual: boolean;
+  virtualHost?: string;
+  raspberry?: {
+    manufacturer: string;
+    processor: string;
+    type: string;
+    revision: string;
+  };
 }
 
 export interface DockerInfo {
@@ -19,16 +89,4 @@ export interface DockerInfo {
   ncpu: number;
   memTotal: number;
   serverVersion: string;
-}
-
-export interface Report {
-  version: string;
-  metadata: { [key: string]: string | number };
-  machine: {
-    cpu: si.Systeminformation.CpuData;
-    os: si.Systeminformation.OsData;
-    system: si.Systeminformation.SystemData;
-    docker: DockerInfo;
-  };
-  results: Result[];
 }
--- tools/src/services/report.ts
+++ tools/src/services/report.ts
@@ -8,15 +8,63 @@ export default class ReportService {
     const docker = new Docker();
     const dockerInfo = await docker.info();
 
+    const cpuInfo = await si.cpu();
+    const osInfo = await si.osInfo();
+    const systemInfo = await si.system();
+
     const report: Report = {
       version: '1',
       metadata: {
         date: Math.floor(new Date().getTime() / 1000)
       },
       machine: {
-        cpu: await si.cpu(),
-        os: await si.osInfo(),
-        system: await si.system(),
+        cpu: {
+          manufacturer: cpuInfo.manufacturer,
+          brand: cpuInfo.brand,
+          vendor: cpuInfo.vendor,
+          family: cpuInfo.family,
+          model: cpuInfo.model,
+          stepping: cpuInfo.stepping,
+          revision: cpuInfo.revision,
+          voltage: cpuInfo.voltage,
+          speed: cpuInfo.speed,
+          speedMin: cpuInfo.speedMin,
+          speedMax: cpuInfo.speedMax,
+          governor: cpuInfo.governor,
+          cores: cpuInfo.cores,
+          physicalCores: cpuInfo.physicalCores,
+          efficiencyCores: cpuInfo.efficiencyCores,
+          performanceCores: cpuInfo.performanceCores,
+          processors: cpuInfo.processors,
+          socket: cpuInfo.socket,
+          flags: cpuInfo.flags,
+          virtualization: cpuInfo.virtualization,
+          cache: cpuInfo.cache
+        },
+        os: {
+          platform: osInfo.platform,
+          distro: osInfo.distro,
+          release: osInfo.release,
+          codename: osInfo.codename,
+          kernel: osInfo.kernel,
+          arch: osInfo.arch,
+          codepage: osInfo.codepage,
+          logofile: osInfo.logofile,
+          build: osInfo.build,
+          servicepack: osInfo.servicepack,
+          uefi: osInfo.uefi,
+          hypervizor: osInfo.hypervizor,
+          remoteSession: osInfo.remoteSession
+        },
+        system: {
+          manufacturer: systemInfo.manufacturer,
+          model: systemInfo.model,
+          version: systemInfo.version,
+          sku: systemInfo.sku,
+          virtual: systemInfo.virtual,
+          virtualHost: systemInfo.virtualHost,
+          raspberry: systemInfo.raspberry
+        },
         docker: {
           kernelVersion: dockerInfo['KernelVersion'],
           operatingSystem: dockerInfo['OperatingSystem'],
