38c826678a52a37b8a864465410562d330002091
Rutger van Bergen <rbergen@xs4all.nl>
2021-06-05 09:17:58 +0200

    Fix duration calculation rounding error

--- PrimeCPP/solution_1/PrimeCPP.cpp
+++ PrimeCPP/solution_1/PrimeCPP.cpp
@@ -131,7 +131,7 @@ int main()
         passes++;
         if (duration_cast<seconds>(steady_clock::now() - tStart).count() >= 5)
         {
-            sieve.printResults(false, duration_cast<microseconds>(steady_clock::now() - tStart).count() / 1000000, passes);
+            sieve.printResults(false, duration_cast<microseconds>(steady_clock::now() - tStart).count() / 1000000.0, passes);
             break;
         }
     } 
