c90bdd148f75c32e79d3d16d65ec1a70ef6bc0b8
Frank van Bakel <fvbakel@gmail.com>
2021-06-13 20:43:26 +0200

    Big performance improvement (20x faster) and output in drag-race format

--- PrimeCOBOL/solution_1/primes.cbl
+++ PrimeCOBOL/solution_1/primes.cbl
@@ -4,25 +4,28 @@
        ENVIRONMENT DIVISION.
        DATA DIVISION.
        WORKING-STORAGE SECTION.
-       77  MAX_LIMIT                      PIC 9(7) VALUE 1000000.
-       77  SHOW_RESULTS                   PIC 9(2) VALUE 0.
-       77  MAX_TIME_SEC                   PIC 9(4) VALUE 5. 
-       77  MAX_TIME_HS                    PIC 9(9).
-       77  DURATION-SEC                   PIC 9(1)V9.     
-       77  PASSES                         PIC 9(5). 
-       77  MAX_ROOT                       PIC 9(7).
-       77  MAX_ROOT_INDEX                 PIC 9(7).
-       77  START-AT                       PIC 9(7).
-       77  PRIME                          PIC 9(7).
-       77  PRIME-COUNT                    PIC 9(7).
-       77  FACTOR                         PIC 9(7).
-       77  STEP-SIZE                      PIC 9(7).
-       77  K                              PIC 9(7).
-       77  I                              PIC 9(7).
-       77  IS-EVEN                        PIC 9(1)v9.
-       77  BIT_SIZE                       PIC 9(7).
+       77  MAX_LIMIT                      PIC 9(7)   COMP VALUE 1000000.
+       77  SHOW_RESULTS                   PIC 9(2)   COMP VALUE 0.
+       77  MAX_TIME_SEC                   PIC 9(4)   COMP VALUE 5. 
+       77  MAX_TIME_HS                    PIC 9(9)   COMP.
+       77  DURATION-SEC                   PIC 9(1)V9 COMP-3.     
+       77  PASSES                         PIC 9(5)   COMP.
+       77  AVG                            PIC 9(5)v9 COMP-3.
+       77  VAL-RES                        PIC X(5).   
+       77  MAX_ROOT                       PIC 9(7)   COMP.
+       77  MAX_ROOT_INDEX                 PIC 9(7)   COMP.
+       77  START-AT                       PIC 9(7)   COMP.
+       77  PRIME                          PIC 9(7)   COMP.
+       77  PRIME-COUNT                    PIC 9(7)   COMP.
+       77  FACTOR                         PIC 9(7)   COMP.
+       77  STEP-SIZE                      PIC 9(7)   COMP.
+       77  I                              PIC 9(7)   COMP.
+       77  IS-EVEN                        PIC 9(1)v9 COMP-3.
+       77  BIT_SIZE                       PIC 9(7)   COMP.
        01  BIT-ARRAY.
-            03 FLAG OCCURS 500000 TIMES     PIC 9(1).
+            03 FLAG PIC 1(1) 
+               OCCURS 500000 TIMES
+               INDEXED BY Z.
        01  WS-TIMES.
            03  WS-TIME                    PIC 9(8).
            03  WS-TIME-R REDEFINES WS-TIME.
@@ -40,21 +43,15 @@
            MOVE ZERO TO PASSES.
            PERFORM GET-NOW THROUGH END-GET-NOW.
            MOVE NOW-HS TO START-HS.
-           PERFORM TIME-LOOP UNTIL DURATION-HS >= MAX_TIME_HS. 
-           PERFORM COUNT-PRIMES THROUGH END-COUNT-PRIMES.
-           DIVIDE 100 INTO DURATION-HS GIVING DURATION-SEC.
-           DISPLAY "PASSES=",PASSES.
-           DISPLAY "DURATION-SEC=",DURATION-SEC.
+           PERFORM UNTIL DURATION-HS >= MAX_TIME_HS
+               PERFORM RUN_SIEVE THROUGH END-RUN_SIEVE
+               ADD 1 TO PASSES
+               PERFORM GET-NOW THROUGH END-GET-NOW
+               COMPUTE DURATION-HS = NOW-HS - START-HS
+           END-PERFORM.
+           PERFORM PRINT_RESULTS THROUGH END-PRINT_RESULTS.
 
            STOP RUN.  
-      *
-       TIME-LOOP.
-           PERFORM RUN_SIEVE THROUGH END-RUN_SIEVE.
-           ADD 1 TO PASSES.
-           PERFORM GET-NOW THROUGH END-GET-NOW.
-           COMPUTE DURATION-HS = NOW-HS - START-HS.
-       END-TIME-LOOP.
-           EXIT.
       *
        RUN_SIEVE.
            COMPUTE MAX_ROOT ROUNDED = (MAX_LIMIT ** .5).
@@ -67,38 +64,48 @@
            DIVIDE 2 INTO MAX_LIMIT
                GIVING BIT_SIZE ROUNDED
                REMAINDER IS-EVEN.
-            IF IS-EVEN = 0 THEN 
+           IF IS-EVEN = 0 THEN 
                    ADD -1 TO BIT_SIZE.    
-           MOVE 1 TO I.
-           PERFORM INIT-BITS BIT_SIZE TIMES.
+           PERFORM VARYING Z 
+                   FROM 1 BY 1 
+                   UNTIL Z>BIT_SIZE
+               MOVE 1 TO FLAG (Z)
+           END-PERFORM.
            MOVE 1 TO FACTOR.
-           PERFORM SCAN-FOR-PRIMES UNTIL FACTOR > MAX_ROOT_INDEX.
-       END-RUN_SIEVE.
-           EXIT.
-      * 
-       INIT-BITS.
-           MOVE 1 TO FLAG (I).
-           ADD 1 TO I.
-       END-INIT-BITS.
-           EXIT.
-      * 
-       SCAN-FOR-PRIMES.
-           IF FLAG (FACTOR) = 1 THEN
+           PERFORM UNTIL FACTOR > MAX_ROOT_INDEX
+               IF FLAG (FACTOR) = 1 THEN
                    ADD FACTOR FACTOR 1 GIVING PRIME
                    ADD FACTOR FACTOR 1 GIVING STEP-SIZE
                    COMPUTE START-AT = (((PRIME * PRIME) - 1) / 2)
-                   MOVE START-AT TO K
-                   PERFORM STRIKOUT UNTIL K > BIT_SIZE.
-           ADD 1 TO FACTOR.
-       END-SCAN-FOR-PRIMES.
-           EXIT.
-      * 
-       STRIKOUT.
-           MOVE 0 TO FLAG (K).
-           ADD STEP-SIZE TO K.
-       END-STRIKOUT.
+                   PERFORM VARYING Z 
+                           FROM START-AT BY STEP-SIZE 
+                           UNTIL Z>BIT_SIZE
+                       MOVE 0 TO FLAG (Z)
+                   END-PERFORM
+               END-IF
+               ADD 1 TO FACTOR
+           END-PERFORM.
+       END-RUN_SIEVE.
            EXIT.
       *
+       PRINT_RESULTS.
+           DIVIDE 100 INTO DURATION-HS GIVING DURATION-SEC.
+           DIVIDE PASSES INTO DURATION-SEC GIVING AVG.
+           PERFORM COUNT-PRIMES THROUGH END-COUNT-PRIMES.
+           MOVE "True" TO VAL-RES.
+           DISPLAY "Passes: ",PASSES,
+                   ", Time: ",DURATION-SEC,
+                   ", Avg: ",AVG,
+                   "(sec/pass), Limit: ",MAX_LIMIT,
+                   ", Count: ",PRIME-COUNT,
+                   ", Valid: ",VAL-RES.
+           DISPLAY " " *>Workaround to display empty new line
+           DISPLAY "fvbakel_Cobol;",PASSES,
+                   ";",DURATION-SEC,
+                   ";1;algorithm=base,faithful=yes,bits=8".
+       END-PRINT_RESULTS.
+           EXIT.
+      * 
        COUNT-PRIMES.
            MOVE 2 TO PRIME
            IF SHOW_RESULTS = 1 THEN
@@ -106,7 +113,6 @@
            MOVE 1 TO PRIME-COUNT.
            MOVE 1 TO I.
            PERFORM COUNT-PRIMES-LOOP UNTIL I > BIT_SIZE.
-           DISPLAY "PRIMES FOUND: ", PRIME-COUNT.
        END-COUNT-PRIMES.
            EXIT.
       * 
