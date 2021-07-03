b08b6ea645fe4c4407bbf6bf65fce4cb29fc917c
TheMatt2 <TheMatt2@users.noreply.github.com>
2021-07-03 15:58:40 -0400

    Fix and add whitespace to sieve_5760of30030_par.c
    File mixed tabs and spaces. All changed to spaces. Also added spacing to 
    try and make the code more readable.
        
    THIS INTENTIONALLY DOES NOT CHANGE THE EXECUTABLE CREATED
        
    Changes are just for readability

--- PrimeC/solution_2/sieve_5760of30030_par.c
+++ PrimeC/solution_2/sieve_5760of30030_par.c
@@ -263,10 +263,10 @@ struct sieve_state {
 };
 
 struct sieve_state *create_sieve(int maxints) {
-  struct sieve_state *sieve_state=malloc(sizeof *sieve_state);
+  struct sieve_state *sieve_state = malloc(sizeof *sieve_state);
   // We need to store only odd integers, so only half the number of integers
-  sieve_state->a=calloc(maxints/2/sizeof(TYPE)+1,sizeof(TYPE));
-  sieve_state->maxints=maxints;
+  sieve_state->a = calloc((maxints / 2 / sizeof(TYPE)) + 1, sizeof(TYPE));
+  sieve_state->maxints = maxints;
   return sieve_state;
 }
 
@@ -277,155 +277,162 @@ void delete_sieve(struct sieve_state *sieve_state) {
 
 void run_sieve(struct sieve_state *sieve_state) {
 #ifdef _OPENMP
-  int numthreads=omp_get_max_threads();
+  int numthreads = omp_get_max_threads();
 #endif
-  unsigned int maxints=sieve_state->maxints;
-  TYPE *a=sieve_state->a;
-  unsigned int maxintsh=maxints>>1;
-  unsigned int q=(unsigned int)sqrt(maxints)+1U;
-  unsigned int step=1U; // From 17 to 19
-  unsigned int inc=steps[step]; // Next increment in steps array
+  unsigned int maxints = sieve_state->maxints;
+  TYPE *a = sieve_state->a;
+  unsigned int maxintsh = maxints >> 1U;
+  unsigned int q = (unsigned int) sqrt(maxints) + 1U;
+  unsigned int step = 1U; // From 17 to 19
+  unsigned int inc = steps[step]; // Next increment in steps array
   // Only check integers not divisible by 2, 3, 5, 7, 11, or 13
-  unsigned int factorh=17U>>1U; // We already have 2, 3, 5, 7, 11, and 13
-  unsigned int qh=q>>1U;
-  while (factorh<=qh) {
+  unsigned int factorh = 17U >> 1U; // We already have 2, 3, 5, 7, 11, and 13
+  unsigned int qh = q >> 1U;
+  while (factorh <= qh) {
     // Search for next prime
-    if (a[factorh>>SHIFT]&((TYPE)1<<(factorh&MASK))) {
-      factorh+=inc;
-      if (++step==5760U) step=0U; // End of steps array, start from the beginning
-      inc=steps[step];
+    if (a[factorh >> SHIFT] & ((TYPE) 1 << (factorh & MASK))) {
+      factorh += inc;
+      if (++step == 5760U) step = 0U; // End of steps array, start from the beginning
+      inc = steps[step];
       continue;
     }
     // Mask all integer multiples of this prime
-    unsigned int factor=(factorh<<1U)+1U;
+    unsigned int factor = (factorh << 1U) + 1U;
     // Use as many threads as possible if the workload is large enough
 #ifdef _OPENMP
-    if (maxints>factor*factor) {
-      unsigned int worksize=(maxints-factor*factor)/(2*factor);
-      if (worksize>(numthreads*300)) {
+    if (maxints > factor * factor) {
+      unsigned int worksize = (maxints - factor * factor) / (2 * factor);
+      if (worksize > (numthreads * 300)) {
 #pragma omp parallel
-	{
-	  unsigned int ithread=omp_get_thread_num();
-	  unsigned int firstbit=(factor*factor)>>1U;
-	  unsigned int lastbit=maxintsh;
-	  unsigned int workbits=(lastbit-firstbit)/factor;
-	  unsigned int threadworkbits=workbits/numthreads;
-	  unsigned int myfirstbit=firstbit+factor*threadworkbits*ithread;
-	  unsigned int mylastbit=firstbit+factor*threadworkbits*(ithread+1U)-factor;
-	  // Make sure I handle full words and do not start in a word updating by the previous thread
-	  if (ithread!=0U) {
-	    while (1) {
-	      unsigned int previous_thread_lastbit=myfirstbit-factor;
-	      unsigned int my_first_word=myfirstbit>>SHIFT;
-	      unsigned int previous_thread_last_word=previous_thread_lastbit>>SHIFT;
-	      if (my_first_word!=previous_thread_last_word)
-		break;
-	      else
-		myfirstbit+=factor;
-	    }
-	  }
-	  if (ithread==numthreads-1)
-	    mylastbit=lastbit;
-	  else {
-	    // Make sure I continue with more bits until the next cpu can handle the bits
-	    while (1) {
-	      unsigned int next_thread_firstbit=mylastbit+factor;
-	      unsigned int my_last_word=mylastbit>>SHIFT;
-	      unsigned int next_thread_first_word=next_thread_firstbit>>SHIFT;
-	      if (my_last_word!=next_thread_first_word)
-		break;
-	      else
-		mylastbit+=factor;
-	    }
-	  }
-	  for (unsigned int m = myfirstbit; m <= mylastbit; m += factor)
-	    a[m>>SHIFT]|=(TYPE)1<<(m&MASK);
-	}
+    {
+      unsigned int ithread = omp_get_thread_num();
+      unsigned int firstbit = (factor * factor) >> 1U;
+      unsigned int lastbit = maxintsh;
+      unsigned int workbits = (lastbit - firstbit) / factor;
+      unsigned int threadworkbits = workbits / numthreads;
+      unsigned int myfirstbit = firstbit + factor * threadworkbits * ithread;
+      unsigned int mylastbit = firstbit + factor * threadworkbits * (ithread + 1U) - factor;
+      // Make sure I handle full words and do not start in a word updating by the previous thread
+      if (ithread != 0U) {
+        while (1) {
+          unsigned int previous_thread_lastbit = myfirstbit - factor;
+          unsigned int my_first_word = myfirstbit >> SHIFT;
+          unsigned int previous_thread_last_word = previous_thread_lastbit >> SHIFT;
+          if (my_first_word != previous_thread_last_word)
+            break;
+          else
+            myfirstbit += factor;
+        }
+      }
+      if (ithread == numthreads - 1)
+        mylastbit = lastbit;
+      else {
+        // Make sure I continue with more bits until the next cpu can handle the bits
+        while (1) {
+          unsigned int next_thread_firstbit = mylastbit + factor;
+          unsigned int my_last_word = mylastbit >> SHIFT;
+          unsigned int next_thread_first_word = next_thread_firstbit >> SHIFT;
+          if (my_last_word != next_thread_first_word)
+            break;
+          else
+            mylastbit += factor;
+        }
+      }
+      for (unsigned int m = myfirstbit; m <= mylastbit; m += factor)
+        a[m >> SHIFT] |= (TYPE) 1 << (m & MASK);
+    }
       }
       else {
-	for (unsigned int m = (factor*factor)>>1; m < maxintsh; m += factor)
-	  a[m>>SHIFT]|=(TYPE)1<<(m&MASK);
+    for (unsigned int m = (factor * factor) >> 1; m < maxintsh; m += factor)
+      a[m >> SHIFT] |= (TYPE) 1 << (m & MASK);
       }
     }
 #else
-    for (unsigned int m = (factor*factor)>>1; m < maxintsh; m += factor)
-      a[m>>SHIFT]|=(TYPE)1<<(m&MASK);
+    for (unsigned int m = (factor * factor) >> 1; m < maxintsh; m += factor)
+      a[m >> SHIFT] |= (TYPE) 1 << (m & MASK);
 #endif
-    factorh+=inc;
-    if (++step==5760U) step=0U; // End of steps array, start from the beginning
-    inc=steps[step];
+    factorh += inc;
+    if (++step == 5760U) step = 0U; // End of steps array, start from the beginning
+    inc = steps[step];
   }
 }
 
 unsigned int count_primes(struct sieve_state *sieve_state) {
-  unsigned int maxints=sieve_state->maxints;
-  TYPE *a=sieve_state->a;
-  unsigned int ncount=6; // We already have 2, 3, 5, 7, 11, and 13 ...
-  unsigned int factor=17; // ...
-  unsigned int step=1; // From 17 to 19
-  unsigned int inc=steps[step]<<1U; // Next increment in steps array
-  while (factor<=maxints) {
-    if (!(a[factor>>(SHIFT+1U)]&((TYPE)1<<((factor>>1U)&MASK))))
+  unsigned int maxints = sieve_state->maxints;
+  TYPE *a = sieve_state->a;
+  unsigned int ncount = 6; // We already have 2, 3, 5, 7, 11, and 13 ...
+  unsigned int factor = 17; // ...
+  unsigned int step = 1; // From 17 to 19
+  unsigned int inc = steps[step] << 1U; // Next increment in steps array
+  while (factor <= maxints) {
+    if (!(a[factor >> (SHIFT + 1U)] & ((TYPE) 1 << ((factor >> 1U) & MASK))))
       ncount++;
-    factor+=inc;
-    if (++step==5760U) step=0U; // End of steps array, start from the beginning
-    inc=steps[step]<<1U;
+    factor += inc;
+    if (++step == 5760U) step = 0U; // End of steps array, start from the beginning
+    inc = steps[step] << 1U;
   }
   return ncount;
 }
 
 int main(int argc, char **argv) {
-  int maxints=1000000;
-  struct timespec t,t2;
-  if (argc>1)
-    sscanf(argv[1],"%d",&maxints);
+  int maxints = 1000000;
+  struct timespec t1, t2;
+  if (argc > 1)
+    sscanf(argv[1], "%d", &maxints);
   int valid_primes;
   switch(maxints) {
   case 10:
-    valid_primes=4;
+    valid_primes = 4;
     break;
   case 100:
-    valid_primes=25;
+    valid_primes = 25;
     break;
   case 1000:
-    valid_primes=168;
+    valid_primes = 168;
     break;
   case 10000:
-    valid_primes=1229;
+    valid_primes = 1229;
     break;
   case 100000:
-    valid_primes=9592;
+    valid_primes = 9592;
     break;
   case 1000000:
-    valid_primes=78498;
+    valid_primes = 78498;
     break;
   case 10000000:
-    valid_primes=664579;
+    valid_primes = 664579;
     break;
   case 100000000:
-    valid_primes=5761455;
+    valid_primes = 5761455;
     break;
   case 1000000000:
-    valid_primes=50847534;
+    valid_primes = 50847534;
     break;
   default:
-    valid_primes=-1;
+    valid_primes = -1;
   }
-  int passes=0;
+  int passes = 0;
   // The initial time
-  clock_gettime(CLOCK_MONOTONIC,&t);
+  clock_gettime(CLOCK_MONOTONIC, &t1);
   struct sieve_state *sieve_state;
   while (1) {
-    sieve_state=create_sieve(maxints);
+    sieve_state = create_sieve(maxints);
     run_sieve(sieve_state);
     passes++;
-    clock_gettime(CLOCK_MONOTONIC,&t2);
-    double elapsed_time=t2.tv_sec+t2.tv_nsec*1e-9-t.tv_sec-t.tv_nsec*1e-9;
-    if (elapsed_time>=5.) {
+    clock_gettime(CLOCK_MONOTONIC, &t2);
+    double elapsed_time = (
+        t2.tv_sec +
+        t2.tv_nsec * 1e-9 -
+        t1.tv_sec -
+        t1.tv_nsec * 1e-9);
+
+    if (elapsed_time >= 5.) {
       // Count the number of primes and validate the result
-      int nprimes=count_primes(sieve_state);
+      int nprimes = count_primes(sieve_state);
       //printf("valid=%d ",(nprimes==valid_primes));
-      printf("danielspaangberg_5760of30030_par;%d;%f;%d;algorithm=wheel,faithful=yes,bits=1\n", passes,elapsed_time,omp_get_max_threads() );
+      printf(
+          "danielspaangberg_5760of30030_par;%d;%f;%d;algorithm=wheel,faithful=yes,bits=1\n",
+           passes, elapsed_time, omp_get_max_threads());
       break;
     }
     delete_sieve(sieve_state);
