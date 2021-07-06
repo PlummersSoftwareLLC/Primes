75c5f5188c1b52fa3ebcd5b59a98b61e16092de7
Jakob K <bluefireolymp@gmail.com>
2021-07-07 00:53:37 +0200

    Improve overall Kotlin performance

--- PrimeKotlin/solution_1/src/main/kotlin/PrimeSieve.kt
+++ PrimeKotlin/solution_1/src/main/kotlin/PrimeSieve.kt
@@ -4,31 +4,33 @@ import kotlin.math.sqrt
 
 const val n = 1_000_000
 
-fun main() = runBlocking(Dispatchers.Default) {
-    // MULTI THREADED
+fun main() {
+    runBlocking(Dispatchers.Default) {
+        // MULTI THREADED
 
-    val cores = Runtime.getRuntime().availableProcessors()
+        val cores = Runtime.getRuntime().availableProcessors()
 
-    val primeSieveJobs = ArrayList<Job>()
+        val primeSieveJobs = ArrayList<Job>()
 
-    val iterations = AtomicInteger()
-    repeat(cores) {
-        primeSieveJobs += launch {
-            while (isActive) {
-                PrimeSieve(n).runSieve()
-                iterations.incrementAndGet()
+        val iterations = AtomicInteger()
+        repeat(cores) {
+            primeSieveJobs += launch {
+                while (isActive) {
+                    PrimeSieve(n).runSieve()
+                    iterations.incrementAndGet()
+                }
             }
         }
-    }
 
-    withContext(Dispatchers.IO) {
+        withContext(Dispatchers.IO) {
+            delay(5000)
+            primeSieveJobs.forEach { it.cancel() }
+            println("jakobk_kotlin_coroutines;${iterations.get()};5;${cores};algorithm=base,faithful=yes")
+        }
+
+        // cool down
         delay(5000)
-        primeSieveJobs.forEach { it.cancel() }
-        println("jakobk_kotlin_coroutines;${iterations.get()};5;${cores};algorithm=base,faithful=yes")
     }
-
-    // cool down
-    delay(5000)
 
     // SINGLE THREADED
 
@@ -46,19 +48,27 @@ fun main() = runBlocking(Dispatchers.Default) {
 }
 
 class PrimeSieve(private val size: Int) {
-    private val bits = BooleanArray((size + 1) / 2) { true }
+    private val bits = BooleanArray((size + 1) shr 1) // true = sieved (no prime); false = not sieved (is a prime number)
 
     fun runSieve() {
         var factor = 3
-        while (factor < sqrt(size.toDouble()).toInt()) {
-            for (num in factor until size step 2)
-                if (bits[num / 2]) {
+        val q = sqrt(size.toDouble()).toInt()
+        while (factor < q) {
+            var num = factor
+            while (num < size) {
+                if (!bits[num shr 1]) {
                     factor = num
                     break
                 }
+                num += 2
+            }
 
-            for (num in factor * 3 until size step factor * 2)
-                bits[num / 2] = false
+            num = factor * 3
+            val incAmount = factor shl 1
+            while (num < size) {
+                bits[num shr 1] = true
+                num += incAmount
+            }
 
             factor += 2
         }
