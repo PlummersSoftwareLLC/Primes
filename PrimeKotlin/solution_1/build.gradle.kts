4c75acfdf703fad096a2e1aa2c7a53ddcd21b94f
Jakob K <bluefireolymp@gmail.com>
2021-07-06 20:39:32 +0200

    Compile to Java 16

--- PrimeKotlin/solution_1/build.gradle.kts
+++ PrimeKotlin/solution_1/build.gradle.kts
@@ -9,3 +9,12 @@ repositories {
 dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.0")
 }
+
+tasks {
+    compileJava {
+        options.release.set(16)
+    }
+    compileKotlin {
+        kotlinOptions.jvmTarget = "16"
+    }
+}
