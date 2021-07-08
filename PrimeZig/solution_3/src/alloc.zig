025582075f82dbfdd4912964309d8f10db1db257
Isaac Yonemoto <isaac.yonemoto@gmail.com>
2021-07-08 01:14:51 -0500

    Faithful Zig solution_3, bitsize fix

--- PrimeZig/solution_3/src/alloc.zig
+++ PrimeZig/solution_3/src/alloc.zig
@@ -1,37 +1,41 @@
-//! implements a "somewhat dumb" but threadsafe and fast allocator.  It will
-//! allocate "blocks" of a particular size.  The first "block" allocated will
-//! be backed by a large memory space assigned at compile-time, that is usually
-//! set aside in the .bss segment of the executable.  Subsequent allocations,
-//! if desired, are performed by the backing allocator.
+//! implements a "somewhat dumb" but threadsafe and FAAAST allocator.  It will
+//! allocate "blocks" of memory using a backing allocator (page allocator is a good
+//! choice, but the world is your oyster).  Doesn't ever really release that memory
+//! back to the OS, because we want to be greedy jerks.  Marks freed memory bits as
+//! recycling (hence environmentally friendly).
+//!
+//! this shouldn't be objectionable, because 1) the C spec says returning free'd
+//! memory to the OS is *optional*, and GoLang is even worse, it *never* returns
+//! memory to the OS, which is why we call GoLang "the CLAM" at the poker table.
 //!
 //! this allocator only allows freeing, no other resizes.
 
 const std = @import("std");
 const Allocator = std.mem.Allocator;
-const Mutex = std.Thread.Mutex;
 
-pub fn SnappyAllocator(comptime size: usize) type {
+const Config = struct { single_threaded: bool = false };
+
+pub fn EnvironmentallyFriendlyBlockAllocator(config: Config) type {
+    const Mutex = if (config.single_threaded) std.Thread.Mutex.Dummy else std.Thread.Mutex;
+
     return struct {
         allocator: Allocator,
         backing_allocator: *Allocator,
         mutex: Mutex,
-        root_block: *[size]u8,
-        root_block_available: bool = true,
+
+        // no WAY we'll need more than 256 of these!
+        used_blocks: [256]?[]u8 = [1]?[]u8{null} ** 256,
+        free_blocks: [256]?[]u8 = [1]?[]u8{null} ** 256,
+        total_blocks: usize = 0,
 
         const Self = @This();
 
-        pub fn init(a: *Allocator, root_block: *[size]u8) Self {
+        pub fn init(a: *Allocator) Self {
             return Self{
                 .allocator = Allocator{ .allocFn = alloc, .resizeFn = resize },
-                .root_block = root_block,
                 .backing_allocator = a,
-                .mutex = Mutex{},
+                .mutex = Mutex{}, // substitute for std.Thread.Mutex.Dummy in the single-threaded case.
             };
-        }
-
-        // tests if the slice is bound to the root block
-        pub fn is_root(self: *Self, slice: []u8) bool {
-            return @ptrToInt(slice.ptr) == @ptrToInt(self.root_block);
         }
 
         fn alloc(
@@ -45,15 +49,21 @@ pub fn SnappyAllocator(comptime size: usize) type {
             const hold = this.mutex.acquire();
             defer hold.release();
 
-            if (this.root_block_available and (n <= size)) {
-                // note that the root_block is guaranteed to be correctly aligned no matter
-                // what the desired alignment is (up to std.mem.page_size)
-                this.root_block_available = false;
-                return this.root_block.*[0..n];
+            // search the previously allocated blocks for an available free slot.
+            for (this.free_blocks) |block, index| {
+                if (block) |b| {
+                    if (b.len == n) {
+                        this.mark_as_used(index);
+                        return b;
+                    }
+                }
             } else {
-                // punt to the backup.
-                return this.backing_allocator.allocAdvancedWithRetAddr(u8, 1, n, .at_least, ret_addr);
-            }
+                var new_block = try this.backing_allocator.alloc(u8, n);
+                if (this.total_blocks < 256) {
+                    this.save(new_block);
+                }
+                return new_block;
+           }
         }
 
         fn resize(
@@ -64,82 +74,169 @@ pub fn SnappyAllocator(comptime size: usize) type {
             len_align: u29,
             ret_addr: usize,
         ) Allocator.Error!usize {
+            // filter out attempts to do anything except free.
             if (new_len != 0) return error.OutOfMemory;
+
             const this = @fieldParentPtr(Self, "allocator", allocator);
             const hold = this.mutex.acquire();
             defer hold.release();
 
-            if (is_root(this, buf)) {
-                this.root_block_available = true;
-                return 0;
+            // search the existing blocks for this particular memory location.
+            // it's possible that it was allocated instead by the backing allocator.
+            for (this.used_blocks) |block, index| {
+                if (block) |b| {
+                    if (b.ptr == buf.ptr) {
+                        this.mark_as_free(index);
+                        return 0;
+                    }
+                }
             } else {
+                // not in there means it was done by the backing allocator.
                 return (try this.backing_allocator.resize(buf, 0)).len;
+            }
+        }
+
+        /// it's like a ledger.  This moves some memory from accounts receivable to accounts payable.
+        /// aka asset -> liability.
+        fn mark_as_used(self: *Self, free_index: usize) void {
+            for (self.used_blocks) |block, used_index| {
+                if (block == null) {
+                    self.used_blocks[used_index] = self.free_blocks[free_index];
+                    self.free_blocks[free_index] = null;
+                    return;
+                }
+            } else unreachable; // mathematically impossible to get here without cosmic rays.
+        }
+
+        /// it's like a ledger.  This moves some memory from accounts payable to accounts receviable.
+        /// aka liability -> asset.
+        fn mark_as_free(self: *Self, used_index: usize) void {
+            for (self.free_blocks) |block, free_index| {
+                if (block == null) {
+                    self.free_blocks[free_index] = self.used_blocks[used_index];
+                    self.used_blocks[used_index] = null;
+                    return;
+                }
+            } else unreachable; // mathematically impossible to get here without cosmic rays.
+        }
+
+        /// this one is a bit like the federal reserve bank.  It prints money, I mean, memory out of nowhere
+        fn save(self: *Self, new_buffer: []u8) void {
+            for (self.used_blocks) |block, used_index| {
+                if (block == null) {
+                    self.used_blocks[used_index] = new_buffer;
+                    self.total_blocks += 1;
+                    return;
+                }
+            } else unreachable; // mathematically impossible to get here without cosmic rays.
+        }
+
+        /// cleans up all stuff that hasn't been released.  Note that we want this because there's
+        /// probably a bunch of free blocks (at least) that the main code *thought* was gone because
+        /// it had called free (but we lied to it tee hee).  Don't freak out!  Most industrial-strength
+        /// allocators also lie to the code, it's really none of the code's business whether the OS got
+        /// the memory back;  At least, the C standard says so.
+        pub fn deinit(self: *Self) void {
+            for (self.used_blocks) |block| {
+                if (block) |b| {
+                    self.backing_allocator.free(b);
+                }
+            }
+
+            for (self.free_blocks) |block| {
+                if (block) |b| {
+                    self.backing_allocator.free(b);
+                }
             }
         }
     };
 }
 
-const scratchpad_t = if (std.builtin.is_test) [256]u8 else u0;
-
-var scratchpad: scratchpad_t align(std.mem.page_size) = undefined;
-
-const TestAllocator = SnappyAllocator(256);
 const backing_allocator = std.testing.allocator;
 const assert = std.debug.assert;
 
-test "allocator works when you allocate stuff smaller than the blocksize" {
-    var test_allocator = TestAllocator.init(backing_allocator, &scratchpad);
-    defer assert(test_allocator.root_block_available);
+test "allocator can allocate stuff and it winds up in used_blocks" {
+    var a = EnvironmentallyFriendlyBlockAllocator(.{}).init(backing_allocator);
+    defer a.deinit();
 
-    var allocator = &test_allocator.allocator;
+    var alloc = &a.allocator;
 
-    var slice1 = try allocator.alloc(u8, 128);
-    defer allocator.free(slice1);
+    var mem1 = try alloc.alloc(u8, 256);
 
-    var slice2 = try allocator.alloc(u8, 128);
-    defer allocator.free(slice2);
-
-    assert(test_allocator.is_root(slice1));
-    assert(!test_allocator.is_root(slice2));
-    assert(!test_allocator.root_block_available);
+    // verify that it's in used_blocks
+    try std.testing.expectEqual(mem1.ptr, a.used_blocks[0].?.ptr);
 }
 
-test "allocator works when you allocate stuff of the same size as the blocksize" {
-    var test_allocator = TestAllocator.init(backing_allocator, &scratchpad);
-    defer assert(test_allocator.root_block_available);
+test "allocator can deallocate stuff and it winds up in free_blocks" {
+    var a = EnvironmentallyFriendlyBlockAllocator(.{}).init(backing_allocator);
+    defer a.deinit();
 
-    var allocator = &test_allocator.allocator;
+    var alloc = &a.allocator;
 
-    var slice1 = try allocator.alloc(u8, 256);
-    defer allocator.free(slice1);
+    var mem1 = try alloc.alloc(u8, 256);
 
-    var slice2 = try allocator.alloc(u8, 256);
-    defer allocator.free(slice2);
+    alloc.free(mem1);
 
-    assert(test_allocator.is_root(slice1));
-    assert(!test_allocator.is_root(slice2));
-    assert(!test_allocator.root_block_available);
+    // verify that it's in free_blocks
+    // verify that it's not in used_blocks
+    try std.testing.expectEqual(a.used_blocks[0], null);
+    try std.testing.expectEqual(mem1.ptr, a.free_blocks[0].?.ptr);
 }
 
-test "allocator works when you allocate stuff bigger than the blocksize" {
-    var test_allocator = TestAllocator.init(backing_allocator, &scratchpad);
-    defer assert(test_allocator.root_block_available);
+test "allocator can allocate and deallocate stuff out of order" {
+    var a = EnvironmentallyFriendlyBlockAllocator(.{}).init(backing_allocator);
+    defer a.deinit();
 
-    var allocator = &test_allocator.allocator;
+    var alloc = &a.allocator;
+    var mem1 = try alloc.alloc(u8, 256);
+    var mem2 = try alloc.alloc(u8, 256);
+    alloc.free(mem1);
 
-    var slice1 = try allocator.alloc(u8, 512);
-    defer allocator.free(slice1);
+    // verify that it's in used_blocks
+    try std.testing.expectEqual(a.used_blocks[0], null);
+    try std.testing.expectEqual(mem2.ptr, a.used_blocks[1].?.ptr);
+    try std.testing.expectEqual(mem1.ptr, a.free_blocks[0].?.ptr);
+}
 
-    var slice2 = try allocator.alloc(u8, 512);
-    defer allocator.free(slice2);
+test "also for the singlethreaded case." {
+    var a = EnvironmentallyFriendlyBlockAllocator(.{ .single_threaded = true }).init(backing_allocator);
+    defer a.deinit();
 
-    assert(!test_allocator.is_root(slice1));
-    assert(!test_allocator.is_root(slice2));
-    assert(test_allocator.root_block_available);
+    var alloc = &a.allocator;
+    var mem1 = try alloc.alloc(u8, 256);
+    var mem2 = try alloc.alloc(u8, 256);
+    alloc.free(mem1);
 
-    var slice3 = try allocator.alloc(u8, 128);
-    defer allocator.free(slice3);
+    // verify that it's in used_blocks
+    try std.testing.expectEqual(a.used_blocks[0], null);
+    try std.testing.expectEqual(mem2.ptr, a.used_blocks[1].?.ptr);
+    try std.testing.expectEqual(mem1.ptr, a.free_blocks[0].?.ptr);
+}
 
-    assert(test_allocator.is_root(slice3));
-    assert(!test_allocator.root_block_available);
+test "allocator can more than 256 things" {
+    var a = EnvironmentallyFriendlyBlockAllocator(.{}).init(backing_allocator);
+    defer a.deinit();
+
+    var alloc = &a.allocator;
+    var idx: usize = 0;
+    while (idx < 256) : (idx += 1) {
+        // allocate the page and throw it away BYEE
+        _ = try alloc.alloc(u8, 256);
+    }
+
+    // all good.  Don't forget to manually clear this one, because it can't be
+    // handled by deinit.
+    var last_memory = try alloc.alloc(u8, 1024);
+    defer alloc.free(last_memory);
+}
+
+test "allocator can create arrays" {
+    var a = EnvironmentallyFriendlyBlockAllocator(.{}).init(backing_allocator);
+    defer a.deinit();
+    var alloc = &a.allocator;
+    var idx: usize = 0;
+    while (idx < 10) : (idx += 1) {
+        _ = try alloc.alloc(u8, 500000);
+    }
+    _ = try alloc.create([15625]u32);
 }
--- PrimeZig/solution_3/src/main.zig
+++ PrimeZig/solution_3/src/main.zig
@@ -1,6 +1,5 @@
 const std = @import("std");
 const heap = std.heap;
-const print = std.debug.print;
 const time = std.time;
 const sieves = @import("./sieves.zig");
 const IntSieve = sieves.IntSieve;
@@ -10,17 +9,21 @@ const SingleThreadedRunner = runners.SingleThreadedRunner;
 const ParallelAmdahlRunner = runners.AmdahlRunner;
 const ParallelGustafsonRunner = runners.GustafsonRunner;
 
-const Allocator = @import("alloc.zig").SnappyAllocator;
+const Allocator = @import("alloc.zig").EnvironmentallyFriendlyBlockAllocator;
 
 const SIZE = 1_000_000;
 
 var scratchpad: [SIZE]u8 align(std.mem.page_size) = undefined;
 
 pub fn main() anyerror!void {
+    // exists so that we don't run out of compiler credits.  Zig compiler is stingier than AWS.
     @setEvalBranchQuota(100000);
 
     const run_for = 5; // Seconds
-    var allocator = &Allocator(SIZE).init(std.heap.page_allocator, &scratchpad).allocator;
+    var single_threaded_allocator = Allocator(.{ .single_threaded = true }).init(std.heap.page_allocator);
+    defer single_threaded_allocator.deinit();
+    var multithreaded_allocator = Allocator(.{}).init(std.heap.page_allocator);
+    defer multithreaded_allocator.deinit();
 
     // check for the --all flag.
     const all = (std.os.argv.len == 2) and (std.mem.eql(u8, std.mem.spanZ(std.os.argv[1]), "--all"));
@@ -29,18 +32,19 @@ pub fn main() anyerror!void {
     comptime const BitSieveDataTypes = .{ u8, u16, u32, u64 };
 
     comptime const specs = .{
-        .{ SingleThreadedRunner, IntSieve, .{}, false },
-        .{ ParallelAmdahlRunner, IntSieve, .{}, false },
-        .{ ParallelGustafsonRunner, IntSieve, .{}, false },
-        .{ ParallelAmdahlRunner, IntSieve, .{ .no_ht = true }, false },
-        .{ ParallelGustafsonRunner, IntSieve, .{ .no_ht = true }, false },
-        .{ SingleThreadedRunner, BitSieve, .{}, false },
-        .{ ParallelGustafsonRunner, BitSieve, .{}, false },
-        .{ ParallelGustafsonRunner, BitSieve, .{ .no_ht = true }, false },
-        .{ SingleThreadedRunner, IntSieve, .{}, true },
-        .{ SingleThreadedRunner, BitSieve, .{}, true },
-        .{ ParallelGustafsonRunner, BitSieve, .{}, true },
-        .{ ParallelGustafsonRunner, BitSieve, .{ .no_ht = true }, true },
+        .{ SingleThreadedRunner, IntSieve, false, false },
+        .{ ParallelAmdahlRunner, IntSieve, false, false },
+        .{ ParallelAmdahlRunner, IntSieve, true, false },
+        .{ ParallelGustafsonRunner, IntSieve, false, false },
+        .{ ParallelGustafsonRunner, IntSieve, true, false },
+        .{ SingleThreadedRunner, BitSieve, false, false },
+        .{ ParallelGustafsonRunner, BitSieve, false, false },
+        .{ ParallelGustafsonRunner, BitSieve, true, false },
+        .{ SingleThreadedRunner, IntSieve, false, true },
+        .{ SingleThreadedRunner, BitSieve, false, true },
+        .{ ParallelGustafsonRunner, BitSieve, false, true },
+        .{ ParallelGustafsonRunner, BitSieve, true, true },
+
     };
 
     // number of pregenerated primes in the wheel
@@ -49,20 +53,24 @@ pub fn main() anyerror!void {
     inline for (specs) |spec| {
         comptime const RunnerFn = spec[0];
         comptime const SieveFn = spec[1];
-        comptime const runner_opts = spec[2];
+        comptime const no_ht_opt = spec[2];
         comptime const wheel = spec[3];
+
+        comptime const runner_opts = if (no_ht_opt) .{.no_ht = true} else .{};
+
+        var allocator = if (RunnerFn == SingleThreadedRunner) &single_threaded_allocator.allocator else &multithreaded_allocator.allocator;
 
         if (wheel) {
             comptime const DataTypes = if (SieveFn == IntSieve) .{u8} else BitSieveDataTypes;
 
             inline for (DataTypes) |Type| {
-                comptime const typebits = if (SieveFn == IntSieve) @bitSizeOf(Type) else 1;
+                comptime const typebits = if (SieveFn == IntSieve) 8 * @sizeOf(Type) else 1;
                 inline for (pregens) |pregen| {
-                    comptime const Sieve = SieveFn(Type, SIZE, .{ .pregen = pregen });
+                    comptime const Sieve = SieveFn(Type, .{ .pregen = pregen });
                     comptime const Runner = RunnerFn(Sieve, runner_opts);
-                    comptime const selected = selected_runs(Runner);
+                    comptime const selected = selected_runs(Runner, no_ht_opt);
                     if (all or selected) {
-                        try runSieveTest(Runner, run_for, allocator, wheel, typebits);
+                        try runSieveTest(Runner, run_for, allocator, wheel, typebits, SIZE);
                     }
                 }
             }
@@ -70,19 +78,26 @@ pub fn main() anyerror!void {
             comptime const DataTypes = if (SieveFn == IntSieve) AllDataTypes else BitSieveDataTypes;
 
             inline for (DataTypes) |Type| {
-                comptime const typebits = if (SieveFn == IntSieve) @bitSizeOf(Type) else 1;
-                comptime const Sieve = SieveFn(Type, SIZE, .{});
+                comptime const typebits = if (SieveFn == IntSieve) 8 * @sizeOf(Type) else 1;
+                comptime const Sieve = SieveFn(Type, .{});
                 comptime const Runner = RunnerFn(Sieve, runner_opts);
-                comptime const selected = selected_runs(Runner);
+                comptime const selected = selected_runs(Runner, no_ht_opt);
                 if (all or selected) {
-                    try runSieveTest(Runner, run_for, allocator, wheel, typebits);
+                    try runSieveTest(Runner, run_for, allocator, wheel, typebits, SIZE);
                 }
             }
         }
     }
 }
 
-fn selected_runs(comptime Runner: type) bool {
+
+const widthstring = switch (std.builtin.target.cpu.arch.ptrBitWidth()) {
+    32 => "u32",
+    64 => "u64",
+    else => unreachable
+};
+
+fn selected_runs(comptime Runner: type, comptime no_ht_opt: bool) bool {
     const selections = .{
         "single-sieve-bool",
         "single-sieve-u8",
@@ -90,25 +105,21 @@ fn selected_runs(comptime Runner: type) bool {
         "parallel-gustafson-sieve-u8",
         "parallel-amdahl-sieve-u8",
         "parallel-gustafson-sieve-u8",
-        "single-bitSieve-u32",
-        "single-bitSieve-u64",
-        "parallel-gustafson-bitSieve-u32",
-        "parallel-gustafson-bitSieve-u64",
+        "single-bitSieve-u8",
+        "parallel-gustafson-bitSieve-u8",
+        "parallel-gustafson-bitSieve-" ++ widthstring,
         "single-sieve-u8-480of2310",
         "single-sieve-u8-5760of30030",
         "single-bitSieve-u8-480of2310",
         "single-bitSieve-u8-5760of30030",
-        "single-bitSieve-u32-480of2310",
-        "single-bitSieve-u32-5760of30030",
-        "single-bitSieve-u64-480of2310",
-        "single-bitSieve-u64-5760of30030",
+        "single-bitSieve-" ++ widthstring ++ "-480of2310",
+        "single-bitSieve-" ++ widthstring ++ "-5760of30030",
         "parallel-gustafson-bitSieve-u8-480of2310",
         "parallel-gustafson-bitSieve-u8-5760of30030",
-        "parallel-gustafson-bitSieve-u32-480of2310",
-        "parallel-gustafson-bitSieve-u32-5760of30030",
-        "parallel-gustafson-bitSieve-u64-480of2310",
-        "parallel-gustafson-bitSieve-u64-5760of30030",
+        "parallel-gustafson-bitSieve-" ++ widthstring ++ "-480of2310",
+        "parallel-gustafson-bitSieve-" ++ widthstring ++ "-5760of30030",
     };
+    if (no_ht_opt) return false;
     for (selections) |selection| {
         if (std.mem.eql(u8, selection, Runner.name)) return true;
     }
@@ -120,18 +131,21 @@ fn runSieveTest(
     run_for: comptime_int,
     allocator: *std.mem.Allocator,
     wheel: bool,
-    bits: usize
+    bits: usize,
+    sieve_size: usize,
 ) anyerror!void {
+    @setAlignStack(256);
     const timer = try time.Timer.start();
     var passes: u64 = 0;
-    var runner = Runner{};
 
-    try runner.init(allocator);
+    var runner = try Runner.init(allocator, sieve_size, &passes);
     defer runner.deinit();
 
-    while (timer.read() < run_for * time.ns_per_s) : (passes += 1) {
-        runner.reset();
-        runner.run(&passes);
+    while (timer.read() < run_for * time.ns_per_s) {
+        try runner.sieveInit();
+        defer runner.sieveDeinit();
+
+        runner.run();
     }
 
     const elapsed = timer.read();
@@ -145,7 +159,5 @@ fn printResults(backing: []const u8, passes: usize, elapsed_ns: u64, threads: us
     const stdout = std.io.getStdOut().writer();
     const algo = if (wheel) "wheel" else "base";
 
-    try stdout.print("{s};{};{d:.5};{};faithful=no,algorithm={s},bits={}\n", .{
-        backing, passes, elapsed, threads, algo, bits
-    });
+    try stdout.print("{s};{};{d:.5};{};faithful=yes,algorithm={s},bits={}\n", .{ backing, passes, elapsed, threads, algo, bits });
 }
