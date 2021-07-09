4ca0d3e126290674c9bae6b7484206a96e63fee9
James Barnes <jamesbarnes2505@gmail.com>
2021-07-09 21:37:45 +1000

    modified `prime_count/2`; faithful -> no; cleanups

--- PrimeProlog/solution_1/primes.pl
+++ PrimeProlog/solution_1/primes.pl
@@ -5,41 +5,34 @@
 main(Argv) :-
   (maplist(term_string, [N], Argv) ; N = 1_000_000),
   statistics(walltime, [T0|_]),
-  time_primes(N, 5000, Iters),
+  timed_primes(N, 5000, Iters),
   statistics(walltime, [T1|_]),
   format(
-    "jimbxb_prolog;~d;~f;1;algorithm=base,faithful=yes,bits=1~n", 
+    "jimbxb_prolog;~d;~f;1;algorithm=base,faithful=no,bits=1~n", 
     [Iters, (T1 - T0) / 1000]
   ),
   true.
 
-time_primes(_, RemT, 0) :-
+timed_primes(_, RemT, 0) :-
   RemT < 0,
   !.
-time_primes(N, RemT, I0+1) :-
+timed_primes(N, RemT, I0+1) :-
   statistics(walltime, [T0|_]),
   primes(N, Ps),
   length(Ps, Count),
   prime_count(N, Count),
   statistics(walltime, [T1|_]),
   RemT1 is RemT - T1 + T0,
-  time_primes(N, RemT1, I0).
+  timed_primes(N, RemT1, I0).
 
-prime_count(N, Count) :-
-  KnownCounts = [
-    10-4,
-    100-25,
-    1000-168,
-    10000-1229,
-    100000-9592,
-    1000000-78498,
-    10000000-664579,
-    100000000-5761455
-  ],
-  ( member(N-Count, KnownCounts)
-  ; \+ member(N-_, KnownCounts)
-  ),
-  !.
+prime_count(10, 4).
+prime_count(100, 25).
+prime_count(1000, 168).
+prime_count(10000, 1229).
+prime_count(100000, 9592).
+prime_count(1000000, 78498).
+prime_count(10000000, 664579).
+prime_count(100000000, 5761455).
 
 primes(N, Ps) :-
   ( N < 2 -> Ps = []
