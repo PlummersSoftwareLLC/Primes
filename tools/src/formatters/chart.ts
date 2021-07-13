f8d474161df0ebca520d5b242598cafc4933ff75
Tudor <tudor@marghidanu.com>
2021-07-13 09:27:46 +0300

    Code cosmetics

--- tools/src/formatters/chart.ts
+++ tools/src/formatters/chart.ts
@@ -4,18 +4,18 @@ import { Report } from '../models';
 import Chartscii, { ChartData } from 'chartscii';
 
 export class ChartFormatter implements IFormatter {
-    render(report: Report): string {
-        const data: Array<ChartData> = report.results.map((result) => {
-            return {
-                label: `${result.implementation}-${result.solution} (${result.label})`,
-                value: result.passes / result.duration / result.threads
-            } as ChartData;
-        });
+  render(report: Report): string {
+    const data: Array<ChartData> = report.results.map((result) => {
+      return {
+        label: `${result.implementation}-${result.solution} (${result.label})`,
+        value: result.passes / result.duration / result.threads
+      } as ChartData;
+    });
 
-        const chart = new Chartscii(data, {
-            sort: true,
-            naked: true,
-        });
-        return chart.create();
-    }
+    const chart = new Chartscii(data, {
+      sort: true,
+      naked: true
+    });
+    return chart.create();
+  }
 }
