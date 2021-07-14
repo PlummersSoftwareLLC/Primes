08733b1973119d3c17cda30526e073a5c3ae89e1
MaxGraey <maxgraey@gmail.com>
2021-07-15 00:56:21 +0300

    refactor index.js

--- PrimeAssemblyScript/solution_1/index.js
+++ PrimeAssemblyScript/solution_1/index.js
@@ -1,14 +1,12 @@
-import loader from "@assemblyscript/loader";
 import fs from 'node:fs/promises';
+import loader from "@assemblyscript/loader";
 import { WASI } from 'wasi';
 
 const wasi = new WASI();
 
 loader.instantiate(fs.readFile("./dist/release.wasm"), {
     wasi_snapshot_preview1: wasi.wasiImport
-}).then((result) => {
+}).then(result => {
     wasi.start(result);
     result.exports.bench();
-}).catch((err) => {
-    console.error(err);
-})
+}).catch(console.error);
