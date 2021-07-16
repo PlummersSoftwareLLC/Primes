87b0cf21481397b55e7ffeec81d0d792839647ba
Nukepayload2 <1939357182@qq.com>
2021-07-16 21:18:46 +0800

    Added VB faithful wheel

---
+++ PrimeBASIC/solution_4/BenchmarkSieves.vb
@@ -0,0 +1,12 @@
+﻿Imports BenchmarkDotNet.Attributes
+
+<MemoryDiagnoser>
+Public Class BenchmarkSieves
+	<Params(1000000)>
+	Public Property SieveSize As Integer
+
+	<Benchmark>
+	Public Sub ArrayPool8of30M()
+		Call New PrimeSieve(SieveSize).Run()
+	End Sub
+End Class
