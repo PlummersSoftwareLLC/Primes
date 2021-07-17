5d29d58e8fff2b715233246d4124df607c201039
Sean Mortara <sean.mortara@gmail.com>
2021-07-17 14:04:30 -0400

    Update main.swift
    Incorporated recommendations by jf-1

--- PrimeSwift/solution_1/Sources/PrimeSieveSwift/main.swift
+++ PrimeSwift/solution_1/Sources/PrimeSieveSwift/main.swift
@@ -3,30 +3,30 @@ import ArgumentParser
 
 class Sieve {
     let sieveSize: Int
-    let q: Int
+    let factorLimit: Int
     var bits: UnsafeMutableBufferPointer<Bool>
 
     init(limit: Int) {
         sieveSize = limit
-        q = Int(sqrt(Double(sieveSize)))
+        factorLimit = Int(sqrt(Double(sieveSize)))
         bits = UnsafeMutableBufferPointer<Bool>.allocate(capacity: (limit + 1) / 2)
         bits.initialize(repeating: true)
     }
 
     deinit { bits.deallocate() }
 
-    @inlinable func index(_ num: Int) -> Int { return num >> 1 - 1}
+    @inlinable func index(forNumber num: Int) -> Int {num >> 1 - 1}
 
     /// Does @inlinable help here?
-    @inlinable func runSieve() {
+    func runSieve() {
         var factor = 3
-        while factor <= q {
-            while !bits[index(factor)] {
+        while factor <= factorLimit {
+            while !bits[index(forNumber: factor)] {
                 factor += 2
             }
             var nFactor = factor*factor
             while nFactor <= sieveSize {
-                bits[index(nFactor)] = false
+                bits[index(forNumber: nFactor)] = false
                 nFactor += 2*factor
             }
             factor += 2
@@ -54,7 +54,7 @@ extension Sieve {
     func countPrimes() -> Int {
         var count = 1
         for num in stride(from: 3, to: sieveSize, by: 2) {
-            if bits[index(num)] {
+            if bits[index(forNumber: num)] {
                 count += 1
             }
         }
@@ -75,7 +75,7 @@ extension Sieve {
         if showingNumbers {
             print("2, ", terminator: "")
             for num in stride(from: 3, to: sieveSize, by: 2) {
-                if bits[index(num)] {
+                if bits[index(forNumber: num)] {
                     print(num, terminator: ", ")
                 }
             }
@@ -90,10 +90,8 @@ extension Sieve {
 }
 
 struct StopWatch {
-    private let start: Date
+    private let start =  Date()
     public var stop = 0.0
-
-    init(){ start = Date() }
 
     mutating func lap() -> Double {
         stop = start.distance(to: Date())
