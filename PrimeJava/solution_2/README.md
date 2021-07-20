89ca4796bf2d81c48302b2d1ff3538f51ed988ce
Pratim Ghosh <pratim.ghosh@outlook.com>
2021-07-21 01:14:50 +0530

    merged solution_3 to solution_2

--- PrimeJava/solution_2/README.md
+++ PrimeJava/solution_2/README.md
@@ -1,23 +1,22 @@
-# Java solution by PratimGhosh86
+# Java BitSet (with Executors) solution by PratimGhosh86
 
 ![Algorithm](https://img.shields.io/badge/Algorithm-base-green)
 ![Faithfulness](https://img.shields.io/badge/Faithful-yes-green)
-![Parallelism](https://img.shields.io/badge/Parallel-no-green)
+![Parallelism](https://img.shields.io/badge/Parallel-yes-green)
 ![Bit count](https://img.shields.io/badge/Bits-1-green)
 
-*This implementation follows the original implementation 1:1 by Dave and uses BitSet rather than arrays. Single threaded performance is slower compared to arrays but we are no longer limited to `Integer.MAX_VALUE - 2` (Requested array size exceeds VM limit).*
+*This implementation keeps the sieve calculation mechanish as close to the original implementation, using BitSets rather than arrays. Instead of patitioning the sieve size, each thread is responsible of handling the entire range and emitting the resultant sieve size at completion. To validate, we use the average of the prime count from every thread against a predefined dictionary*
 
 ## Run instructions
 
-*Java 11+ should be available in the system path if you want to run manually*
-*Performance varies based on JVM. `GraalVM` provides the best performance, followed by `hotspot`. `openj9` is the most memory efficient, and the slowest*
+*Java 11+ should be available in the system path if you want to run manually. Performance varies based on JVM; `GraalVM` provides the best performance, followed by `hotspot`. `openj9` is the most memory efficient, and the slowest*
 
 ## Output
 
 ```
-> Executing task: docker run --rm -it  solution2:latest <
+> Executing task: docker run --rm -it  solution3:latest <
 
-Passes: 2504, Time: 5.000000, Avg: 0.001997, Limit: 1000000, Count: 78498, Valid: true
+Passes: 10150, Time: 5.000000, Avg: 0.000493, Limit: 1000000, Count: 78498, Valid: true
 
-PratimGhosh86;2504;5.000000;1;algorithm=base,faithful=yes,bits=1
+PratimGhosh86-solution_3;10150;5.000000;212;algorithm=base,faithful=yes,bits=1
 ```
--- PrimeJava/solution_2/src/PrimeSieveJavaBitSet.java
+++ PrimeJava/solution_2/src/PrimeSieveJavaBitSet.java
@@ -1,17 +1,151 @@
-import static java.lang.System.currentTimeMillis;
-import static java.util.concurrent.TimeUnit.MILLISECONDS;
 import java.util.BitSet;
 import java.util.Map;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.concurrent.atomic.LongAccumulator;
 
 public class PrimeSieveJavaBitSet {
 
-  private static final int LIMIT = 1000000;
-  private static final int TIME = 5;
+  // upper limit, highest prime we'll consider
+  private static final int SIEVE_SIZE = 1000000;
+
+  // for how long the sieve calculation will run
+  private static final int TIME_IN_SECONDS = 5;
+
+  // used to ensure that no waiting thread runs after the time limit is reached
+  private static final AtomicBoolean SHOULD_RUN = new AtomicBoolean(true);
+
+  // to store number of times the sieve calculation completed successfully
+  private static final AtomicLong PASSES = new AtomicLong(0L);
+
+  // accumulator to store count of primes from individual thread
+  // if each run produced a correct value, it should match count
+  // from dictionary when divided by the number of passes
+  private static final LongAccumulator COUNT_OF_PRIMES_ACCUMULATOR =
+      new LongAccumulator(Long::sum, 0L);
+
+  // cached thread pool to spawn as many threads as possible
+  // alternatively, Executors.newFixedThreadPool(n) can be used to spawn a fixed number of thread
+  private static final ThreadPoolExecutor POOL =
+      (ThreadPoolExecutor) Executors.newCachedThreadPool();
+
+  public static void main(final String[] args) {
+    final var tStart = System.currentTimeMillis();
+    while (TimeUnit.MILLISECONDS.toSeconds(System.currentTimeMillis() - tStart) < TIME_IN_SECONDS) {
+      POOL.execute(() -> runSieve(SIEVE_SIZE, getBitArray(SIEVE_SIZE)));
+    }
+    SHOULD_RUN.set(false);
+    shutdownPool();
+    final var tEnd = System.currentTimeMillis();
+    printResults(SIEVE_SIZE, PASSES.get(), TimeUnit.MILLISECONDS.toSeconds(tEnd - tStart),
+        COUNT_OF_PRIMES_ACCUMULATOR.longValue(), POOL.getLargestPoolSize());
+  }
+
+  // Calculate the primes up to the specified limit
+  public static void runSieve(final int sieveSize, final BitSet bitSet) {
+    // make sure any waiting threads are skipped in time limit is reached
+    if (!SHOULD_RUN.get())
+      return;
+    var factor = 3;
+    final var q = (int) Math.sqrt(sieveSize);
+    while (factor < q) {
+      for (var num = factor; num <= sieveSize; num++) {
+        if (getBit(bitSet, num)) {
+          factor = num;
+          break;
+        }
+      }
+      // If marking factor 3, you wouldn't mark 6 (it's a mult of 2) so start with the 3rd
+      // instance of this factor's multiple. We can then step by factor * 2 because every second
+      // one is going to be even by definition
+      for (var num = factor * 3; num <= sieveSize; num += factor * 2)
+        clearBit(bitSet, num);
+      factor += 2; // No need to check evens, so skip to next odd (factor = 3, 5, 7, 9...)
+    }
+    if (SHOULD_RUN.get())
+      validateAndStoreResults(sieveSize, countPrimes(bitSet));
+  }
+
+  // generate a clean bit array
+  private static BitSet getBitArray(final int sieveSize) {
+    // since we filter evens, just half as many bits
+    final var bitArrayLength = (sieveSize + 1) / 2;
+    final var bitArray = new BitSet(bitArrayLength);
+    bitArray.set(0, bitArrayLength, true);
+    return bitArray;
+  }
+
+  // Gets a bit from the array of bits, but automatically just filters out even numbers as
+  // false, and then only uses half as many bits for actual storage
+  private static boolean getBit(final BitSet bitSet, final int index) {
+    if (index % 2 == 0)
+      return false;
+    return bitSet.get(index / 2);
+  }
+
+  // Reciprocal of GetBit, ignores even numbers and just stores the odds. Since the prime sieve
+  // work should never waste time clearing even numbers, this code will assert if you try to
+  private static void clearBit(final BitSet bitSet, final int index) {
+    if (index % 2 == 0) {
+      System.out.println("You are setting even bits, which is sub-optimal");
+    }
+    bitSet.set(index / 2, false);
+  }
+
+  // Return the count of bits that are still set in the sieve. Assumes you've already called
+  // runSieve, of course!
+  public static int countPrimes(final BitSet bitArray) {
+    return bitArray.cardinality();
+  }
+
+  // Look up our count of primes in the historical data (if we have it) to see if it matches
+  // If a match is found then store the sieve count and increment the number of passes
+  private static void validateAndStoreResults(final int sieveSize, final int countOfPrimes) {
+    if (MY_DICT.containsKey(sieveSize) && MY_DICT.get(sieveSize) == countOfPrimes) {
+      COUNT_OF_PRIMES_ACCUMULATOR.accumulate(countOfPrimes);
+      PASSES.incrementAndGet();
+    }
+  }
+
+  // Look up our count of primes in the historical data (if we have it) to see if it matches
+  private static boolean validate(final int sieveSize, final long count) {
+    if (MY_DICT.containsKey(sieveSize))
+      return MY_DICT.get(sieveSize) == count;
+    return false;
+  }
+
+  private static void printResults(final int sieveSize, final long passes,
+      final double durationInSeconds, final long accumulatedCount, final int maxThreadCount) {
+    System.out.printf("Passes: %d, Time: %f, Avg: %f, Limit: %d, Count: %d, Valid: %s%n", passes,
+        durationInSeconds, durationInSeconds / passes, sieveSize, accumulatedCount / passes,
+        validate(sieveSize, accumulatedCount / passes));
+    System.out.println();
+    System.out.printf("PratimGhosh86;%d;%f;%d;algorithm=base,faithful=yes,bits=1\n", passes,
+        durationInSeconds, maxThreadCount);
+  }
+
+  private static void shutdownPool() {
+    try {
+      // Disable new tasks from being submitted
+      POOL.shutdown();
+      // Wait a while for existing tasks to terminate
+      POOL.awaitTermination(1, TimeUnit.MILLISECONDS);
+    } catch (final InterruptedException e) {
+      // Preserve interrupt status
+      Thread.currentThread().interrupt();
+    } finally {
+      POOL.shutdownNow(); // Cancel currently executing tasks
+    }
+  }
 
   // Historical data for validating our results - the number of primes to be found under some
   // limit, such as 168 primes under 1000
   private static final Map<Integer, Integer> MY_DICT = Map.of( //
       10, 1, //
+      100, 25, //
       1000, 168, //
       10000, 1229, //
       100000, 9592, //
@@ -19,111 +153,5 @@ public class PrimeSieveJavaBitSet {
       10000000, 664579, //
       100000000, 5761455 //
   );
-
-  public static void main(final String[] args) {
-
-    var passes = 0;
-    var sieve = new PrimeSieve(LIMIT);
-    final var tStart = currentTimeMillis();
-
-    while (MILLISECONDS.toSeconds(currentTimeMillis() - tStart) < TIME) {
-      sieve = new PrimeSieve(LIMIT);
-      sieve.runSieve();
-      passes++;
-    }
-
-    sieve.printResults(false, MILLISECONDS.toSeconds(currentTimeMillis() - tStart), passes);
-  }
-
-  public static class PrimeSieve {
-
-    private final int sieveSize;
-    private final BitSet bitArray;
-
-    public PrimeSieve(final int size) {
-      // Upper limit, highest prime we'll consider
-      this.sieveSize = size;
-      // since we filter evens, just half as many bits
-      final var bitArrayLength = (this.sieveSize + 1) / 2;
-      this.bitArray = new BitSet(bitArrayLength);
-      this.bitArray.set(0, bitArrayLength, true);
-    }
-
-    // Calculate the primes up to the specified limit
-    public void runSieve() {
-      var factor = 3;
-      final var q = (int) Math.sqrt(this.sieveSize);
-
-      while (factor < q) {
-        for (var num = factor; num <= this.sieveSize; num++) {
-          if (getBit(num)) {
-            factor = num;
-            break;
-          }
-        }
-
-        // If marking factor 3, you wouldn't mark 6 (it's a mult of 2) so start with the 3rd
-        // instance of this factor's multiple. We can then step by factor * 2 because every second
-        // one is going to be even by definition
-        for (var num = factor * 3; num <= this.sieveSize; num += factor * 2)
-          this.clearBit(num);
-
-        factor += 2; // No need to check evens, so skip to next odd (factor = 3, 5, 7, 9...)
-      }
-    }
-
-    // Return the count of bits that are still set in the sieve. Assumes you've already called
-    // runSieve, of course!
-    public int countPrimes() {
-      return this.bitArray.cardinality();
-    }
-
-    // Look up our count of primes in the historical data (if we have it) to see if it matches
-    public boolean validateResults() {
-      if (MY_DICT.containsKey(this.sieveSize))
-        return MY_DICT.get(this.sieveSize) == this.countPrimes();
-      return false;
-    }
-
-    // Gets a bit from the array of bits, but automatically just filters out even numbers as
-    // false, and then only uses half as many bits for actual storage
-    private boolean getBit(final int index) {
-      if (index % 2 == 0)
-        return false;
-      return this.bitArray.get(index / 2);
-    }
-
-    // Reciprocal of GetBit, ignores even numbers and just stores the odds. Since the prime sieve
-    // work should never waste time clearing even numbers, this code will assert if you try to
-    private void clearBit(final int index) {
-      if (index % 2 == 0) {
-        System.out.println("You are setting even bits, which is sub-optimal");
-      }
-      this.bitArray.set(index / 2, false);
-    }
-
-    // Displays the primes found (or just the total count, depending on what you ask for)
-    public void printResults(final boolean showResults, final double duration, final long passes) {
-      if (showResults)
-        System.out.print("2, ");
-
-      var count = 1;
-      for (var num = 3; num <= this.sieveSize; num++) {
-        if (this.getBit(num)) {
-          if (showResults)
-            System.out.printf("%d, ", String.valueOf(num));
-          count++;
-        }
-      }
-      if (showResults)
-        System.out.println("");
-
-      System.out.printf("Passes: %d, Time: %f, Avg: %f, Limit: %d, Count: %d, Valid: %s%n", passes,
-          duration, duration / passes, sieveSize, count, this.validateResults());
-      System.out.println();
-      System.out.printf("PratimGhosh86;%d;%f;1;algorithm=base,faithful=yes,bits=1\n", passes,
-          duration);
-    }
-  }
 
 }
