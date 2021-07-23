fed51697114a62bb6baabbb5333cbfb56490c2f6
Jakob K <bluefireolymp@gmail.com>
2021-07-23 23:51:00 +0200

    Remove single threaded (jakob) Kotlin solution

--- PrimeKotlin/solution_1/README.md
+++ PrimeKotlin/solution_1/README.md
@@ -4,8 +4,7 @@
 ![Faithfulness](https://img.shields.io/badge/Faithful-yes-green)
 ![Parallelism](https://img.shields.io/badge/Parallel-yes-green)
 
-An implementation of the base algorithm using Kotlin. The first test runs concurrently using coroutines, the other one
-runs on a single thread.
+An implementation of the base algorithm using Kotlin. The test runs concurrently using coroutines.
 
 ## Output
 
@@ -14,9 +13,4 @@ Tested on a machine with 4 cores (Intel i5 4590) and using slow DDR3 ram.
 The output with coroutines (**15694** iterations in 5 seconds):
 ```
 jakobk_kotlin_coroutines;15694;5;4;algorithm=base
-```
-
-The output without coroutines (**4381** iterations in 5 seconds):
-```
-jakobk_kotlin_singlethreaded;4381;5.0;1;algorithm=base
 ```
--- PrimeKotlin/solution_1/src/main/kotlin/PrimeSieve.kt
+++ PrimeKotlin/solution_1/src/main/kotlin/PrimeSieve.kt
@@ -4,47 +4,26 @@ import kotlin.math.sqrt
 
 const val n = 1_000_000
 
-fun main() {
-    runBlocking(Dispatchers.Default) {
-        // MULTI THREADED
+fun main() = runBlocking(Dispatchers.Default) {
+    val cores = Runtime.getRuntime().availableProcessors()
 
-        val cores = Runtime.getRuntime().availableProcessors()
+    val primeSieveJobs = ArrayList<Job>()
 
-        val primeSieveJobs = ArrayList<Job>()
-
-        val iterations = AtomicInteger()
-        repeat(cores) {
-            primeSieveJobs += launch {
-                while (isActive) {
-                    PrimeSieve(n).runSieve()
-                    iterations.incrementAndGet()
-                }
+    val iterations = AtomicInteger()
+    repeat(cores) {
+        primeSieveJobs += launch {
+            while (isActive) {
+                PrimeSieve(n).runSieve()
+                iterations.incrementAndGet()
             }
         }
+    }
 
-        withContext(Dispatchers.IO) {
-            delay(5000)
-            primeSieveJobs.forEach { it.cancel() }
-            println("jakobk_kotlin_coroutines;${iterations.get()};5;${cores};algorithm=base,faithful=yes")
-        }
-
-        // cool down
+    withContext(Dispatchers.IO) {
         delay(5000)
+        primeSieveJobs.forEach { it.cancel() }
+        println("jakobk_kotlin_coroutines;${iterations.get()};5;${cores};algorithm=base,faithful=yes")
     }
-
-    // SINGLE THREADED
-
-    val startTime = System.currentTimeMillis()
-
-    var iterationsSingleThreaded = 0
-    while (System.currentTimeMillis() - startTime < 5000) {
-        PrimeSieve(n).runSieve()
-        iterationsSingleThreaded++
-    }
-
-    val duration = System.currentTimeMillis() - startTime
-
-    println("jakobk_kotlin_singlethreaded;${iterationsSingleThreaded};${duration / 1000.0};1;algorithm=base,faithful=yes")
 }
 
 class PrimeSieve(private val size: Int) {
