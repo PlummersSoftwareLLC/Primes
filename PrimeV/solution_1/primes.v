914e54fb19c70efeb8fcfb0d008f3c3cf1f31d5a
Subhomoy Haldar <hungrybluedev@gmail.com>
2021-07-28 20:25:30 +0530

    Use inline map instead of function in V/solution_1 (#527)

--- PrimeV/solution_1/primes.v
+++ PrimeV/solution_1/primes.v
@@ -2,34 +2,27 @@ import time
 import math
 
 const (
-	sieve_size = 1_000_000
-	q = math.sqrt(sieve_size)	
-	all_bits_true_array = []bool{len: sieve_size, init: true}	
+	sieve_size          = 1_000_000
+	q                   = math.sqrt(sieve_size)
+	all_bits_true_array = []bool{len: sieve_size, init: true}
+	dictionary          = map{
+		'10':          4
+		'100':         25
+		'1000':        168
+		'10000':       1229
+		'100000':      9592
+		'1000000':     78498
+		'10000000':    664579
+		'100000000':   5761455
+		'1000000000':  50847534
+		'10000000000': 455052511
+	}
 )
 
 struct Sieve {
 	sieve_size u64
 mut:
 	bits []bool
-}
-
-fn get_dict_value(key string) int {
-	// I couldn't declare a global typed inline map. 
-	// The documentation is almost non-existent, and I can't find any examples of how to do this.
-
-	mut dictionary := map[string]int{}
-	dictionary['10'] = 4
-	dictionary['100'] = 25
-	dictionary['1000'] = 168
-	dictionary['10000'] = 1229
-	dictionary['100000'] = 9592
-	dictionary['1000000'] = 78498
-	dictionary['10000000'] = 664579
-	dictionary['100000000'] = 5761455
-	dictionary['1000000000'] = 50847534
-	dictionary['10000000000'] = 455052511
-
-	return dictionary[key]
 }
 
 [direct_array_access]
@@ -74,7 +67,7 @@ fn (sieve Sieve) print_results(show_results bool, duration time.Duration, passes
 
 	avg := f64(duration / passes)
 	count_primes := sieve.count_primes()
-	valid := (count_primes == get_dict_value(sieve.sieve_size.str()))
+	valid := (count_primes == dictionary[sieve.sieve_size.str()])
 	eprintln('Passes: $passes, Time: $duration, Avg: $avg, Limit: $sieve.sieve_size, Count1: $count, Count2: $count_primes, Valid: $valid')
 
 	println('marghidanu;$passes;$duration;1;algorithm=base,faithful=yes')
