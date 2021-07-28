19c9da75a3904541150e8897b4d8de291190565d
Sean Mortara <yellowcub@gmail.com>
2021-07-27 22:34:21 -0400

    Faithful 1bit Swift Implementation
    Moved original 8bit routine under `PrimeSwift_8bitBool` and added 1bit routine under `PrimeSwift_1bitBool`.  Modified `Dockerfile` and `run.sh` to execute both routines within same image.

--- PrimeSwift/solution_1/run.sh
+++ PrimeSwift/solution_1/run.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 
-swift build --configuration release
-
-./.build/release/PrimeSieveSwift --upper-limit 1000000 --time 5 --list-results false
-
-# Alternative
-# ./.build/release/PrimeSieveSwift -n 1000000 -t 5 -l false
+for pkg in $(ls -d ./*/); do
+    swift run --configuration release --disable-sandbox -Xswiftc -O --package-path ${pkg}
+done
