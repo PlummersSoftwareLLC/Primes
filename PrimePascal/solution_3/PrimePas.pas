9fad54c765bc6383f5a2c2a7f4cb3072fb3865c6
olivierbrun <87953390+olivierbrun@users.noreply.github.com>
2021-07-29 17:05:21 +0200

    Update PrimePas.pas
    Placed the function to get the number of CPU cores in a separate unit, to make the code cleaner.
    I've also moved the functions displaying results inside the prime_sieve class for clarity.
    Finally I've added an option -v in the command line to make the display of validation results optional  (such as "Passes: 19794, Theads: 8, Time: 5.016000 s, Avg: 0.253410 ms, Limit: 1000000, Counts: 78498/78498, Valid: true")

--- PrimePascal/solution_3/PrimePas.pas
+++ PrimePascal/solution_3/PrimePas.pas
@@ -3,30 +3,26 @@ program PrimePas;
 {$mode objfpc}{$H+}
 
 uses
-  {$IF defined(linux)}
-  cthreads, ctypes,
-  {$ELSEIF defined(freebsd) or defined(darwin)}
-  cthreads, ctypes, sysctl,
-  {$ELSEIF defined(windows)}
-  Windows,
-    {$IFDEF UseTProcessW}
-    pipes,
-    {$ENDIF}
-  {$ENDIF}
-  SysUtils, fgl;
+  SysUtils, fgl, CPUCountUtil;
 
 type
   TResultsDictionary = specialize TFPGMap<int64, int64>;
 
-  TPackedBits = bitpacked array [0..high(longint)] of boolean;
+  TPackedBits = bitpacked array [0..high(uint32)] of boolean;
   PPackedBits = ^TPackedBits;
 
   TSieveThreadParam = record
-    sieveSize: int64;
+    sieveSize: size_t;
+
     runningThreads,
-    passes: longint;
+    totalThreads,
+    passes: integer;
+
+    showValidation,
+    showResults,
     terminateThreads: boolean;
-    bits: PPackedBits;
+
+    startTime: int64;
   end;
 
   PSieveThreadParam = ^TSieveThreadParam;
@@ -34,33 +30,32 @@ type
   { prime_sieve }
   prime_sieve = class
     private
-      sieveSize: int64;
+      sieveSize: size_t;
       bits: PPackedBits;
 
+      procedure ClearBits(n, skip: size_t);
+      function CountPrimes: size_t;
+      procedure PrintResults(showResults, showValidation: boolean; duration: double; passes, threads: integer);
+      function ValidateResults: boolean;
+
     public
-      constructor Create(n: int64);
+      constructor Create(n: size_t);
       destructor Destroy; override;
 
       procedure RunSieve;
   end;
-
-{$IFDEF Linux}
-function sysconf(i: cint): clong; cdecl; external name 'sysconf';
-const _SC_NPROCESSORS_ONLN = 83;
-{$ENDIF}
-
 { prime_sieve }
 
-constructor prime_sieve.Create(n: int64);
+constructor prime_sieve.Create(n: size_t);
 var
-  nbBytes: int64;
+  nbBytes: size_t;
 
 begin
   sieveSize:=n;
   //allocating sieveSize/8 bytes on the heap to store the bits
   nbBytes:=n >> 3 + ord((n and $7)>0); // an extra byte is added in the case where n is not a multiple of 8
   bits:=GetMem(nbBytes);
-  fillchar(bits[0], nbBytes, $FF); // initializing all bits to true
+  fillchar(bits[0], nbBytes, $AA); // initializing bits to true for odd numbers and false for even numbers
 end;
 
 destructor prime_sieve.Destroy;
@@ -69,13 +64,20 @@ begin
   inherited Destroy;
 end;
 
+procedure prime_sieve.ClearBits(n, skip: size_t);
+begin
+  while n<=sieveSize do
+  begin
+    bits^[n]:=false;
+    n+=skip;
+  end;
+end;
+
 procedure prime_sieve.RunSieve;
 var
   num,
-  step,
   factor,
-  q: int64;
-
+  q: size_t;
 
 begin
   factor:=3;
@@ -85,111 +87,47 @@ begin
   begin
     num:=factor;
 
-    while num<sieveSize do
-    begin
-      if bits^[num] then
-      begin
-        factor:=num;
-        break;
-      end;
-
+    while (num<=sieveSize) and not bits^[num] do
       num+=2;
-    end;
 
-    num:=factor*factor;
-    step:=factor+factor;
+    if num>sieveSize then
+      break;
 
-    while num<sieveSize do
-    begin
-      bits^[num]:=false;
-      num+=step;
-    end;
+    factor:=num;
+    ClearBits(factor*factor, factor+factor);
 
     factor+=2;
   end;
 end;
 
-function GetSystemThreadCount: integer;
-// returns a good default for the number of threads on this system
-{$IF defined(windows)}
-//returns total number of processors available to system including logical hyperthreaded processors
+function prime_sieve.CountPrimes: size_t;
 var
-  SystemInfo: SYSTEM_INFO;
-  {$IFnDEF WinCE}
-  i: Integer;
-  ProcessAffinityMask, SystemAffinityMask: DWORD_PTR;
-  Mask: DWORD;
-  {$ENDIF}
-begin
-  {$IFnDEF WinCE}
-  if GetProcessAffinityMask(GetCurrentProcess, ProcessAffinityMask{%H-}, SystemAffinityMask{%H-})
-  then begin
-    Result := 0;
-    for i := 0 to 31 do begin
-      Mask := DWord(1) shl i;
-      if (ProcessAffinityMask and Mask)<>0 then
-        inc(Result);
-    end;
-    exit;
-  end;
-  {$ENDIF}
-  //can't get the affinity mask so we just report the total number of processors
-  GetSystemInfo(SystemInfo{%H-});
-  Result := SystemInfo.dwNumberOfProcessors;
-end;
-{$ELSEIF defined(freebsd) or defined(darwin)}
-var
-  mib: array[0..1] of cint;
-  len: cint;
-  t: cint;
-begin
-  mib[0] := CTL_HW;
-  mib[1] := HW_NCPU;
-  len := sizeof(t);
-  {$if FPC_FULLVERSION >= 30101}
-  fpsysctl(@mib, 2, @t, @len, Nil, 0);
-  {$else}
-  fpsysctl(pchar(@mib), 2, @t, @len, Nil, 0);
-  {$endif}
-  Result:=t;
-end;
-{$ELSEIF defined(linux)}
-  begin
-    Result:=sysconf(_SC_NPROCESSORS_ONLN);
-  end;
-{$ELSE}
-  begin
-    Result:=1;
-  end;
-{$ENDIF}
-
-// retrieve command line options
-procedure GetRequested(var lThreadsRequested, lSecondsRequested: longint; var lSizeRequested: int64);
-var
-  numParam: longint;
-  lCommand: string;
+  i,
+  count: size_t;
 
 begin
-  lThreadsRequested:=GetSystemThreadCount;
-  lSecondsRequested:=5;
-  lSizeRequested:=1000000;
+  count:=ord(sieveSize>=2);
 
-  for numParam:=1 to ParamCount do
+  i:=3;
+
+  while i<=sieveSize do
   begin
-    lCommand:=ParamStr(numParam);
+    if bits^[i] then
+      count+=1;
 
-    case LowerCase(LeftStr(lCommand,2)) of
-      '-t': lThreadsRequested:=StrToIntDef(Trim(RightStr(lCommand, length(lCommand)-2)), 1);
-      '-d': lSecondsRequested:=StrToIntDef(Trim(RightStr(lCommand, length(lCommand)-2)), 5);
-      '-s': lSizeRequested:=StrToIntDef(Trim(RightStr(lCommand, length(lCommand)-2)), 1000000);
-    end;
+    i+=2;
   end;
+
+  result:=count;
 end;
 
-// initialize the dictionary storing precomputed primes counts for validation
-procedure FillValidateResults(var lResultsDictionary: TResultsDictionary);
+// validateResults
+//
+// Checks to see if the number of primes found matches what we should expect.  This data isn't used in the
+// sieve processing at all, only to sanity check that the results are right when done.
+function prime_sieve.ValidateResults: boolean;
 const
-  PRIME_COUNTS: array [0..9, 0..1] of int64 = (
+  PRIME_COUNTS: array [0..9, 0..1] of uint64 = (
     (10, 4),
     (100, 25),
     (1000, 168),
@@ -203,77 +141,42 @@ const
   );
 
 var
-  i: longint;
+  i: integer;
+  resultsDictionary: TResultsDictionary;
 
 begin
-  lResultsDictionary:=TResultsDictionary.Create;
-  lResultsDictionary.Sorted:=true;
+  resultsDictionary:=TResultsDictionary.Create;
+  resultsDictionary.Sorted:=true;
 
   for i:=0 to high(PRIME_COUNTS) do
-     lResultsDictionary.Add(PRIME_COUNTS[i,0], PRIME_COUNTS[i,1]);
-end;
+     resultsDictionary.Add(PRIME_COUNTS[i,0], PRIME_COUNTS[i,1]);
 
-function CountPrimes(lSieveSize: int64; lBits: PPackedBits): int64;
-var
-  i,
-  count: int64;
-
-begin
-  if lSieveSize>=2 then
-    count:=1
-  else
-    count:=0;
-
-  i:=3;
-
-  while i<lSieveSize do
-  begin
-    if lBits^[i] then
-      count+=1;
-
-    i+=2;
-  end;
-
-  result:=count;
-end;
-
-function ValidateResults(const lResultsDictionary: TResultsDictionary; lSieveSize: int64; lBits: PPackedBits): boolean;
-var
-  index: longint;
-
-begin
-  if lResultsDictionary.Find(lSieveSize, index) then
-    result:=lResultsDictionary.Data[index]=CountPrimes(lSieveSize, lBits)
+  if resultsDictionary.Find(sieveSize, i) then
+    result:=resultsDictionary.Data[i]=CountPrimes
   else
     result:=false;
+
+  resultsDictionary.Free;
 end;
 
-procedure PrintResults(lShowResults: boolean; lDuration: double; lPasses: longint; lSieveSize: int64; lBits: PPackedBits; const lResultsDictionary: TResultsDictionary; lThreadsRequested: longint);
-const
-  TrueFalse: array[boolean] of string = ('false', 'true');
-
+procedure prime_sieve.PrintResults(showResults, showValidation: boolean; duration: double; passes, threads: integer);
 var
-  num,
-  count: int64;
+  count,
+  num: uint64;
 
 begin
-  if lSieveSize>=2 then
-  begin
-    if lShowResults then
+    if showResults and (sieveSize>=2) then
       write('2, ');
 
-    count:=1;
-  end
-  else
-    count:=0;
+  count:=ord(sieveSize>=2);
 
   num:=3;
 
-  while num<lSieveSize do
+  while num<=sieveSize do
   begin
-    if lBits^[num] then
+    if bits^[num] then
     begin
-      if lShowResults then
+      if showResults then
          write(format('%d, ', [num]));
 
       count+=1;
@@ -282,33 +185,61 @@ begin
     num+=2;
   end;
 
-  if lShowResults then
+  if showResults then
     writeln;
 
-  writeln(format('passes: %d, Time: %f s, Avg: %.6f ms, Limit: %d, Count1: %d, Count2: %d, Valid: %s',[
-                 lPasses,
-                 lDuration,
-                 lDuration*1000/lPasses,
-                 lSieveSize,
-                 count,
-                 CountPrimes(lSieveSize, lBits),
-                 TrueFalse[validateResults(lResultsDictionary, lSieveSize, lBits)]]));
-  writeln(format('olivierbrun;%d;%f;%d;algorithm=base,faithful=yes,bits=1', [lPasses, lDuration, lThreadsRequested]));
+  if showValidation then
+  begin
+    writeln(format('Passes: %d, Theads: %d, Time: %.6f s, Avg: %.6f ms, Limit: %d, Counts: %d/%d, Valid: %s',[
+                   passes,
+                   threads,
+                   duration,
+                   duration*1000/passes,
+                   sieveSize,
+                   count, CountPrimes,
+                   BoolToStr(validateResults, 'true', 'false')]));
+    writeln;
+  end;
+
+  writeln(format('olivierbrun;%d;%.6f;%d;algorithm=base,faithful=yes,bits=1', [passes, duration, threads]));
 end;
 
+// retrieve command line options
+procedure GetRequested(var lThreadsRequested, lSecondsRequested: longint; var lSizeRequested: size_t; var lShowResults, lShowValidation: boolean);
+var
+  numParam: longint;
+  lCommand: string;
+
+begin
+  lThreadsRequested:=GetSystemThreadCount;
+  lSecondsRequested:=5;
+  lSizeRequested:=1000000;
+  lShowResults:=false;
+  lShowValidation:=false;
+
+  for numParam:=1 to ParamCount do
+  begin
+    lCommand:=ParamStr(numParam);
+
+    case LowerCase(LeftStr(lCommand,2)) of
+      '-l': lShowResults:=true;
+      '-t': lThreadsRequested:=StrToIntDef(Trim(RightStr(lCommand, length(lCommand)-2)), 1);
+      '-d': lSecondsRequested:=StrToIntDef(Trim(RightStr(lCommand, length(lCommand)-2)), 5);
+      '-s': lSizeRequested:=StrToIntDef(Trim(RightStr(lCommand, length(lCommand)-2)), 1000000);
+      '-v': lShowValidation:=true;
+    end;
+  end;
+end;
+
+// this is the function to spawn a thread
 function SieveThread(p: pointer): ptrint;
 var
   sieve: prime_sieve;
-
-  nbBytes,
-  sieveSize: int64;
-
   lSieveThreadParam: PSieveThreadParam;
 
 begin
   lSieveThreadParam:=p;
   InterLockedIncrement(lSieveThreadParam^.runningThreads); // increment the running threads count
-  sieveSize:=lSieveThreadParam^.sieveSize;
   sieve:=nil;
 
   // calculating primes until the main thread sets the terminateThreads to true
@@ -318,18 +249,14 @@ begin
     if assigned(sieve) then
       FreeAndNil(sieve);
 
-    sieve:=prime_sieve.Create(sieveSize); // instantiate a new sieve
+    sieve:=prime_sieve.Create(lSieveThreadParam^.sieveSize); // instantiate a new sieve
     sieve.runSieve; // run it
     InterLockedIncrement(lSieveThreadParam^.passes); // the passes variable is shared among all threads, hence the use of the thread safe InterLockedIncrement instruction instead of directly doing a +=1 operation
   end;
 
-  // copying the bits from the latest thread for validation when printing the results
+  // displaying results if this is the latest thread to terminate
   if lSieveThreadParam^.runningThreads=1 then
-  begin
-    nbBytes:=sieveSize >> 3 + ord((sieveSize and $7)>0);
-    lSieveThreadParam^.bits:=GetMem(nbBytes);
-    move(sieve.bits[0], lSieveThreadParam^.bits[0], nbBytes);
-  end;
+     sieve.PrintResults(lSieveThreadParam^.showResults, lSieveThreadParam^.showValidation, (GetTickCount64-lSieveThreadParam^.startTime)/1000, lSieveThreadParam^.passes, lSieveThreadParam^.totalThreads);
 
   sieve.Free;
   InterLockedDecrement(lSieveThreadParam^.runningThreads); // decrement the running threads count
@@ -340,33 +267,23 @@ var
   sieveThreadParam: TSieveThreadParam;
 
   numThread,
-  secondsRequested,
-  threadsRequested: longint;
-
-  timeStart: int64;
-
-  resultsDictionary: TResultsDictionary;
-  elapsedTime: double;
+  secondsRequested: longint;
 
 begin
-  GetRequested(threadsRequested, secondsRequested, sieveThreadParam.sieveSize); // retrieving command line options
+  GetRequested(sieveThreadParam.totalThreads, secondsRequested, sieveThreadParam.sieveSize, sieveThreadParam.showResults, sieveThreadParam.showValidation); // retrieving command line options
 
   sieveThreadParam.passes:=0;
   sieveThreadParam.runningThreads:=0;
   sieveThreadParam.terminateThreads:=false;
-  sieveThreadParam.bits:=nil;
-
-  timeStart:=GetTickCount64;
+  sieveThreadParam.startTime:=GetTickCount64;
 
   // spawning the requested threads
-  for numThread:=1 to threadsRequested do
+  for numThread:=1 to sieveThreadParam.totalThreads do
     BeginThread(@sieveThread, @sieveThreadParam);
 
   while true do
   begin
-    elapsedTime:=(GetTickCount64-timeStart)/1000;
-
-    if elapsedTime>=secondsRequested then
+    if (GetTickCount64-sieveThreadParam.startTime)/1000>=secondsRequested then
     begin
       sieveThreadParam.terminateThreads:=true;
 
@@ -374,14 +291,7 @@ begin
       repeat
       until sieveThreadParam.runningThreads=0;
 
-      // getting the elapsed time once all threads are destroyed
-      elapsedTime:=(GetTickCount64-timeStart)/1000;
       break;
     end;
   end;
-
-  FillValidateResults(resultsDictionary); // populate the prime counts dictionary for the validation
-  PrintResults(false, elapsedTime, sieveThreadParam.passes, sieveThreadParam.sieveSize, sieveThreadParam.bits, resultsDictionary, threadsRequested);
-  FreeMem(sieveThreadParam.bits);
-  resultsDictionary.Free;
 end.
