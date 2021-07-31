0ad3461aab7c8c9933ca92cf063601fcc7afb5ef
Isaac Yonemoto <isaac.yonemoto@gmail.com>
2021-07-31 12:31:31 -0500

    completes implementation of the calloc allocator

--- PrimeZig/solution_3/src/alloc.zig
+++ PrimeZig/solution_3/src/alloc.zig
@@ -1,69 +1,64 @@
-//! implements a "somewhat dumb" but threadsafe and FAAAST allocator.  It will
-//! allocate "blocks" of memory using a backing allocator (page allocator is a good
-//! choice, but the world is your oyster).  Doesn't ever really release that memory
-//! back to the OS, because we want to be greedy jerks.  Marks freed memory bits as
-//! recycling (hence environmentally friendly).
-//!
-//! this shouldn't be objectionable, because 1) the C spec says returning free'd
-//! memory to the OS is *optional*, and GoLang is even worse, it *never* returns
-//! memory to the OS, which is why we call GoLang "the CLAM" at the poker table.
-//!
-//! this allocator only allows freeing, no other resizes.
+//! It turns out that calloc has some dark magic speed tricks that it makes
+//! sense to try to use instead of rebuilding noah's ark.  For simplicity, we
+//! only allow completely freeing the memory space.
 
 const std = @import("std");
+const builtin = std.builtin;
 const Allocator = std.mem.Allocator;
+const assert = std.debug.assert;
+const mem = std.mem;
+const c_stdlib = @cImport({
+    @cInclude("stdlib.h");
+});
 
-const Config = struct { single_threaded: bool = false };
-
-pub fn EnvironmentallyFriendlyBlockAllocator(config: Config) type {
-    const Mutex = if (config.single_threaded) std.Thread.Mutex.Dummy else std.Thread.Mutex;
-
+fn CAlloc(c: anytype) type {
     return struct {
-        allocator: Allocator,
-        backing_allocator: *Allocator,
-        mutex: Mutex,
+        allocator: Allocator = .{.allocFn = alloc, .resizeFn = resize},
 
-        // no WAY we'll need more than 256 of these!
-        used_blocks: [256]?[]u8 = [1]?[]u8{null} ** 256,
-        free_blocks: [256]?[]u8 = [1]?[]u8{null} ** 256,
-        total_blocks: usize = 0,
+        // shamelessly copied from https://github.com/ziglang/zig/blob/master/lib/std/heap.zig
+        comptime {
+            if (!builtin.link_libc) {
+                @compileError("ClearingAllocator is only available when linking against libc");
+            }
+        }
 
-        const Self = @This();
+        fn getHeader(ptr: [*]u8) *[*]u8 {
+            return @intToPtr(*[*]u8, @ptrToInt(ptr) - @sizeOf(usize));
+        }
 
-        pub fn init(a: *Allocator) Self {
-            return Self{
-                .allocator = Allocator{ .allocFn = alloc, .resizeFn = resize },
-                .backing_allocator = a,
-                .mutex = Mutex{}, // substitute for std.Thread.Mutex.Dummy in the single-threaded case.
-            };
+        fn alignedAlloc(len: usize, alignment: usize) ?[*]u8 {
+            // Thin wrapper around regular calloc, overallocate to account for
+            // alignment padding.
+            const length_with_padding = len + alignment - 1 + @sizeOf(usize);
+            var index: usize = 0;
+            var unaligned_ptr = @ptrCast([*]u8, c.calloc(length_with_padding, 1) orelse return null);
+            const unaligned_addr = @ptrToInt(unaligned_ptr);
+            const aligned_addr = mem.alignForward(unaligned_addr + @sizeOf(usize), alignment);
+            var aligned_ptr = unaligned_ptr + (aligned_addr - unaligned_addr);
+            getHeader(aligned_ptr).* = unaligned_ptr;
+            return aligned_ptr;
+        }
+
+        fn alignedFree(ptr: [*]u8) void {
+            const unaligned_ptr = getHeader(ptr).*;
+            c.free(unaligned_ptr);
         }
 
         fn alloc(
             allocator: *Allocator,
-            n: usize,
-            ptr_align: u29,
+            len: usize,
+            alignment: u29,
             len_align: u29,
-            ret_addr: usize,
-        ) Allocator.Error![]u8 {
-            const this = @fieldParentPtr(Self, "allocator", allocator);
-            const hold = this.mutex.acquire();
-            defer hold.release();
+            return_address: usize,
+        ) error{OutOfMemory}![]u8 {
+            _ = allocator;
+            _ = return_address;
+            _ = len_align;
+            assert(len > 0);
+            assert(std.math.isPowerOfTwo(alignment));
 
-            // search the previously allocated blocks for an available free slot.
-            for (this.free_blocks) |block, index| {
-                if (block) |b| {
-                    if (b.len == n) {
-                        this.mark_as_used(index);
-                        return b;
-                    }
-                }
-            } else {
-                var new_block = try this.backing_allocator.alloc(u8, n);
-                if (this.total_blocks < 256) {
-                    this.save(new_block);
-                }
-                return new_block;
-           }
+            var ptr = alignedAlloc(len, alignment) orelse return error.OutOfMemory;
+            return ptr[0..len];
         }
 
         fn resize(
@@ -72,171 +67,128 @@ pub fn EnvironmentallyFriendlyBlockAllocator(config: Config) type {
             buf_align: u29,
             new_len: usize,
             len_align: u29,
-            ret_addr: usize,
+            return_address: usize,
         ) Allocator.Error!usize {
-            // filter out attempts to do anything except free.
-            if (new_len != 0) return error.OutOfMemory;
+            _ = allocator;
+            _ = buf_align;
+            _ = return_address;
+            _ = len_align;
 
-            const this = @fieldParentPtr(Self, "allocator", allocator);
-            const hold = this.mutex.acquire();
-            defer hold.release();
-
-            // search the existing blocks for this particular memory location.
-            // it's possible that it was allocated instead by the backing allocator.
-            for (this.used_blocks) |block, index| {
-                if (block) |b| {
-                    if (b.ptr == buf.ptr) {
-                        this.mark_as_free(index);
-                        return 0;
-                    }
-                }
-            } else {
-                // not in there means it was done by the backing allocator.
-                return (try this.backing_allocator.resize(buf, 0)).len;
-            }
-        }
-
-        /// it's like a ledger.  This moves some memory from accounts receivable to accounts payable.
-        /// aka asset -> liability.
-        fn mark_as_used(self: *Self, free_index: usize) void {
-            for (self.used_blocks) |block, used_index| {
-                if (block == null) {
-                    self.used_blocks[used_index] = self.free_blocks[free_index];
-                    self.free_blocks[free_index] = null;
-                    return;
-                }
-            } else unreachable; // mathematically impossible to get here without cosmic rays.
-        }
-
-        /// it's like a ledger.  This moves some memory from accounts payable to accounts receviable.
-        /// aka liability -> asset.
-        fn mark_as_free(self: *Self, used_index: usize) void {
-            for (self.free_blocks) |block, free_index| {
-                if (block == null) {
-                    self.free_blocks[free_index] = self.used_blocks[used_index];
-                    self.used_blocks[used_index] = null;
-                    return;
-                }
-            } else unreachable; // mathematically impossible to get here without cosmic rays.
-        }
-
-        /// this one is a bit like the federal reserve bank.  It prints money, I mean, memory out of nowhere
-        fn save(self: *Self, new_buffer: []u8) void {
-            for (self.used_blocks) |block, used_index| {
-                if (block == null) {
-                    self.used_blocks[used_index] = new_buffer;
-                    self.total_blocks += 1;
-                    return;
-                }
-            } else unreachable; // mathematically impossible to get here without cosmic rays.
-        }
-
-        /// cleans up all stuff that hasn't been released.  Note that we want this because there's
-        /// probably a bunch of free blocks (at least) that the main code *thought* was gone because
-        /// it had called free (but we lied to it tee hee).  Don't freak out!  Most industrial-strength
-        /// allocators also lie to the code, it's really none of the code's business whether the OS got
-        /// the memory back;  At least, the C standard says so.
-        pub fn deinit(self: *Self) void {
-            for (self.used_blocks) |block| {
-                if (block) |b| {
-                    self.backing_allocator.free(b);
-                }
+            if (new_len == 0) {
+                alignedFree(buf.ptr);
+                return 0;
             }
 
-            for (self.free_blocks) |block| {
-                if (block) |b| {
-                    self.backing_allocator.free(b);
-                }
-            }
+            return error.OutOfMemory;
         }
     };
 }
 
-const backing_allocator = std.testing.allocator;
-const assert = std.debug.assert;
+// instantiate calloc, as wrapped by zig.
+var calloc: CAlloc(c_stdlib) = .{};
 
-test "allocator can allocate stuff and it winds up in used_blocks" {
-    var a = EnvironmentallyFriendlyBlockAllocator(.{}).init(backing_allocator);
-    defer a.deinit();
-
-    var alloc = &a.allocator;
-
-    var mem1 = try alloc.alloc(u8, 256);
-
-    // verify that it's in used_blocks
-    try std.testing.expectEqual(mem1.ptr, a.used_blocks[0].?.ptr);
+pub fn calloc_aligned(comptime T: type, count: usize, comptime alignment: u28) ![]T {
+    return try calloc_aligned_gen(T, &calloc.allocator, count, alignment);
 }
 
-test "allocator can deallocate stuff and it winds up in free_blocks" {
-    var a = EnvironmentallyFriendlyBlockAllocator(.{}).init(backing_allocator);
-    defer a.deinit();
-
-    var alloc = &a.allocator;
-
-    var mem1 = try alloc.alloc(u8, 256);
-
-    alloc.free(mem1);
-
-    // verify that it's in free_blocks
-    // verify that it's not in used_blocks
-    try std.testing.expectEqual(a.used_blocks[0], null);
-    try std.testing.expectEqual(mem1.ptr, a.free_blocks[0].?.ptr);
+pub fn free(slice: anytype) void {
+    free_gen(slice, &calloc.allocator);
 }
 
-test "allocator can allocate and deallocate stuff out of order" {
-    var a = EnvironmentallyFriendlyBlockAllocator(.{}).init(backing_allocator);
-    defer a.deinit();
-
-    var alloc = &a.allocator;
-    var mem1 = try alloc.alloc(u8, 256);
-    var mem2 = try alloc.alloc(u8, 256);
-    alloc.free(mem1);
-
-    // verify that it's in used_blocks
-    try std.testing.expectEqual(a.used_blocks[0], null);
-    try std.testing.expectEqual(mem2.ptr, a.used_blocks[1].?.ptr);
-    try std.testing.expectEqual(mem1.ptr, a.free_blocks[0].?.ptr);
-}
-
-test "also for the singlethreaded case." {
-    var a = EnvironmentallyFriendlyBlockAllocator(.{ .single_threaded = true }).init(backing_allocator);
-    defer a.deinit();
-
-    var alloc = &a.allocator;
-    var mem1 = try alloc.alloc(u8, 256);
-    var mem2 = try alloc.alloc(u8, 256);
-    alloc.free(mem1);
-
-    // verify that it's in used_blocks
-    try std.testing.expectEqual(a.used_blocks[0], null);
-    try std.testing.expectEqual(mem2.ptr, a.used_blocks[1].?.ptr);
-    try std.testing.expectEqual(mem1.ptr, a.free_blocks[0].?.ptr);
-}
-
-test "allocator can more than 256 things" {
-    var a = EnvironmentallyFriendlyBlockAllocator(.{}).init(backing_allocator);
-    defer a.deinit();
-
-    var alloc = &a.allocator;
-    var idx: usize = 0;
-    while (idx < 256) : (idx += 1) {
-        // allocate the page and throw it away BYEE
-        _ = try alloc.alloc(u8, 256);
+// generalized aligned, clearing allocator implementation.
+fn calloc_aligned_gen(comptime T: type, allocator: *Allocator, count: usize, comptime alignment: u28) ![]T {
+    const slice = try allocator.allocAdvanced(T, alignment, count, .exact);
+    if (getRuntimeSafety()) {
+        // zig drops 0xaa in all of the data slots in the case of release-safe modes.
+        const sliceptr = @ptrCast([*]u8, slice.ptr);
+        @memset(sliceptr, 0, count * @sizeOf(T));
     }
-
-    // all good.  Don't forget to manually clear this one, because it can't be
-    // handled by deinit.
-    var last_memory = try alloc.alloc(u8, 1024);
-    defer alloc.free(last_memory);
+    return slice;
 }
 
-test "allocator can create arrays" {
-    var a = EnvironmentallyFriendlyBlockAllocator(.{}).init(backing_allocator);
-    defer a.deinit();
-    var alloc = &a.allocator;
-    var idx: usize = 0;
-    while (idx < 10) : (idx += 1) {
-        _ = try alloc.alloc(u8, 500000);
+// generalized clearing allocator.
+fn free_gen(slice: anytype, allocator: *Allocator) void {
+    allocator.free(slice);
+}
+
+fn getRuntimeSafety() comptime bool {
+    return switch(std.builtin.mode) {
+        std.builtin.Mode.Debug => true,
+        std.builtin.Mode.ReleaseSafe => true,
+        std.builtin.Mode.ReleaseFast => false,
+        std.builtin.Mode.ReleaseSmall => false,
+    };
+}
+
+test "produces an aligned memory slot that is empty" {
+    const datatypes = .{u8, u32, u64};
+
+    inline for (datatypes) |T| {
+        const alignment = @alignOf(T);
+        var slice = try calloc_aligned(T, 10, @alignOf(T));
+        defer free(slice);
+
+        try std.testing.expectEqual(@ptrToInt(slice.ptr) % alignment, 0);
+
+        for (slice) |value| {
+            try std.testing.expectEqual(value, 0);
+        }
     }
-    _ = try alloc.create([15625]u32);
+}
+
+test "can produce higher alignments" {
+    const datatypes = .{u8, u32, u64};
+
+    inline for (datatypes) |T| {
+        const alignment = @alignOf(T);
+        var slice = try calloc_aligned(T, 10, std.mem.page_size);
+        defer free(slice);
+
+        try std.testing.expectEqual(@ptrToInt(slice.ptr) % alignment, 0);
+
+        for (slice) |value| {
+            try std.testing.expectEqual(value, 0);
+        }
+    }
+}
+
+const testing_allocator = std.testing.allocator;
+
+var last_allocation: usize = undefined;
+// instantiate a mock calloc, which instead uses test_allocator, to double check
+// we aren't leaking memory.  Definitely NOT threadsafe, only one at a time plz.
+var mock_calloc: CAlloc(struct {
+    pub fn calloc(count: usize, bytes: usize) ?[*]u8 {
+        last_allocation = count * bytes;
+        const slice = testing_allocator.alloc(u8, last_allocation) catch |_| return null;
+        return @ptrCast([*]u8, slice.ptr);
+    }
+    pub fn free(pointer: [*]u8) void {
+        const slice = pointer[0..last_allocation];
+        testing_allocator.free(slice);
+    }
+}) = .{};
+
+fn mock_calloc_aligned(comptime T: type, count: usize, comptime alignment: u28) ![]T {
+    return try calloc_aligned_gen(T, &mock_calloc.allocator, count, alignment);
+}
+
+fn mock_free(slice: anytype) void {
+    free_gen(slice, &mock_calloc.allocator);
+}
+
+test "can produce higher alignments" {
+    const datatypes = .{u8, u32, u64};
+
+    inline for (datatypes) |T| {
+        const alignment = @alignOf(T);
+        var slice = try mock_calloc_aligned(T, 10, std.mem.page_size);
+        defer mock_free(slice);
+
+        try std.testing.expectEqual(@ptrToInt(slice.ptr) % alignment, 0);
+
+        for (slice) |value| {
+            try std.testing.expectEqual(value, 0);
+        }
+    }
 }
