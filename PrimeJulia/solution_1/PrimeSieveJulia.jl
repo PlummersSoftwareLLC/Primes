29559f1b7d55ca9aa0674684d53652ea692faf23
Louie Torres <30176969+louie-github@users.noreply.github.com>
2021-08-04 11:50:42 +0800

    Small changes to Julia solution_1

--- PrimeJulia/solution_1/PrimeSieveJulia.jl
+++ PrimeJulia/solution_1/PrimeSieveJulia.jl
@@ -11,7 +11,7 @@ struct prime_sieve
 	sieveSize::Int
 	rawbits::BitVector # was BitArray before
 
-	prime_sieve(limit::Int) = new(  limit, trues( floor(Int,(limit+1)/2) )  )
+	prime_sieve(limit::Int) = new(  limit, trues( div(limit+1,2) )  )
 end
 
 function validateResults(sieve::prime_sieve)
@@ -24,17 +24,17 @@ end
 
 function runSieve!(sieve::prime_sieve)
 	factor = 3
-	q = sqrt(sieve.sieveSize)
+	q = trunc(Int, sqrt(sieve.sieveSize))
 
 	while factor <= q
-		for num in factor:sieve.sieveSize
-			if num % 2 == 0 ? false : sieve.rawbits[Int((num+1)/2)]
+		for num in factor:2:sieve.sieveSize
+			if @inbounds sieve.rawbits[div(num+1,2)]
 				factor = num
 				break
 			end
 		end
 
-		for idx in div(factor*3+1,2):div(factor*2+1,2):div(sieve.sieveSize+1,2)
+		for idx in div(factor*factor+1,2):div(factor*2+1,2):div(sieve.sieveSize+1,2)
     			@inbounds sieve.rawbits[idx] = false
 		end
 
@@ -54,18 +54,16 @@ function printResults(sieve::prime_sieve, showResults::Bool, duration::Number, p
 	count = 1
 
 	for num in 3:2:sieve.sieveSize
-		if sieve.rawbits[Int((num+1)/2)]
+		if sieve.rawbits[div(num+1,2)]
 			showResults && print("$num" * ", ")
 
 			count += 1
 		end
 	end
 
-	println(count == countPrimes(sieve))
-	println("Passes: $passes, Time: $duration, Avg: $(duration/passes), Limit: $(sieve.sieveSize), Count: $count, Valid: $(validateResults(sieve))")
-
-	# Following 2 lines added by rbergen to conform to drag race output format
-	println()
+	println(stderr, count == countPrimes(sieve))
+	println(stderr, "Passes: $passes, Time: $duration, Avg: $(duration/passes), Limit: $(sieve.sieveSize), Count: $count, Valid: $(validateResults(sieve))")
+	println(stderr)
 	println("dcbi;$passes;$duration;1;algorithm=base,faithful=yes,bits=1")
 
 end
