9340c526050c2b5cc74eb28966ba91ac470165d4
yellowcub <yellowcub@gmail.com>
2021-08-09 05:53:58 -0400

    PrimeSwift/solution_1 fine tuning. (#617)

--- PrimeSwift/solution_1/PrimeSwift_1bitBool/Sources/PrimeSieveSwift/main.swift
+++ PrimeSwift/solution_1/PrimeSwift_1bitBool/Sources/PrimeSieveSwift/main.swift
@@ -19,27 +19,39 @@ struct BooleanBitArray {
         words.deallocate()
     }
 
-    @inline(__always) internal func maskIndex(_ num: Int) -> (Int, Word) {
-        let wordIndex = num / wordSize
-        let mask = Word(1) << (num % wordSize)
-        return (wordIndex, mask)
+    // Returns the array word index and sub index for an input bit index
+    //   - This takes advantage of wordSize being a power of 2 to compute
+    //     the modulus (%) faster by masking wordSize -1
+    @inline(__always) internal func maskIndex(at index: Int) -> (Int, Int) {
+        let wordIndex = index / wordSize
+        let subIndex = index & (wordSize - 1)
+        return (wordIndex, subIndex)
     }
 
-    @inline(__always) func getBit(atIndex num: Int) -> Bool {
-        let (i, m) = maskIndex(num)
-        return (words[i] & m) != 0
+    @inline(__always) func getBit(at index: Int) -> Bool {
+        let (i, m) = maskIndex(at: index)
+        return (words[i] & (Word(1) << m) ) != 0
     }
 
-    @inline(__always) func setBit(atIndex num: Int) {
-        let (i, m) = maskIndex(num)
-        words[i] |= m
+    @inline(__always) func setBit(at index: Int) {
+        let (i, m) = maskIndex(at: index)
+        words[i] |= (Word(1) << m)
     }
 
-    @inline(__always) func clearBit(atIndex num: Int) {
-        let (i, m) = maskIndex(num)
-        words[i] &= ~m
+    @inline(__always) func clearBit(at index: Int) {
+        let (i, m) = maskIndex(at: index)
+        words[i] &= ~(Word(1) << m)
     }
 
+}
+
+func iSqrt(_ x: Int) -> Int {
+    if x == 0 { return 0 }
+    let z = x >> 2
+    let r2 = iSqrt(z) << 1
+    let r3 = r2 + 1
+    if x < (r3 * r3) { return r2 }
+    return r3
 }
 
 class Sieve {
@@ -51,7 +63,7 @@ class Sieve {
 
     init(limit: Int) {
         sieveLimit = limit
-        factorLimit = Int(sqrt(Double(limit)))
+        factorLimit = iSqrt(limit)
         arraySize = (limit + 1) / 2
         primeArray = BooleanBitArray(repeating: true, count: arraySize)
     }
@@ -61,29 +73,30 @@ class Sieve {
     }
 
     @inline(__always) internal func index(for num: Int) -> Int {
-        return num / 2 - 1 // 3 -> 0, 5 -> 1, 7 -> 2, etc.
+        return num / 2 - 1 // {3, 5, 7, ...} -> {0, 1, 2, ...}
     }
     
     @inline(__always) internal func number(at index: Int) -> Int {
-        return index * 2 + 3 // 0 -> 3, 1 -> 5, 2 -> 7, etc.
+        return index * 2 + 3 // {0, 1, 2, ...} -> {3, 5, 7, ...}
     }
 
     func runSieve() {
-        var factorIndex = 0
-        let factorIndexLimit = factorLimit / 2 - 1
+        let factorIndexLimit = index(for: factorLimit)
         let nonPrimeIndexLimit = index(for: sieveLimit)
         
-        while factorIndex <= factorIndexLimit {
+        var factorIndex = -1
+        repeat {
+            factorIndex += 1
+            if !primeArray.getBit(at: factorIndex) { continue }
+            
             let factor = number(at: factorIndex)
-            let isPrime = primeArray.getBit(atIndex: factorIndex)
             var nonPrimeIndex = index(for: factor*factor)
             
-            while isPrime && ( nonPrimeIndex <= nonPrimeIndexLimit ) {
-                primeArray.clearBit(atIndex: nonPrimeIndex)
+            repeat {
+                primeArray.clearBit(at: nonPrimeIndex)
                 nonPrimeIndex += factor
-            }
-            factorIndex += 1
-        }
+            } while ( nonPrimeIndex <= nonPrimeIndexLimit )
+        } while factorIndex < factorIndexLimit
     }
 
 }
@@ -107,7 +120,7 @@ extension Sieve {
     func countPrimes() -> Int {
         var count = 1
         for num in stride(from: 3, to: sieveLimit, by: 2) {
-            if primeArray.getBit(atIndex: index(for: num)) {
+            if primeArray.getBit(at: index(for: num)) {
                 count += 1
             }
         }
@@ -128,7 +141,7 @@ extension Sieve {
         if showingNumbers {
             print(2, terminator: ", ")
             for num in stride(from: 3, to: sieveLimit, by: 2) {
-                if primeArray.getBit(atIndex: index(for: num)) {
+                if primeArray.getBit(at: index(for: num)) {
                     print(num, terminator: ", ")
                 }
             }
@@ -168,16 +181,16 @@ struct PrimeSieveSwift: ParsableCommand {
         var passes = 0
         
         var stopWatch = StopWatch()
-        while true {
+        repeat {
             let sieve = Sieve(limit: upperLimit)
             sieve.runSieve()
             passes += 1
 
-            if stopWatch.lap() >= maxTime {
-                sieve.printResults(showingNumbers: listResults, duration: stopWatch.stop, passes: passes)
-                break
-            }
-        }
+            if stopWatch.lap() < maxTime { continue }
+            
+            sieve.printResults(showingNumbers: listResults, duration: stopWatch.stop, passes: passes)
+            break
+        } while true
 
     }
 
