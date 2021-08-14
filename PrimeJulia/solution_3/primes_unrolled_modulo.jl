6abbaaccc3457c5690328af342ba4d05c68a0f72
Louie Torres <louie.btowers@gmail.com>
2021-08-14 11:14:16 +0800

    Make modulo pattern variable length

--- PrimeJulia/solution_3/primes_unrolled_modulo.jl
+++ PrimeJulia/solution_3/primes_unrolled_modulo.jl
@@ -53,11 +53,13 @@ the corresponding modulo pattern is shorter than that.
 
 function get_modulo_pattern(start::Integer, skip::Integer, bit_length::Integer)
     @assert (skip >= 0) "skip must not be less than 0."
+    first_modulo = start % bit_length
     modulo_increment = skip % bit_length
-    output = Vector{typeof(modulo_increment)}(undef, bit_length)
-    current_modulo = start % bit_length
-    for index in eachindex(output)
-        output[index] = current_modulo
+    current_modulo = (first_modulo + modulo_increment) % bit_length
+    output = Vector{promote_type(typeof(current_modulo))}()
+    push!(output, first_modulo)
+    while current_modulo != first_modulo
+        push!(output, current_modulo)
         current_modulo += modulo_increment
         current_modulo %= bit_length
     end
@@ -101,10 +103,12 @@ end
 
 # This is really hacky; we are evaluating function definitions in
 # global scope. It's better than manually creating a 700 or so line
-# function, though, I guess.
-for ((start, skip), modulo_pattern) in generate_modulo_patterns(UINT_BIT_LENGTH)
-    eval(generate_loop_clearing_function(start, skip, modulo_pattern))
-end
+# function, though, I guess. It also helps with optimization.
+(function ()
+    for ((start, skip), modulo_pattern) in generate_modulo_patterns(UINT_BIT_LENGTH)
+        eval(generate_loop_clearing_function(start, skip, modulo_pattern))
+    end
+end)()
 
 @inline function _unrolled_clear_bits_finalizer(
     arr::Vector{MainUInt}, start::Integer, skip::Integer, stop::Integer
@@ -119,7 +123,7 @@ end
 # Here we go. The main factor clearing loop. Lots of if-elseif
 # statements. Hopefully Julia can optimize this thing, somehow.
 function generate_final_loop_clearing_function()
-    current_outer_expr = main_if_expr = Expr(:if)
+    prev_outer_expr = current_outer_expr = main_if_expr = Expr(:if)
     for start_modulo in 0:UINT_BIT_LENGTH - 1
         func_prefix = Symbol("_unrolled_clear_bits_start_", start_modulo, "_skip_")
         func_name = Symbol(func_prefix, 0)
@@ -139,11 +143,10 @@ function generate_final_loop_clearing_function()
         push!(current_outer_expr.args, :(start_modulo == $start_modulo))
         push!(current_outer_expr.args, Expr(:block, inner_if_expr))
         push!(current_outer_expr.args, Expr(:elseif))
+        prev_outer_expr = current_outer_expr
         current_outer_expr = current_outer_expr.args[end]
     end
-    # This is *really* hacky. We're removing the final elseif Expr
-    # since it is empty.
-    pop!(main_if_expr.args[end].args[end].args[end].args[end].args[end].args[end].args[end].args)
+    pop!(prev_outer_expr.args)
     return quote
         function unsafe_clear_factors!(
             arr::Vector{MainUInt}, start::Integer, skip::Integer, stop::Integer
@@ -272,32 +275,4 @@ end
 
 if abspath(PROGRAM_FILE) == @__FILE__
     main()
-end
-
-function foo(a, b)
-    if x == 1
-        x * 7
-    elseif x == 2
-        x * 8
-    elseif x == 3
-        x * 9
-    elseif x == 4
-        x * 10
-    elseif x == 5
-        x * 11
-    elseif x == 6
-        x * 12
-    elseif x == 7
-        x * 13
-    elseif x == 8
-        x * 14
-    elseif x == 9
-        x * 15
-    elseif x == 10
-        x * 16
-    elseif x == 11
-        x * 17
-    else
-        x * 0
-    end
 end
