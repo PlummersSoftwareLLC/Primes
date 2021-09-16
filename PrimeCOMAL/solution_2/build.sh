611d0c1ccd88c407e6385c91118658cff5847524
Rutger van Bergen <rbergen@xs4all.nl>
2021-09-16 08:59:01 +0200

    COMAL/solution_2: apply timeouts to socat invocations (#720)

--- PrimeCOMAL/solution_2/build.sh
+++ PrimeCOMAL/solution_2/build.sh
@@ -1,6 +1,8 @@
 export TERM=xterm
 
-socat - EXEC:'opencomal',pty,setsid,ctty > /dev/null << EOF
+set -e
+
+timeout -s 9 1m socat - EXEC:'opencomal',pty,setsid,ctty > /dev/null << EOF
 ENTER "opencomal.txt"
 SAVE "opencomal.cml"
 BYE
