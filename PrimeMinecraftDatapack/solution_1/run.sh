37a823ca7e84860ab88a8717e84cb5a8b9b14f61
Rutger van Bergen <rbergen@xs4all.nl>
2021-11-03 23:50:30 +0100

    Clean up run.sh

--- PrimeMinecraftDatapack/solution_1/run.sh
+++ PrimeMinecraftDatapack/solution_1/run.sh
@@ -12,18 +12,17 @@ fi
 
 ./playio java -Xmx1024M -Xms1024M -jar server.jar nogui < runioscript.txt > output.txt
 
-if [ -f output.txt ]; then
-    if ! grep -Fq 'Storage minecraft:primes has the following contents: 78498' output.txt ; then
-        echo "ERROR: prime count missing or invalid"
-        rm output.txt
-        exit 1
-    fi
-
-    awk '/starttime/ {STARTTIME=$2} /endtime/ {ENDTIME=$2} END {printf("RCoder01;1;%.6f;1;algorithm=base,faithful=no\n", (ENDTIME-STARTTIME)/1000000.0)}' output.txt
-
-    rm output.txt
-else
+if [ ! -f output.txt ]; then
     echo "ERROR: no output found"
     exit 1
 fi
 
+if ! grep -Fq 'Storage minecraft:primes has the following contents: 78498' output.txt ; then
+    echo "ERROR: prime count missing or invalid"
+    rm output.txt
+    exit 1
+fi
+
+awk '/starttime/ {STARTTIME=$2} /endtime/ {ENDTIME=$2} END {printf("RCoder01;1;%.6f;1;algorithm=base,faithful=no\n", (ENDTIME-STARTTIME)/1000000.0)}' output.txt
+
+rm output.txt
