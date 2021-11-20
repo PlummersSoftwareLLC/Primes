c9d11c873c7a32189efaac9e6b91569ca85fe76a
topseader <topseader@gmail.com>
2021-11-20 15:46:52 +0300

    Optimizations for Ruby solution

--- PrimeRuby/solution_1/prime.rb
+++ PrimeRuby/solution_1/prime.rb
@@ -1,5 +1,10 @@
 #!/usr/bin/ruby
 
+def get_time
+    #This fuction is needed for a more accurate time measurement
+    Process.clock_gettime(Process::CLOCK_MONOTONIC).to_f
+end
+
 class PrimeSieve
     def initialize(size)
         @sieve_size = size
@@ -17,7 +22,7 @@ class PrimeSieve
         while factor <= sieve_sqrt
             number = factor
 
-            until @prime_array[number / 2] || number > sieve_sqrt do number += 2 end
+            until number > sieve_sqrt do number += 2 end
 
             break unless number <= sieve_sqrt
 
@@ -49,16 +54,15 @@ reference_results = {
 
 sieve_size = 1000000
 pass_count = 0
-start_time = Time.now.to_f
-sieve = nil
+start_time = get_time
 
 begin 
     sieve = PrimeSieve.new(sieve_size)
     sieve.run_sieve
     pass_count += 1
-end until (Time.now.to_f - start_time) > 5.0
+end until (get_time - start_time) > 5.0
 
-duration = (Time.now.to_f - start_time).round(3)
+duration = (get_time - start_time).round(3)
 
 puts "WARNING: result is incorrect!" unless reference_results[sieve_size] == sieve.count_primes
 puts "rbergen;#{ pass_count };#{ duration };1;algorithm=base,faithful=yes"
