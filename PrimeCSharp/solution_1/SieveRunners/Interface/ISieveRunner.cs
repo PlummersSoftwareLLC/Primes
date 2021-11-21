270227d990a4329a0de596fffc224ff47590b707
David Smith <dsmith@datasync.com>
2021-11-21 14:02:49 -0600

    Create interface for new sieve runners.

---
+++ PrimeCSharp/solution_1/SieveRunners/Interface/ISieveRunner.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PrimeCSharp.SieveRunners
+{
+    public interface ISieveRunner
+    {
+        // Identity
+        public string Name { get; }
+        public string Description { get; }
+
+        // Runner details (default values)
+        bool IsParallel => false;
+        bool IsBaseAlgorithm => false;
+        int BitsPerPrime => 1;
+
+        // Runtime details
+        public int SieveSize { get; }
+
+        // Functionality
+        void Run();
+        IEnumerable<int> GetFoundPrimes();
+        int CountPrimes() => GetFoundPrimes().Count();
+        public bool? IsCountValid() => PrimeData.IsCountCorrect(SieveSize, CountPrimes());
+
+        // Debug
+        int ClearCount { get; set; }
+    }
+}
