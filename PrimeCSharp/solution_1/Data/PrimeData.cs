c6d8a3f5cdaad9a5f2080fa6d1003ffe1be29f8b
David Smith <dsmith@datasync.com>
2021-11-21 14:02:34 -0600

    Move PrimeData, and add a few extra data points.

--- PrimeCSharp/solution_1/Data/PrimeData.cs
+++ PrimeCSharp/solution_1/Data/PrimeData.cs
@@ -3,12 +3,20 @@ using System.Collections.Generic;
 
 namespace PrimeCSharp
 {
-    class PrimeData
+    public class PrimeData
     {
         public static readonly IReadOnlyDictionary<int, int> PrimeCounts = new Dictionary<int, int>
             {
                 { 10 , 4 },                 // Historical data for validating our results - the number of primes
                 { 100 , 25 },               // to be found under some limit, such as 168 primes under 1000
+                { 200 , 46 },
+                { 300 , 62 },
+                { 400 , 78 },
+                { 500 , 95 },
+                { 600 , 109 },
+                { 700 , 125 },
+                { 800 , 139 },
+                { 900 , 154 },
                 { 1000 , 168 },
                 { 10000 , 1229 },
                 { 100000 , 9592 },
