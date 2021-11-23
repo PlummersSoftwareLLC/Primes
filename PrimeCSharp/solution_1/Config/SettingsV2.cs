606004ff4db1cb69a5c226e59738a41acc12449c
David Smith <dsmith@datasync.com>
2021-11-23 11:24:05 -0600

    Add v2 settings and sieve runner classes.

---
+++ PrimeCSharp/solution_1/Config/SettingsV2.cs
@@ -0,0 +1,369 @@
+﻿using System;
+using System.Collections.Generic;
+using CommandLine;
+using PrimeCSharp.SieveDetails;
+
+namespace PrimeCSharp.Config
+{
+    public class SettingsV2
+    {
+        [Option('s', "size",
+            Default = 1_000_000, HelpText = "The size of the sieve to run.")]
+        public int SieveSize { get; set; }
+
+        [Option('t', "time",
+            Default = 5, HelpText = "How many seconds to run the sieve.")]
+        public int SecondsToRun { get; set; }
+
+        [Option('v', "verbose",
+            Default = false, HelpText = "Show the prime numbers that were found.")]
+        public bool Verbose { get; set; }
+
+        [Option('m', "multi",
+            Default = false, HelpText = "Run the sieve in a multi-threaded manner.")]
+        public bool MultiThreaded { get; set; }
+
+        [Option("threads",
+            Default = 0, HelpText = "Set the number of threads to use while multithreading. Setting to 0 will make it use the number of processors on the machine.")]
+        public int ThreadCount { get; set; }
+
+        [Option("pthreads",
+            Default = 0, HelpText = "Set the number of threads to use while running parallel algorithms. Setting to 0 will make it use the number of processors on the machine.")]
+        public int PThreadCount { get; set; }
+
+
+        [Option("benchmark",
+            Default = false, HelpText = "Run benchmarks on the sieve implementations.")]
+        public bool Benchmark { get; set; }
+
+        [Option('b', "bench",
+            Default = "", HelpText = "A type of benchmark to run. Leaving it blank will run the standard benchmarks. Current options: mod, ref, ofN, par")]
+        public string? Bench { get; set; }
+
+
+        [Option("all",
+            Default = false, HelpText = "Run all sieves")]
+        public bool All { get; set; }
+
+
+        [Option("bit2",
+            Default = false, HelpText = "Use bitarray storage with the 1 of 2 algorithm")]
+        public bool Bit2 { get; set; }
+
+        [Option("bit2while",
+            Default = false, HelpText = "Use bitarray storage with the 1 of 2 algorithm and while loop")]
+        public bool Bit2While { get; set; }
+
+        [Option("bit6",
+            Default = false, HelpText = "Use bitarray storage with the 2 of 6 algorithm")]
+        public bool Bit6 { get; set; }
+
+        [Option("bit30",
+            Default = false, HelpText = "Use bitarray storage with the 8 of 30 algorithm")]
+        public bool Bit30 { get; set; }
+
+
+        [Option("bool2",
+            Default = false, HelpText = "Use bool array storage with the 1 of 2 algorithm")]
+        public bool Bool2 { get; set; }
+
+        [Option("bool2while",
+            Default = false, HelpText = "Use bool array storage with the 1 of 2 algorithm and while loop")]
+        public bool Bool2While { get; set; }
+
+        [Option("bool6",
+            Default = false, HelpText = "Use bool array storage with the 2 of 6 algorithm")]
+        public bool Bool6 { get; set; }
+
+        [Option("bool30",
+            Default = false, HelpText = "Use bool array storage with the 8 of 30 algorithm")]
+        public bool Bool30 { get; set; }
+
+
+        [Option("ibool2",
+            Default = false, HelpText = "Use inverted bool array storage with the 1 of 2 algorithm")]
+        public bool iBool2 { get; set; }
+
+        [Option("ibool2while",
+            Default = false, HelpText = "Use inverted bool array storage with the 1 of 2 algorithm and while loop")]
+        public bool iBool2While { get; set; }
+
+        [Option("ibool6",
+            Default = false, HelpText = "Use inverted bool array storage with the 2 of 6 algorithm")]
+        public bool iBool6 { get; set; }
+
+        [Option("ibool30",
+            Default = false, HelpText = "Use inverted bool array storage with the 8 of 30 algorithm")]
+        public bool iBool30 { get; set; }
+
+
+        [Option("poolb2",
+            Default = false, HelpText = "Use array pool byte storage with the 1 of 2 algorithm")]
+        public bool PoolB2 { get; set; }
+
+        [Option("poolb6",
+            Default = false, HelpText = "Use array pool byte storage with the 2 of 6 algorithm")]
+        public bool PoolB6 { get; set; }
+
+        [Option("poolb30",
+            Default = false, HelpText = "Use array pool byte storage with the 8 of 30 algorithm")]
+        public bool PoolB30 { get; set; }
+
+        [Option("poold2",
+            Default = false, HelpText = "Use array pool 32-bit storage with the 1 of 2 algorithm")]
+        public bool PoolD2 { get; set; }
+
+        [Option("poold6",
+            Default = false, HelpText = "Use array pool 32-bit storage with the 2 of 6 algorithm")]
+        public bool PoolD6 { get; set; }
+
+        [Option("poold30",
+            Default = false, HelpText = "Use array pool 32-bit storage with the 8 of 30 algorithm")]
+        public bool PoolD30 { get; set; }
+
+        [Option("poolq2",
+            Default = false, HelpText = "Use array pool 64-bit storage with the 1 of 2 algorithm")]
+        public bool PoolQ2 { get; set; }
+
+        [Option("poolq6",
+            Default = false, HelpText = "Use array pool 64-bit storage with the 2 of 6 algorithm")]
+        public bool PoolQ6 { get; set; }
+
+        [Option("poolq30",
+            Default = false, HelpText = "Use array pool 64-bit storage with the 8 of 30 algorithm")]
+        public bool PoolQ30 { get; set; }
+
+        [Option("poolq30m",
+            Default = false, HelpText = "Use array pool 64-bit storage with the 8 of 30 algorithm and bitmasking")]
+        public bool PoolQ30M { get; set; }
+
+
+        [Option("rawb2",
+            Default = false, HelpText = "Use manual byte storage with the 1 of 2 algorithm")]
+        public bool RawB2 { get; set; }
+
+        [Option("rawb6",
+            Default = false, HelpText = "Use manual byte storage with the 2 of 6 algorithm")]
+        public bool RawB6 { get; set; }
+
+        [Option("rawb30",
+            Default = false, HelpText = "Use manual byte storage with the 8 of 30 algorithm")]
+        public bool RawB30 { get; set; }
+
+        [Option("rawd2",
+            Default = false, HelpText = "Use manual 32-bit storage with the 1 of 2 algorithm")]
+        public bool RawD2 { get; set; }
+
+        [Option("rawd6",
+            Default = false, HelpText = "Use manual 32-bit storage with the 2 of 6 algorithm")]
+        public bool RawD6 { get; set; }
+
+        [Option("rawd30",
+            Default = false, HelpText = "Use manual 32-bit storage with the 8 of 30 algorithm")]
+        public bool RawD30 { get; set; }
+
+        [Option("rawq2",
+            Default = false, HelpText = "Use manual 64-bit storage with the 1 of 2 algorithm")]
+        public bool RawQ2 { get; set; }
+
+        [Option("rawq6",
+            Default = false, HelpText = "Use manual 64-bit storage with the 2 of 6 algorithm")]
+        public bool RawQ6 { get; set; }
+
+        [Option("rawq30",
+            Default = false, HelpText = "Use manual 64-bit storage with the 8 of 30 algorithm")]
+        public bool RawQ30 { get; set; }
+
+        [Option("rawq30m",
+            Default = false, HelpText = "Use manual 64-bit storage with the 8 of 30 algorithm")]
+        public bool RawQ30M { get; set; }
+
+
+        [Option("bitarray",
+            Default = false, HelpText = "Run all sieves that use a bitarray")]
+        public bool Bitarray { get; set; }
+
+        [Option("bool",
+            Default = false, HelpText = "Run all sieves that use a bool array")]
+        public bool Boolarray { get; set; }
+
+        [Option("pool",
+            Default = false, HelpText = "Run all sieves that use an array pool")]
+        public bool Arraypool { get; set; }
+
+        [Option("raw",
+            Default = false, HelpText = "Run all sieves that use manual memory allocation")]
+        public bool RawArray { get; set; }
+
+        [Option("invert",
+            Default = false, HelpText = "Run all sieves that use inverted storage logic")]
+        public bool Invert { get; set; }
+
+        [Option("bytes",
+            Default = false, HelpText = "Run all sieves that use byte storage")]
+        public bool DataByte { get; set; }
+
+        [Option("32bit",
+            Default = false, HelpText = "Run all sieves that use 32 bit storage")]
+        public bool DataDword { get; set; }
+
+        [Option("64bit",
+            Default = false, HelpText = "Run all sieves that use 64 bit storage")]
+        public bool DataQword { get; set; }
+
+        [Option("1of2",
+            Default = false, HelpText = "Run all sieves that use the 1 of 2 algorithm")]
+        public bool Alg1of2 { get; set; }
+
+        [Option("2of6",
+            Default = false, HelpText = "Run all sieves that use the 2 of 6 algorithm")]
+        public bool Alg2of6 { get; set; }
+
+        [Option("8of30",
+            Default = false, HelpText = "Run all sieves that use the 8 of 30 algorithm")]
+        public bool Alg8of30 { get; set; }
+
+        [Option("bitmask",
+            Default = false, HelpText = "Run all sieves that use a bitmask wheel")]
+        public bool Bitmask { get; set; }
+
+        [Option("parallel",
+            Default = false, HelpText = "Run all sieves that use parallelized algorithms")]
+        public bool Parallel { get; set; }
+
+        [Option("composed",
+            Default = false, HelpText = "")]
+        public bool Composed { get; set; }
+
+
+        internal List<SieveProperty> GetSieves()
+        {
+            List<SieveProperty> sieves = new();
+
+            if (All)
+            {
+                sieves.AddRange(SieveDetails.SieveRunners.SieveSets);
+                return sieves;
+            }
+
+            if (Bit2)
+                sieves.Add(SieveDetails.SieveRunners.Bit2);
+            if (Bit2While)
+                sieves.Add(SieveDetails.SieveRunners.Bit2While);
+            if (Bit6)
+                sieves.Add(SieveDetails.SieveRunners.Bit6);
+            if (Bit30)
+                sieves.Add(SieveDetails.SieveRunners.Bit30);
+            if (Bool2)
+                sieves.Add(SieveDetails.SieveRunners.Bool2);
+            if (Bool2While)
+                sieves.Add(SieveDetails.SieveRunners.Bool2While);
+            if (Bool6)
+                sieves.Add(SieveDetails.SieveRunners.Bool6);
+            if (Bool30)
+                sieves.Add(SieveDetails.SieveRunners.Bool30);
+            if (iBool2)
+                sieves.Add(SieveDetails.SieveRunners.IBool2);
+            if (iBool2While)
+                sieves.Add(SieveDetails.SieveRunners.IBool2While);
+            if (iBool6)
+                sieves.Add(SieveDetails.SieveRunners.IBool6);
+            if (iBool30)
+                sieves.Add(SieveDetails.SieveRunners.IBool30);
+            if (PoolB2)
+                sieves.Add(SieveDetails.SieveRunners.PoolB2);
+            if (PoolB6)
+                sieves.Add(SieveDetails.SieveRunners.PoolB6);
+            if (PoolB30)
+                sieves.Add(SieveDetails.SieveRunners.PoolB30);
+            if (PoolD2)
+                sieves.Add(SieveDetails.SieveRunners.PoolD2);
+            if (PoolD6)
+                sieves.Add(SieveDetails.SieveRunners.PoolD6);
+            if (PoolD30)
+                sieves.Add(SieveDetails.SieveRunners.PoolD30);
+            if (PoolQ2)
+                sieves.Add(SieveDetails.SieveRunners.PoolQ2);
+            if (PoolQ6)
+                sieves.Add(SieveDetails.SieveRunners.PoolQ6);
+            if (PoolQ30)
+                sieves.Add(SieveDetails.SieveRunners.PoolQ30);
+            if (PoolQ30M)
+                sieves.Add(SieveDetails.SieveRunners.PoolQ30M);
+            if (RawB2)
+                sieves.Add(SieveDetails.SieveRunners.RawB2);
+            if (RawB6)
+                sieves.Add(SieveDetails.SieveRunners.RawB6);
+            if (RawB30)
+                sieves.Add(SieveDetails.SieveRunners.RawB30);
+            if (RawD2)
+                sieves.Add(SieveDetails.SieveRunners.RawD2);
+            if (RawD6)
+                sieves.Add(SieveDetails.SieveRunners.RawD6);
+            if (RawD30)
+                sieves.Add(SieveDetails.SieveRunners.RawD30);
+            if (RawQ2)
+                sieves.Add(SieveDetails.SieveRunners.RawQ2);
+            if (RawQ6)
+                sieves.Add(SieveDetails.SieveRunners.RawQ6);
+            if (RawQ30)
+                sieves.Add(SieveDetails.SieveRunners.RawQ30);
+            if (RawQ30M)
+                sieves.Add(SieveDetails.SieveRunners.RawQ30M);
+
+
+            var sieveProperties = GetSieveProperties();
+
+            foreach (var sieve in SieveDetails.SieveRunners.GetSieves(sieveProperties))
+            {
+                if (!sieves.Contains(sieve))
+                    sieves.Add(sieve);
+            }
+
+            return sieves;
+        }
+
+        private List<SieveProperty> GetSieveProperties()
+        {
+            List<SieveProperty> properties = new();
+
+            if (Bitarray)
+                properties.Add(SieveProperty.BitarrayStorage);
+            if (Boolarray)
+            {
+                properties.Add(SieveProperty.BoolArrayStorage);
+
+                if (Invert)
+                    properties.Add(SieveProperty.InvertStorageValues);
+            }
+            if (Arraypool)
+                properties.Add(SieveProperty.PoolStorage);
+            if (RawArray)
+                properties.Add(SieveProperty.RawStorage);
+
+            if (DataByte)
+                properties.Add(SieveProperty.DataByte);
+            if (DataDword)
+                properties.Add(SieveProperty.DataDword);
+            if (DataQword)
+                properties.Add(SieveProperty.DataQword);
+
+            if (Alg1of2)
+                properties.Add(SieveProperty.Alg1of2);
+            if (Alg2of6)
+                properties.Add(SieveProperty.Alg2of6);
+            if (Alg8of30)
+                properties.Add(SieveProperty.Alg8of30);
+
+            if (Bitmask)
+                properties.Add(SieveProperty.Bitmasking);
+            if (Parallel)
+                properties.Add(SieveProperty.Parallel);
+            if (Composed)
+                properties.Add(SieveProperty.Composed);
+
+
+            return properties;
+        }
+    }
+}
---
+++ PrimeCSharp/solution_1/SieveRunnerV2.cs
@@ -0,0 +1,226 @@
+﻿using PrimeCSharp.Config;
+using PrimeCSharp.SieveDetails;
+using PrimeCSharp.SieveRunners;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PrimeCSharp
+{
+    internal class SieveRunnerV2
+    {
+        const long MillisecondsPerSecond = 1000;
+        const int WarmupCount = 10;
+
+        public static void Run(SettingsV2 settings)
+        {
+            List<SieveProperty> sievesToRun = settings.GetSieves();
+
+            foreach (var sieve in sievesToRun)
+            {
+                try
+                {
+                    Run(sieve, settings);
+                }
+                catch (ArgumentException e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+        }
+
+        private static void Run(SieveProperty sieve, SettingsV2 settings)
+        {
+            Func<ISieveRunner> createSieve = GetSieveCreator(sieve, settings)
+                ?? throw new ArgumentException($"Unable to run sieve: {sieve}", nameof(sieve));
+
+            var demo = createSieve();
+
+            Console.WriteLine($"@Kinematics: Starting ({demo.Description}{(settings.MultiThreaded ? " (multithreaded)" : "")})...");
+
+            long millisecondsToRun = settings.SecondsToRun * MillisecondsPerSecond;
+
+            Stopwatch watch = new();
+
+            var (completedSieve, passes) = settings.MultiThreaded switch
+            {
+                false => RunSingleThread(createSieve, watch, millisecondsToRun, settings),
+                true => RunMultiThread(createSieve, watch, millisecondsToRun, settings),
+            };
+
+            if (completedSieve is null)
+            {
+                Console.WriteLine("Invalid state after run.");
+                return;
+            }
+
+            PrintResults(completedSieve, passes, watch, settings);
+        }
+
+        private static (ISieveRunner?, int) RunSingleThread(
+            Func<ISieveRunner> createSieve, Stopwatch watch, long millisecondsToRun, SettingsV2 settings)
+        {
+            ISieveRunner? sieve = null;
+            int passes = 0;
+            Warmup(createSieve);
+
+            watch.Start();
+
+            while (watch.ElapsedMilliseconds < millisecondsToRun)
+            {
+                sieve = createSieve();
+                sieve.Run();
+                passes++;
+            }
+
+            watch.Stop();
+            return (sieve, passes);
+        }
+
+        private static (ISieveRunner?, int) RunMultiThread(
+            Func<ISieveRunner> createSieve, Stopwatch watch, long millisecondsToRun, SettingsV2 settings)
+        {
+            ISieveRunner? sieve;
+            ISieveRunner? firstSieve = null;
+            int passes = 0;
+            int threadCount = settings.ThreadCount;
+            Warmup(createSieve);
+
+            if (threadCount <= 0)
+                threadCount = Environment.ProcessorCount;
+
+            watch.Start();
+
+            while (watch.ElapsedMilliseconds < millisecondsToRun)
+            {
+                Parallel.For(0, threadCount, i =>
+                {
+                    sieve = createSieve();
+                    firstSieve ??= sieve;
+                    sieve.Run();
+                    if (watch.ElapsedMilliseconds < millisecondsToRun)
+                    {
+                        Interlocked.Increment(ref passes);
+                    }
+                });
+            }
+
+            watch.Stop();
+            return (firstSieve, passes);
+        }
+
+        private static void Warmup(Func<ISieveRunner> createSieve)
+        {
+            for (int i = 0; i < WarmupCount; i++)
+            {
+                var sieve = createSieve();
+                sieve.Run();
+            }
+        }
+
+        private static void PrintResults(ISieveRunner sieve, int passes, Stopwatch watch, SettingsV2 settings)
+        {
+            if (settings.Verbose)
+            {
+                string listing = sieve.GetFoundPrimes()
+                    .Select(a => a.ToString())
+                    .Aggregate((a, b) => $"{a}, {b}");
+
+                Console.WriteLine(listing);
+                Console.WriteLine();
+            }
+
+            int threads = 1;
+            if (settings.MultiThreaded)
+            {
+                threads = settings.ThreadCount == 0 ? Environment.ProcessorCount : settings.ThreadCount;
+            }
+
+            int pthreads = 1;
+            if (sieve.IsParallel)
+            {
+                pthreads = settings.PThreadCount == 0 ? Environment.ProcessorCount : settings.PThreadCount;
+            }
+
+            List<string> results = new();
+
+            results.Add($"Passes: {passes}");
+            results.Add($"Time: {watch.Elapsed.TotalSeconds:G6} s");
+            results.Add($"{((double)watch.ElapsedMilliseconds / passes):F6} ms/loop");
+            results.Add($"Sieve Size: {sieve.SieveSize}");
+            results.Add($"Thread Count: {threads}");
+            if (sieve.IsParallel)
+            {
+                results.Add($"Parallel Thread Count: {pthreads}");
+            }
+            results.Add($"Primes Found: {sieve.CountPrimes()}");
+            results.Add($"Valid: {PrimeData.IsCountCorrect(sieve.SieveSize, sieve.CountPrimes())?.ToString() ?? "Unable to determine"}");
+            if (sieve.ClearCount > 0)
+            {
+                results.Add($"Clear Count: {sieve.ClearCount}");
+            }
+
+            Console.WriteLine(results.Aggregate((a, b) => $"{a}, {b}"));
+
+            // official syntax:
+            // kinematics_<sieve_tag>;<pass_count>;<runtime>;<pthread_count>
+
+            StringBuilder sb = new();
+
+            sb.Append($"kinematics_{sieve.Name};");
+            sb.Append($"{passes};");
+            sb.AppendFormat("{0:G6};", watch.Elapsed.TotalSeconds);
+            sb.Append($"threads={threads * pthreads};");
+            sb.Append($"algorithm={(sieve.IsBaseAlgorithm ? "base" : "wheel")};");
+            sb.Append($"faithful={(sieve.IsBaseAlgorithm ? "yes" : "no")};");
+            sb.Append($"bits={sieve.BitsPerPrime}");
+            sb.AppendLine();
+
+            Console.WriteLine(sb.ToString());
+        }
+
+        private static Func<ISieveRunner>? GetSieveCreator(SieveProperty sieve, SettingsV2 settings)
+        {
+            return sieve switch
+            {
+                SieveDetails.SieveRunners.Bit2 => () => new Bit2Sieve(settings.SieveSize),
+                SieveDetails.SieveRunners.Bit2While => () => new Bit2WhileSieve(settings.SieveSize),
+                SieveDetails.SieveRunners.Bit6 => () => new Bit6Sieve(settings.SieveSize),
+                SieveDetails.SieveRunners.Bit30 => () => new Bit30Sieve(settings.SieveSize),
+                SieveDetails.SieveRunners.Bool2 => () => new Bool2Sieve(settings.SieveSize),
+                SieveDetails.SieveRunners.Bool2While => () => new Bool2WhileSieve(settings.SieveSize),
+                SieveDetails.SieveRunners.Bool6 => () => new Bool6Sieve(settings.SieveSize),
+                SieveDetails.SieveRunners.Bool30 => () => new Bool30Sieve(settings.SieveSize),
+                SieveDetails.SieveRunners.IBool2 => () => new IBool2Sieve(settings.SieveSize),
+                SieveDetails.SieveRunners.IBool2While => () => new IBool2WhileSieve(settings.SieveSize),
+                SieveDetails.SieveRunners.IBool6 => () => new IBool6Sieve(settings.SieveSize),
+                SieveDetails.SieveRunners.IBool30 => () => new IBool30Sieve(settings.SieveSize),
+                SieveDetails.SieveRunners.PoolB2 => () => new PoolB2Sieve(settings.SieveSize),
+                SieveDetails.SieveRunners.PoolD2 => () => new PoolD2Sieve(settings.SieveSize),
+                SieveDetails.SieveRunners.PoolQ2 => () => new PoolQ2Sieve(settings.SieveSize),
+                SieveDetails.SieveRunners.PoolB6 => () => new PoolB6Sieve(settings.SieveSize),
+                SieveDetails.SieveRunners.PoolD6 => () => new PoolD6Sieve(settings.SieveSize),
+                SieveDetails.SieveRunners.PoolQ6 => () => new PoolQ6Sieve(settings.SieveSize),
+                SieveDetails.SieveRunners.PoolB30 => () => new PoolB30Sieve(settings.SieveSize),
+                SieveDetails.SieveRunners.PoolD30 => () => new PoolD30Sieve(settings.SieveSize),
+                SieveDetails.SieveRunners.PoolQ30 => () => new PoolQ30Sieve(settings.SieveSize),
+                SieveDetails.SieveRunners.PoolQ30M => () => new PoolQ30MSieve(settings.SieveSize),
+                SieveDetails.SieveRunners.RawB2 => () => new RawB2Sieve(settings.SieveSize),
+                SieveDetails.SieveRunners.RawD2 => () => new RawD2Sieve(settings.SieveSize),
+                SieveDetails.SieveRunners.RawQ2 => () => new RawQ2Sieve(settings.SieveSize),
+                SieveDetails.SieveRunners.RawB6 => () => new RawB6Sieve(settings.SieveSize),
+                SieveDetails.SieveRunners.RawD6 => () => new RawD6Sieve(settings.SieveSize),
+                SieveDetails.SieveRunners.RawQ6 => () => new RawQ6Sieve(settings.SieveSize),
+                SieveDetails.SieveRunners.RawB30 => () => new RawB30Sieve(settings.SieveSize),
+                SieveDetails.SieveRunners.RawD30 => () => new RawD30Sieve(settings.SieveSize),
+                SieveDetails.SieveRunners.RawQ30 => () => new RawQ30Sieve(settings.SieveSize),
+                SieveDetails.SieveRunners.RawQ30M => () => new RawQ30MSieve(settings.SieveSize),
+                _ => null
+            };
+        }
+    }
+}
