f17aa99df5e7e83976a9f80a5e2b630b74a7eae7
David Smith <dsmith@datasync.com>
2021-11-23 11:27:47 -0600

    Add handling for the v2 code commandline options.

--- PrimeCSharp/solution_1/Program.cs
+++ PrimeCSharp/solution_1/Program.cs
@@ -13,6 +13,17 @@ namespace PrimeCSharp
         {
             CultureInfo.CurrentCulture = new CultureInfo("en-US", false);
 
+            if (args[0] == "v2")
+            {
+                var v2args = args[1..];
+
+                var result2 = Parser.Default.ParseArguments<SettingsV2>(v2args)
+                    .WithParsed(options => Run(options))
+                    .WithNotParsed(errors => HandleErrors(errors)); // errors is a sequence of type IEnumerable<Error>
+
+                return;
+            }
+
             var result = Parser.Default.ParseArguments<RunSettings>(args)
                 .WithParsed(options => Run(options))
                 .WithNotParsed(errors => HandleErrors(errors)); // errors is a sequence of type IEnumerable<Error>
@@ -32,6 +43,14 @@ namespace PrimeCSharp
             {
                 SieveRunner.RunSieve(options);
             }
+        }
+
+        private static void Run(SettingsV2 options)
+        {
+            if (options.Benchmark)
+                Benchmarker.RunBenchmark(options.Bench);
+            else
+                SieveRunnerV2.Run(options);
         }
 
         private static void HandleErrors(IEnumerable<Error> errors)
