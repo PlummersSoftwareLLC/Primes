d23284be5e82a0383e13b05f89ef2de2da921343
Rutger van Bergen <rbergen@xs4all.nl>
2021-11-27 11:31:27 +0100

    6502 assembly solution for Commodore PET (#769)

---
+++ Prime6502Assembly/solution_2/dockerrun.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+c1541 -format primes,c1 d64 ./primes.d64 -write ./primes.prg > /dev/null
+xpet -console -autostart ./primes.d64 +sound > /dev/null &
+
+while : ; do
+    sleep 10 
+    c1541 -attach ./primes.d64 -list | grep -q output && \
+        c1541 -attach ./primes.d64 -read output,s output.txt > /dev/null && \
+        grep -q "TIME 0X[0-9A-F]\{6\}" output.txt && \
+        break
+done
+
+kill %1
+
+. ./parse.sh
