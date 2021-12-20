975e221466a0c7a4945ba29b15f1d64c3722c904
Peter Strömberg <pez@pezius.com>
2021-12-20 13:54:41 +0100

    Add two non-parallel Clojure/solution_3 variants (#797)

--- PrimeClojure/solution_3/run.sh
+++ PrimeClojure/solution_3/run.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
-clojure -X sieve/run :warm-up? true
+clojure -X sieve/run :variant :boolean-array :warm-up? true
+clojure -X sieve/run :variant :bitset :warm-up? false
+clojure -X sieve/run :variant :boolean-array-futures :warm-up? true
