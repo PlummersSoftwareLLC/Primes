c1ae7e0c2a2e38c17ec4db045d6c5f6bfaf2aa51
Matthew McDonald <mmcdon20@live.com>
2022-01-17 13:55:20 -0600

    Dart/solution_1: rewrite of parallel runners (#811)

--- PrimeDart/solution_1/bin/PrimeDartParallel.dart
+++ PrimeDart/solution_1/bin/PrimeDartParallel.dart
@@ -1,26 +1,40 @@
 import 'dart:io';
+import 'dart:isolate';
+import 'dart:math';
 import 'PrimeDart.dart';
-import 'WorkerPool.dart';
 
-void work() {
-  final sieve = PrimeSieve(1000000);
-  sieve.runSieve();
+void work(SendPort port) {
+  var passes = 0;
+  final timer = Stopwatch()..start();
+
+  while (true) {
+    final sieve = PrimeSieve(1000000);
+    sieve.runSieve();
+    passes++;
+
+    if (timer.elapsedMicroseconds >= 5000000) {
+      port.send({
+        'time': timer.elapsedMicroseconds / 1000000,
+        'passes': passes,
+      });
+      return;
+    }
+  }
 }
 
 Future<void> main() async {
   final processors = Platform.numberOfProcessors;
-  final pool = await WorkerPool.init(numberOfWorkers: processors);
-
-  final timer = Stopwatch()..start();
-
-  pool.broadcast(const Start(work: work));
-  await Future.delayed(const Duration(seconds: 5));
-  pool.broadcast(const Stop());
-
-  timer.stop();
-
-  final duration = timer.elapsedMicroseconds / 1000000;
-  final passes = await pool.passes();
+  final receivePort = ReceivePort();
+  for (var i = 0; i < processors; i++) {
+    await Isolate.spawn(work, receivePort.sendPort);
+  }
+  var passes = 0;
+  var time = 0.0;
+  await for (Map<String, num> message in receivePort.take(processors)) {
+    passes += message['passes'] as int;
+    time = max(time, message['time'] as double);
+  }
+  receivePort.close();
   stdout.writeln(
-      'eagerestwolf&mmcdon20_8bit_par;$passes;$duration;$processors;algorithm=base,faithful=yes,bits=8');
+      'eagerestwolf&mmcdon20_8bit_par;$passes;$time;$processors;algorithm=base,faithful=yes,bits=8');
 }
