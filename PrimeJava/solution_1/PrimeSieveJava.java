36d5eb14bdd0ab4ba376500bd8bd63880edf4aa7
Peter Strömberg <pez@pezius.com>
2022-02-01 17:10:08 +0100

    Java/solution_1: simplify and speed up implementation (#817)

--- PrimeJava/solution_1/PrimeSieveJava.java
+++ PrimeJava/solution_1/PrimeSieveJava.java
@@ -1,138 +1,112 @@
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Arrays;
 
 public class PrimeSieveJava
 {
 	private static final Map<Integer, Integer> VALIDATION_DATA;
-	
-	// Java has a BitSet class included but switching to a boolean array of size improves performance by a lot
-	// This brings the limitation of the sieve only being able to test numbers up to Integer.MAX_VALUE - 2 (Requested array size exceeds VM limit)
+
 	private final boolean[] dataSet;
 	private final int sieveSize;
-	
+
 	public PrimeSieveJava(int sieveSize)
 	{
 		this.sieveSize = sieveSize;
-		// unlike the other old implementations the dataSet isn't initialized with true values to optimize speed
 		dataSet = new boolean[(sieveSize + 1) >> 1];
+		Arrays.fill(dataSet, true);
 	}
-	
-	public int countPrimes()
+
+	public int count() 
 	{
 		int count = 0;
 		for (int i = 0; i < dataSet.length; i++)
 		{
-			if (!dataSet[i])
+			if (dataSet[i])
 			{
 				count++;
 			}
 		}
-		
 		return count;
 	}
-	
+
 	public boolean validateResults()
 	{
 		if (VALIDATION_DATA.containsKey(sieveSize))
 		{
-			return VALIDATION_DATA.get(sieveSize) == countPrimes();
+			return VALIDATION_DATA.get(sieveSize) == count();
 		}
-		
 		return false;
 	}
-	
-	// Naming hasn't changed for comparison of the methods but removing the if statement for a branchless comparison
-	// and inverting the statement due to not initializing the dataSet to true results in a boost too.
-	// Also rather interesting: checking index % 2 != 0 is slower than index % 2 == 1
-	private boolean getBit(int index)
-	{
-		return (index & 1) == 1 && !dataSet[index >> 1];
-	}
-	
-	// Again instead of checking if index is even we just update the array at that index equivalent to that check
-	// to boost performance
-	private void clearBit(int index)
-	{
-		dataSet[index >> 1] = (index & 1) == 1;
-	}
-	
-	public void runSieve()
-	{
-		int factor = 3;
-		int q = (int) Math.sqrt(sieveSize);
-		
-		while (factor < q)
-		{
-			for (int num = factor; num <= sieveSize; num++)
-			{
-				if (getBit(num))
-				{
-					factor = num;
-					break;
-				}
-			}
-			
-			for (int num = factor * factor; num <= sieveSize; num += factor * 2)
-				clearBit(num);
-			
-			factor += 2;
-		}
-	}
-	
-	public void printResults(boolean showResults, double duration, int passes)
-	{
-		if (showResults)
-		{
-			System.out.print("2, ");
-		}
-		
-		int count = 1;
-		for (int num = 3; num <= this.sieveSize; num++)
-		{
-			if (getBit(num))
-			{
-				if (showResults)
-				{
-					System.out.print(num + ", ");
-				}
-				
-				count++;
-			}
-		}
-		
-		if (showResults)
-		{
-			System.out.println();
-		}
-		
-		System.out.printf("Passes: %d, Time: %f, Avg: %f, Limit: %d, Count: %d, Valid: %s%n", passes, duration, (duration / passes), sieveSize, count, validateResults());
 
-        // Following 2 lines added by rbergen to conform to drag race output format
-		System.out.println();
-		System.out.printf("MansenC;%d;%f;1;algorithm=base,faithful=yes\n", passes, duration);
-		
+	public void run()
+	{
+		int q = (int) Math.ceil(Math.sqrt(sieveSize));
+		for (int p = 3; p < q; p += 2)
+		{
+			if (dataSet[p >> 1])
+			{
+				for (int i = (p * p) >> 1; i < sieveSize >> 1; i += p)
+				{
+					dataSet[i] = false;
+				}
+			}
+		}
 	}
-	
+
+	public void printResults(double duration, int passes)
+	{
+		int count = count();
+		System.out.printf("Passes: %d, Time: %f, Avg: %f, Limit: %d, Count: %d, Valid: %s%n", passes,
+				duration, (duration / passes), sieveSize, count, validateResults());
+		System.out.printf("MansenC+pez;%d;%f;1;algorithm=base,faithful=yes,bits=8\n", passes, duration);
+	}
+
 	public static void main(String[] args)
 	{
 		long start = System.currentTimeMillis();
-		int passes = 0;
 		PrimeSieveJava sieve = null;
-		
-		while ((System.currentTimeMillis() - start) < 5000)
+		int limit = 1000000;
+		boolean warmup = false;
+		int warmupTime = 5000;
+		int runTime = 5000;
+
+		for (int i = 0; i < args.length; i++)
 		{
-			sieve = new PrimeSieveJava(1000000);
-			sieve.runSieve();
+			if (args[i].equals("-limit") && args.length - 1 > i)
+			{
+				limit = Integer.parseInt(args[i + 1]);
+			}
+			if (args[i].equals("-warmup"))
+			{
+				warmup = true;
+			}
+		}
+
+		if (warmup)
+		{
+			while ((System.currentTimeMillis() - start) < warmupTime)
+			{
+				sieve = new PrimeSieveJava(limit);
+				sieve.run();
+			}
+		}
+
+		int passes = 0;
+		start = System.currentTimeMillis();
+		while ((System.currentTimeMillis() - start) < runTime)
+		{
+			sieve = new PrimeSieveJava(limit);
+			sieve.run();
 			passes++;
 		}
-		
+
 		long delta = System.currentTimeMillis() - start;
 		if (sieve != null)
 		{
-			sieve.printResults(false, delta / 1000d, passes);
+			sieve.printResults(delta / 1000d, passes);
 		}
 	}
-	
+
 	static
 	{
 		VALIDATION_DATA = new HashMap<>();
--- PrimeJava/solution_1/README.md
+++ PrimeJava/solution_1/README.md
@@ -1,6 +1,32 @@
-# Java solution by MansenC
+# Plain Java solution by MansenC
 
 ![Algorithm](https://img.shields.io/badge/Algorithm-base-green)
 ![Faithfulness](https://img.shields.io/badge/Faithful-yes-green)
 ![Parallelism](https://img.shields.io/badge/Parallel-no-green)
-![Bit count](https://img.shields.io/badge/Bits-unknown-yellowgreen)
+![Bit count](https://img.shields.io/badge/Bits-8-yellowgreen)
+
+
+A faithful and straight-forward implementation of the sieve. Creates a `boolean` array with all indexes `true`, then jogs through it, following the Eratosthenes algorith, setting all non-primes to `false`. (Simplified and sped up by [PEZ](https://github.com/PEZ).)
+
+Run it like so:
+
+```sh
+$ javac Sieve.java && java Sieve [-limit <limit>] [-warmup]
+```
+
+Defaults to `limit` = 1000000, and no warmup.
+
+Run it with docker like so:
+
+```sh
+$ docker pull openjdk:17
+$ docker build -t pez-primes-java .
+$ docker run --rm -it pez-primes-java
+```
+
+Sample output:
+
+```
+Passes: 10324, Time: 5.000000, Avg: 0.000484, Limit: 1000000, Count: 78498, Valid: true
+MansenC+pez;10324;5.000000;1;algorithm=base,faithful=yes,bits=8
+```
