f18acebcab9e6cc41b9852da395243f8481a10f9
Peter Strömberg <pez@pezius.com>
2022-02-07 12:15:44 +0100

    Use -XX:+UseNUMA instead of numactl

--- PrimeJava/solution_1/run.sh
+++ PrimeJava/solution_1/run.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
-java PrimeSieveJava -warmup
-numactl --cpunodebind=0 --membind=0 java PrimeSieveJava -variant bitset -warmup || java PrimeSieveJava -variant bitset -warmup
+RUNS=$(if [ -z "$1" ]; then echo 1; else echo $1; fi)
+for ((i = 0; i < RUNS; i++))
+do
+  echo $i
+  java -XX:+UseNUMA PrimeSieveJava -warmup
+  java -XX:+UseNUMA PrimeSieveJava -variant bitset -warmup || java PrimeSieveJava -variant bitset -warmup
+done
