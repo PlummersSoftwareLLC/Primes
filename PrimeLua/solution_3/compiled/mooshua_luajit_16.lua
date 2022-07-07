8787f185ae0d9edd73b06a750f41842e6de7efe9
Mooshua <mooshua@crii.io>
2022-07-07 11:53:02 -0700

    Several Optimizations & Improvements to PrimeLua/solution-3
    * Improved cache performance by not allocating space for even numbers
    * Added experimental marking method which aims to break up dependency chains (and likely will fail horribly)
    * Added jitanalysis option to benchmark with certain opts disabled
    * Improved test script
    * Removed useless opt tuning (close enough to error margin that it's practically useless)
    * Compiled scripts are now in the compiled directory for ease of browsing (three are included for visibility on github)
    * Updated readme

---
+++ PrimeLua/solution_3/compiled/mooshua_luajit_16.lua
@@ -0,0 +1,47 @@
+local bit = require "bit"
+local ffi = require "ffi"
+local _i32 = ffi.typeof("int32_t")
+return function(ARENA)
+    local SIZE = 1000000
+    local PRIME_LEN = math.sqrt(SIZE)
+    local clock = os.clock
+    local begin = clock()
+    local iter = 0
+
+    while (clock()-begin) <= 5 do
+
+                for k = 3, PRIME_LEN, 2 do
+            if ARENA[bit.rshift(k,1)]  == 0 then
+                local v = k--_i32(k)
+                local vk = v*2
+                for x = (k*k), SIZE, k*32 do
+
+                    
+
+                    
+		ARENA[ bit.rshift(x,1) ] = 1;
+		ARENA[ bit.rshift(x + vk,1) ] = 1;
+		ARENA[ bit.rshift(x + vk + vk,1) ] = 1;
+		ARENA[ bit.rshift(x + vk + vk + vk,1) ] = 1;
+		ARENA[ bit.rshift(x + vk + vk + vk + vk,1) ] = 1;
+		ARENA[ bit.rshift(x + vk + vk + vk + vk + vk,1) ] = 1;
+		ARENA[ bit.rshift(x + vk + vk + vk + vk + vk + vk,1) ] = 1;
+		ARENA[ bit.rshift(x + vk + vk + vk + vk + vk + vk + vk,1) ] = 1;
+		ARENA[ bit.rshift(x + vk + vk + vk + vk + vk + vk + vk + vk,1) ] = 1;
+		ARENA[ bit.rshift(x + vk + vk + vk + vk + vk + vk + vk + vk + vk,1) ] = 1;
+		ARENA[ bit.rshift(x + vk + vk + vk + vk + vk + vk + vk + vk + vk + vk,1) ] = 1;
+		ARENA[ bit.rshift(x + vk + vk + vk + vk + vk + vk + vk + vk + vk + vk + vk,1) ] = 1;
+		ARENA[ bit.rshift(x + vk + vk + vk + vk + vk + vk + vk + vk + vk + vk + vk + vk,1) ] = 1;
+		ARENA[ bit.rshift(x + vk + vk + vk + vk + vk + vk + vk + vk + vk + vk + vk + vk + vk,1) ] = 1;
+		ARENA[ bit.rshift(x + vk + vk + vk + vk + vk + vk + vk + vk + vk + vk + vk + vk + vk + vk,1) ] = 1;
+		ARENA[ bit.rshift(x + vk + vk + vk + vk + vk + vk + vk + vk + vk + vk + vk + vk + vk + vk + vk,1) ] = 1;
+
+                end
+            end
+        end
+    
+
+        iter = iter + 1
+    end -->loop
+    return iter, clock()-begin
+end
