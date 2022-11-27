059ad312c38f44e7638a575eff7c4bc81e66c0d6
Rogier van Dam <rogiervandam@gmail.com>
2022-11-27 01:58:13 +0100

    PrimeC/solution_5: 50% speed increase and multiprocessor supported (#866)

--- PrimeC/solution_5/compile.sh
+++ PrimeC/solution_5/compile.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
-CC="gcc -Ofast -march=native -mtune=native -funroll-all-loops" 
+CC="gcc -Ofast -march=native -mtune=native -funroll-all-loops -fno-asynchronous-unwind-tables -fno-exceptions -fomit-frame-pointer -Wno-psabi"  
+PAR="-fopenmp"
+PAREXT="_epar"
 for x in sieve_extend; do
-    $CC -o $x $x.c -lm
+    for y in u32_v8 u64_v4 u64_v8; do
+        $CC -o $x-$y $x.c -D$y -s
+        $CC $PAR -o $x$PAREXT-$y $x.c -D$y -s
+    done
 done
