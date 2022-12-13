335b202c1d369de04ab534a56b327c0a187ef0d6
Darnell Brawner <167518+darnellbrawner@users.noreply.github.com>
2022-12-13 16:15:57 -0500

    small fix

--- PrimeRuby/solution_4/prime.rb
+++ PrimeRuby/solution_4/prime.rb
@@ -59,4 +59,4 @@ end until (Time.now.to_f - start_time) > 5.0
 duration = (Time.now.to_f - start_time).round(3)
 
 puts 'WARNING: result is incorrect!' unless reference_results[SIEVE_SIZE] == r[1].count_1
-puts "darnellbrawner-MultiThreaded;#{pass_count};#{duration};1;algorithm=base,faithful=no"
+puts "darnellbrawner-MultiThreaded+Numo;#{pass_count};#{duration};1;algorithm=base,faithful=no"
