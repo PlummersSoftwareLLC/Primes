25528b13413c03122c6898e14e73a3216180c09c
Ron Zuckerman <ron.zuckerman@adtran.com>
2023-01-07 13:38:04 -0600

    Make language case-insensitive and clarify usage of script

--- tools/hadolint.sh
+++ tools/hadolint.sh
@@ -1,25 +1,41 @@
 #!/bin/bash
+SCRIPT="tools/$(basename "$0")"
 if [ "$#" -lt 2 ]
 then
-    echo "usage: tools/$(basename "$0") <language> <number>"
+    echo "usage: ${SCRIPT} <language> <number>"
     echo "where:"
-    echo "- <language> is language of your solution"
-    echo "- <number> is the solution number"
+    echo "- <language> corresponds to the 'Prime' directory for the specified language."
+    echo "  This is case-insensitive."
+    echo "- <number> is the solution number."
     echo ""
-    echo "Example:"
+    echo "Examples:"
     echo ""
-    echo "    tools/$(basename "$0") CPP 1"
+    echo "    ${SCRIPT} CPP 1"
+    echo ""
+    echo "or"
+    echo ""
+    echo "    ${SCRIPT} cpp 1"
+    echo ""
+    echo "Both of these correspond to the PrimeCPP/solution_1 directory."
     exit 1
 fi
 
-if [ ! -f "Prime$1/solution_$2/Dockerfile" ]
+LANG_DIR="$(find . -mindepth 1 -maxdepth 1 -type d -iname "Prime$1" -exec basename '{}' ';' 2>/dev/null)"
+if [ -z "${LANG_DIR}" ]
 then
-    echo "'Primes$1/solution_$2/Dockerfile' does not exist"
+    echo "Cannot find corresponding 'Prime' directory for language $1"
     exit 1
 fi
 
-echo "Running hadolint"
-if docker run --rm -i -v "$(pwd)/config:/.config" hadolint/hadolint < "Prime$1/solution_$2/Dockerfile"
+DOCKERFILE="${LANG_DIR}/solution_$2/Dockerfile"
+if [ ! -f "${DOCKERFILE}" ]
+then
+    echo "'${DOCKERFILE}' does not exist"
+    exit 1
+fi
+
+echo "Running hadolint on '${DOCKERFILE}'"
+if docker run --rm -i -v "$(pwd)/config:/.config" hadolint/hadolint < "${DOCKERFILE}"
 then
     echo "hadolint succeeded"
 else
