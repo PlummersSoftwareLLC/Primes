8f1984a2124df3c8a63a97ee28961b51f8e21d23
David W Plummer <davepl@davepl.com>
2023-01-29 15:43:35 -0800

    C#/solution_2: improve performance (#489)

--- PrimeCSharp/solution_2/PrimeCS.cs
+++ PrimeCSharp/solution_2/PrimeCS.cs
@@ -3,9 +3,11 @@
 // ---------------------------------------------------------------------------
 
 using System;
+using System.Buffers;
 using System.Collections;
 using System.Collections.Generic;
 using System.Globalization;
+using System.Runtime.CompilerServices;
 
 namespace PrimeSieveCS
 {
@@ -13,89 +15,111 @@ namespace PrimeSieveCS
     {
         class prime_sieve
         {
-            private readonly int sieveSize = 0;
-            private readonly BitArray bitArray; //making it readonly so we tell the compiler that the variable reference cant change. around 5% increase in performance
-            private Dictionary<int, int> myDict = new Dictionary<int, int> 
-            { 
-                { 10 , 4 },                 // Historical data for validating our results - the number of primes
-                { 100 , 25 },               // to be found under some limit, such as 168 primes under 1000
-                { 1000 , 168 },
-                { 10000 , 1229 },
-                { 100000 , 9592 },
-                { 1000000 , 78498 },
-                { 10000000 , 664579 },
-                { 100000000 , 5761455 } 
+            private int sieveSize = 0;
+            private int halfLimit;
+            private const int shiftAmount = 5;
+
+            private readonly UInt32[] halfbits;
+
+            private static Dictionary<int, int> myDict = new Dictionary<int, int>
+            {
+                {          10 , 4 },                // Historical data for validating our results - the number of primes
+                {         100 , 25 },               // to be found under some limit, such as 168 primes under 1000
+                {       1_000 , 168 },
+                {      10_000 , 1229 },
+                {     100_000 , 9592 },
+                {   1_000_000 , 78498 },
+                {  10_000_000 , 664579 },
+                { 100_000_000 , 5761455 }
             };
 
-            public prime_sieve(int size) 
+            public prime_sieve(int size)
             {
                 sieveSize = size;
-                bitArray = new BitArray(((this.sieveSize + 1) / 2), true);
+                halfLimit = (size + 1) / 2;
+                
+                int arraySize = (halfLimit >> shiftAmount) + 1;
+                halfbits = ArrayPool<UInt32>.Shared.Rent(arraySize);
+                
+                // the buffer we get from the pool may not be zero-initialized
+                for (int i = 0; i < arraySize; i++)
+                    halfbits[i] = 0;
             }
 
-            public int countPrimes()
+            public int countOfPrimes
             {
-                int count = 0;
-                for (int i = 0; i < this.bitArray.Count; i++)
-                    if (bitArray[i])
-                        count++;
-                return count;
+                get
+                {
+                    var bits = halfbits.AsSpan();
+
+                    int count = 0;
+                    for (int num = 1; num <= this.halfLimit; num++)
+                        if ((bits[num >> shiftAmount] & (1U << (num))) == 0)
+                            count++;
+                    return count;
+                }
             }
 
-            public bool validateResults()
+            public bool containsValidResults
             {
-                if (myDict.ContainsKey(this.sieveSize))
-                    return this.myDict[this.sieveSize] == this.countPrimes();
-                return false;
-            }
-
-            private bool GetBit(int index)
-            {
-                if (index % 2 == 0)
-                    return false;
-                return bitArray[index / 2];
+                get
+                {
+                    return (myDict.ContainsKey(this.sieveSize) && myDict[this.sieveSize] == this.countOfPrimes);
+                }
             }
 
             // primeSieve
             // 
             // Calculate the primes up to the specified limit
 
+            [MethodImpl(MethodImplOptions.AggressiveOptimization)]
             public void runSieve()
             {
-                int factor = 3;
-                int q = (int) Math.Sqrt(this.sieveSize);
+                var bits = halfbits.AsSpan();
+                var factor = 3;
+                var halfFactor = factor >> 1;
+                var halfRoot = ((int)(Math.Sqrt(this.sieveSize) + 1)) >> 1;
 
-                while (factor < q)
+                // We ignore even numbers by using values that track half of the actuals, and the only
+                // number we keep in original form is the prime factor we're walking through the sieve
+
+                while (halfFactor <= halfRoot)
                 {
-                    for (int num = factor / 2; num <= this.bitArray.Count; num++)
+                    // Scan for the next unset bit which means it is a prime factor
+
+                    while (halfFactor <= halfRoot)
                     {
-                        if (bitArray[num])
-                        {
-                            factor = num * 2 + 1;
+                        if ((bits[halfFactor >> shiftAmount] & (1U << (halfFactor))) == 0U)
                             break;
-                        }
+                        halfFactor++;
                     }
 
-                    // If marking factor 3, you wouldn't mark 6 (it's a mult of 2) so start with the 3rd instance of this factor's multiple.
-                    // We can then step by factor * 2 because every second one is going to be even by definition.
-                    // Note that bitArray is only storing odd numbers. That means an increment of "num" by "factor" is actually an increment of 2 * "factor"
+                    factor = (halfFactor << 1) + 1;
 
-                    for (int num = factor * 3 / 2; num < this.bitArray.Count; num += factor)
-                        this.bitArray[num] = false;
+                    // Mark off all multiples starting with the factor's square up to the square root of the limit
 
-                    factor += 2;
+                    for (var index = (factor * factor) >> 1; index < halfLimit; index += factor)
+                        bits[index >> shiftAmount] |= (1U << (index));
+
+                    halfFactor++;
                 }
+
+                // Return the memory to the pool
+
+                ArrayPool<UInt32>.Shared.Return(halfbits);
             }
 
             public void printResults(bool showResults, double duration, int passes)
             {
+                var bits = halfbits.AsSpan();
+
                 if (showResults)
                     Console.Write("2, ");
 
                 int count = 1;
-                for (int num = 3; num <= this.sieveSize; num++)
+                for (int num = 3; num <= this.sieveSize; num += 2)
                 {
-                    if (GetBit(num))
+                    if ((bits[(num / 2) >> shiftAmount] & (1U << (num / 2))) == 0)
                     {
                         if (showResults)
                             Console.Write(num + ", ");
@@ -107,8 +131,8 @@ namespace PrimeSieveCS
 
                 CultureInfo.CurrentCulture = new CultureInfo("en_US", false);
 
-                Console.WriteLine("Passes: " + passes + ", Time: " + duration + ", Avg: " + (duration / passes) + ", Limit: " + this.sieveSize + ", Count: " + count + ", Valid: " + validateResults());
-            
+                Console.WriteLine("Passes: " + passes + ", Time: " + duration + ", Avg: " + (duration / passes) + ", Limit: " + this.sieveSize + ", Count: " + count + ", Valid: " + this.containsValidResults);
+
                 // Following 2 lines added by rbergen to conform to drag race output format
                 Console.WriteLine();
                 Console.WriteLine($"davepl;{passes};{duration:G6};1;algorithm=base,faithful=yes,bits=1");
