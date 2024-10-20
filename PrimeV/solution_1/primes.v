ad818c8c256e55f8d40f4c5756c6cdd4891f8a76
Tudor Marghidanu <tudor@f2pool.io>
2024-10-20 21:26:19 +0300

    Added formatting and removed deprecated attributes syntax

--- PrimeV/solution_1/primes.v
+++ PrimeV/solution_1/primes.v
@@ -1,23 +1,21 @@
 import time
 import math
 
-const (
-	sieve_size          = 1_000_000
-	q                   = math.sqrt(sieve_size)
-	all_bits_true_array = []bool{len: sieve_size, init: true}
-	dictionary          = {
-		'10':          4
-		'100':         25
-		'1000':        168
-		'10000':       1229
-		'100000':      9592
-		'1000000':     78498
-		'10000000':    664579
-		'100000000':   5761455
-		'1000000000':  50847534
-		'10000000000': 455052511
-	}
-)
+const sieve_size = 1_000_000
+const q = math.sqrt(sieve_size)
+const all_bits_true_array = []bool{len: sieve_size, init: true}
+const dictionary = {
+	'10':          4
+	'100':         25
+	'1000':        168
+	'10000':       1229
+	'100000':      9592
+	'1000000':     78498
+	'10000000':    664579
+	'100000000':   5761455
+	'1000000000':  50847534
+	'10000000000': 455052511
+}
 
 struct Sieve {
 	sieve_size u64
@@ -25,7 +23,7 @@ mut:
 	bits []bool
 }
 
-[direct_array_access]
+@[direct_array_access]
 fn (mut sieve Sieve) run_sieve() {
 	mut factor := u64(3)
 
@@ -54,7 +52,7 @@ fn (sieve Sieve) print_results(show_results bool, duration time.Duration, passes
 	for num := u64(3); num <= sieve.sieve_size; num += u64(2) {
 		if sieve.bits[num] {
 			if show_results {
-				print('$num, ')
+				print('${num}, ')
 			}
 
 			count++
@@ -68,9 +66,9 @@ fn (sieve Sieve) print_results(show_results bool, duration time.Duration, passes
 	avg := f64(duration / passes)
 	count_primes := sieve.count_primes()
 	valid := (count_primes == u64(dictionary[sieve.sieve_size.str()]))
-	eprintln('Passes: $passes, Time: $duration, Avg: $avg, Limit: $sieve.sieve_size, Count1: $count, Count2: $count_primes, Valid: $valid')
+	eprintln('Passes: ${passes}, Time: ${duration}, Avg: ${avg}, Limit: ${sieve.sieve_size}, Count1: ${count}, Count2: ${count_primes}, Valid: ${valid}')
 
-	println('marghidanu;$passes;$duration;1;algorithm=base,faithful=yes')
+	println('marghidanu;${passes};${duration};1;algorithm=base,faithful=yes')
 }
 
 fn (sieve Sieve) count_primes() u64 {
@@ -92,7 +90,7 @@ fn main() {
 	for {
 		mut sieve := Sieve{
 			sieve_size: 1_000_000
-			bits: all_bits_true_array
+			bits:       all_bits_true_array
 		}
 		sieve.run_sieve()
 
