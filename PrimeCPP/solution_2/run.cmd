99304e64df3c50b6788271aefd934d4365d99882
Rutger van Bergen <rbergen@xs4all.nl>
2024-11-08 06:02:15 +0100

    Fix CMD script

--- PrimeCPP/solution_2/run.cmd
+++ PrimeCPP/solution_2/run.cmd
@@ -1,17 +1,27 @@
-@echo off
+@ECHO OFF
 
-IF "%1" == "" || "%1" == "1" || "%1" == "array" (
+SET "_RUN_ARRAY=0"
+IF [%1] == [] SET "_RUN_ARRAY=1"
+IF [%1] == [1] SET "_RUN_ARRAY=1"
+IF [%1] == [array] SET "_RUN_ARRAY=1"
+IF %_RUN_ARRAY% == 1 (
     ECHO Building and running the array approach...
-    ECHO(
+    ECHO:
     g++ -Ofast PrimeCPP_array.cpp -std=c++17 -lstdc++ -oPrimes_array.exe
     .\Primes_array.exe
-    ECHO(
+    ECHO:
 )
+SET _RUN_ARRAY=
 
-IF "%1" == "" || "%1" == "2" || "%1" == "mask" (
+SET "_RUN_MASK=0"
+IF [%1] == [] SET "_RUN_MASK=1"
+IF [%1] == [2] SET "_RUN_MASK=1"
+IF [%1] == [mask] SET "_RUN_MASK=1"
+IF %_RUN_MASK% == 1 (
     ECHO Building and running the mask approach...
-    ECHO(
+    ECHO:
     g++ -Ofast PrimeCPP_mask.cpp -std=c++17 -lstdc++ -oPrimes_mask.exe
     .\Primes_mask.exe
-    ECHO(
+    ECHO:
 )
+SET _RUN_MASK=
