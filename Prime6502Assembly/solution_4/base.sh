4694b6519fb4dd732fcc527cf3b65c42042efc61
Rutger van Bergen <rbergen@xs4all.nl>
2026-04-11 04:38:23 +0200

    Bash script refactor

---
+++ Prime6502Assembly/solution_4/base.sh
@@ -0,0 +1,2 @@
+#/bin/bash
+SRC="PrimeSieve1M.asm"
--- Prime6502Assembly/solution_4/build.sh
+++ Prime6502Assembly/solution_4/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
+. base.sh
+
 KICKASS="KickAssembler/KickAss.jar"
-SRC="PrimeSieve1M.asm"
-PRG="PrimeSieve1M.prg" # Must be same as SRC with a .prg suffix
 
 # Assemble - produce BATCHMODE version of the program
 java -jar $KICKASS -define BATCHMODE $SRC
